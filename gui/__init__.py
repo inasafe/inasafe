d14800e4d4b07effbba1ffffb046daaebc8d111e
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2012-01-23 13:57:23 +0700

    Small fixes in clipper

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,7 +1,6 @@
-import sys,os
-sys.path.append(
-    os.path.abspath(
-      os.path.join(os.path.dirname(__file__), '..')
-      )
-    )
+import os
+import sys
 
+# Add parent directory to path to make test aware of other modules
+pardir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(pardir)
