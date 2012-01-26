f93a5361603bc30b640e34704149c670af7ac94d
Ted Dunstone <ted@biometix.com>
2012-01-26 20:56:11 +1100

    path modified to ensure that plugins can be seen from the gui logging added but still not fully functional plugin refresh code worked on but still not functional

--- __init__.py
+++ __init__.py
@@ -20,6 +20,15 @@ __copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
                  'Disaster Reduction')
 
 
+# Add parent directory to path to make test aware of other modules
+import sys
+import os
+
+curdir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(curdir)
+
+
+
 def name():
     """A user friendly name for the plugin."""
     return 'Risk in a box'
@@ -49,5 +58,22 @@ def qgisMinimumVersion():
 def classFactory(iface):
     """Load Riab class from file Riab"""
 
+    # setup the logging
+    import logging
+    logger = logging.getLogger('risiko')
+    
+    # TODO (TD): Change to read from a log file see http://docs.python.org/howto/logging-cookbook.html#logging-cookbook
+    # FIXME (TD): Logger is working at this level but not in the impact functions!!
+    logger.setLevel(logging.DEBUG)
+    ch = logging.FileHandler('risiko.log')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    
+    # Try loading the FunctionProvider
+    from impact_functions.core import FunctionProvider
+    # FIXME (TD): reload doesn't seem to reload the plugins anything
+    
+    logger.debug("reload core 3")
     from gui.riab import Riab
     return Riab(iface)
