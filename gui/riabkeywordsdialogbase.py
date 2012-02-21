1d77cb6bb28bb42c586eca22b205bb7a3489e8c5
Tim Sutton <tim@linfiniti.com>
2012-02-21 12:35:22 +0200

    Added keyword dialog base class

--- gui/riabkeywordsdialogbase.py
+++ gui/riabkeywordsdialogbase.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'riabkeywordsdialogbase.ui'
 #
-# Created: Tue Feb 21 11:11:36 2012
+# Created: Tue Feb 21 12:34:53 2012
 #      by: PyQt4 UI code generator 4.8.5
 #
 # WARNING! All changes made in this file will be lost!
