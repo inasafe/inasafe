b96b00ddbf58375d2b60419c10f13daa4b9c3b74
Tim Sutton <tim@linfiniti.com>
2012-02-23 18:03:30 +0200

    Merged branch density_issue92 into keywords branch to combine Ole's work on #92 with my work on #80.

--- gui/resources.py
+++ gui/resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Thu Feb 23 05:51:03 2012
+# Created: Thu Feb 23 18:01:10 2012
 #      by: The Resource Compiler for PyQt (Qt v4.7.3)
 #
 # WARNING! All changes made in this file will be lost!
--- gui/riabkeywordsdialogbase.py
+++ gui/riabkeywordsdialogbase.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'riabkeywordsdialogbase.ui'
 #
-# Created: Thu Feb 23 06:17:00 2012
+# Created: Thu Feb 23 18:01:10 2012
 #      by: PyQt4 UI code generator 4.8.5
 #
 # WARNING! All changes made in this file will be lost!
