4c7169619c2aaa723c1b038e3c9addd615103ef7
Ole Nielsen <nielso@safe.(none)>
2012-03-08 10:37:46 +0700

    Attempted raster style with floats which raise issue #126

--- impact_functions/styles.py
+++ impact_functions/styles.py
@@ -26,3 +26,16 @@ style_classes = [dict(colour='#38A800', quantity=2, transparency=100),
                  dict(colour='#7A0000', quantity=300, transparency=0)]
 flood_population_style = dict(target_field=None,
                               style_classes=style_classes)
+
+# Flood population impact raster style
+# FIXME (Ole): The styler cannot handle floats yet. Issue #126
+style_classes = [dict(colour='#38A800', quantity=0.000002, transparency=100),
+                 dict(colour='#38A800', quantity=0.000005, transparency=0),
+                 dict(colour='#79C900', quantity=0.000010, transparency=0),
+                 dict(colour='#CEED00', quantity=0.000050, transparency=0),
+                 dict(colour='#FFCC00', quantity=0.000100, transparency=0),
+                 dict(colour='#FF6600', quantity=0.000200, transparency=0),
+                 dict(colour='#FF0000', quantity=0.000500, transparency=0),
+                 dict(colour='#7A0000', quantity=0.001000, transparency=0)]
+earthquake_fatality_style = dict(target_field=None,
+                                 style_classes=style_classes)
