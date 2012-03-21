a6dc11dabc712c518af7fc386bde3c604e4700e6
Tim Sutton <tim@linfiniti.com>
2012-03-21 15:36:55 +0200

    More riab to inasafe renaming

--- scripts/update-strings.sh
+++ scripts/update-strings.sh
@@ -2,7 +2,7 @@
 #set -x
 LOCALE=$1
 PODIR=i18n/${LOCALE}/LC_MESSAGES
-POPATH=${PODIR}/riab.po
+POPATH=${PODIR}/inasafe.po
 #keep the current field separator
 oIFS=$IFS
 POFILES=$(egrep -r "import ugettext" . | cut -f 1 -d ':' | grep 'py$' | sort | uniq | tr '\n' ' ')
