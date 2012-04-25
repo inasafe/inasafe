e895ee12c618a5df1996a090d782f0098e371659
Tim Sutton <tim@linfiniti.com>
2012-04-26 01:31:20 +0200

    PEP8 Fix

--- impact_functions/tsunami/tsunami_building_impact.py
+++ impact_functions/tsunami/tsunami_building_impact.py
@@ -106,11 +106,11 @@ class TsunamiBuildingImpactFunction(FunctionProvider):
 
         # Create style
         style_classes = [dict(label='< 1 m', min=0, max=1,
-                              colour='#1EFC7C', transparency=0,size=1),
+                              colour='#1EFC7C', transparency=0, size=1),
                          dict(label='1 - 3 m', min=1, max=2,
-                              colour='#FFA500', transparency=0,size=1),
+                              colour='#FFA500', transparency=0, size=1),
                          dict(label='> 3 m', min=2, max=4,
-                              colour='#F31A1C', transparency=0,size=1)]
+                              colour='#F31A1C', transparency=0, size=1)]
         style_info = dict(target_field=self.target_field,
                           style_classes=style_classes)
 
