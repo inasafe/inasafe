5d40cf5d690d0049a32ee2855a1bef84b00ffd3f
Tim Sutton <tim@linfiniti.com>
2012-05-14 06:52:52 +0200

    Refactored the release script to use git archive so that sha1 is written into __revision__ tag in python modules

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,40 +12,23 @@ VERSION=$1
 # TODO
 #replace _type_ = 'alpha' or 'beta' with final
 
-#update the metadata file version
-TMP=metdata.txt$$
-cat metadata.txt | \
-  sed "s/^[Vv]ersion=[0-9]\.[0-9]\.[0-9v]/version=${VERSION}/g" \
-  | sed "s/^InaSAFE-dev/InaSAFE/g" \
-  > ${TMP}
-mv ${TMP} metadata.txt
-
-#update the __init__ version
-TMP=__init__.py$$
-cat __init__.py | \
-  sed "s/^[Vv]ersion=[0-9]\.[0-9]\.[0-9v]/version=${VERSION}/g" \
-  | sed "s/^InaSAFE-dev/InaSAFE/g" \
-  > ${TMP} 
-mv ${TMP} __init__.py
-
-#remove any crud
-find . -name "*.pyc" -exec rm -rf {} \;
-find . -name "*.*~" -exec rm -rf {} \;
-rm Makefile~
-
 #regenerate docs
 make docs
 
 #see http://stackoverflow.com/questions/1371261/get-current-working-directory-name-in-bash-script
 DIR=${PWD##*/}
 OUT="/tmp/${DIR}.${1}.zip"
-# Cant use git archive since we need generated docs to be bundled
-#git archive --prefix=${DIR}/ --format zip --output ${OUT} master
 
+rm -rf /tmp/${DIR}
+mkdir /tmp/${DIR}
+git archive `git branch | grep '\*'| sed 's/^\* //g'` | tar -x -C /tmp/${DIR}
+rm -rf /tmp/${DIR}/docs/
+mkdir -p /tmp/${DIR}/docs/_build
+cp -r docs/build/html /tmp/${DIR}/docs/_build/
+pushd .
+cd /tmp/
 # The \* tells zip to ignore recursively
 rm ${OUT}
-cd ..
-pwd
 zip -r ${OUT} ${DIR} --exclude \*.pyc \
                                   ${DIR}/docs/source\* \
                                   ${DIR}/docs/*.jpeg\
@@ -57,6 +40,7 @@ zip -r ${OUT} ${DIR} --exclude \*.pyc \
                                   ${DIR}/docs/cloud_sptheme\* \
                                   ${DIR}/docs/Flyer_InaSafe_FINAL.pdf \
                                   ${DIR}/.git\* \
+                                  ${DIR}/.gitattributes \
                                   ${DIR}/.settings\* \
                                   ${DIR}/.pydev\* \
                                   ${DIR}/.coverage\* \
@@ -74,10 +58,9 @@ zip -r ${OUT} ${DIR} --exclude \*.pyc \
                                   \Makefile~ \
                                   \Makefile
                                   
+popd
 
-cd ${DIR}
 
 echo "Your plugin archive has been generated as"
 ls -lah ${OUT}
 echo "${OUT}"
-echo "You should git commit the changes made by this script now" 
