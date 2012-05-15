021ecea75e0b1b52262b201a2b1b2fa3f55fc0b3
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2012-05-15 09:05:59 +0700

    pep8

--- impact_functions/volcanic/general_volcanic_population_impact.py
+++ impact_functions/volcanic/general_volcanic_population_impact.py
@@ -33,16 +33,16 @@ class VolcanoPopulationImpactFunction(FunctionProvider):
 
         # Identify hazard and exposure layers
         # Volcanic hazard level [0-1]
-        volcanic_hazard_level = get_hazard_layer(layers)    
+        volcanic_hazard_level = get_hazard_layer(layers)
         population = get_exposure_layer(layers)  # Density [people/area]
 
         # Extract data as numeric arrays
         V = volcanic_hazard_level.get_data(nan=0.0)
         # Population density
-        P = population.get_data(nan=0.0, scaling=True)  
+        P = population.get_data(nan=0.0, scaling=True)
 
         # Calculate impact as population exposed to depths > threshold
-        I = numpy.where(V > 2.0/3, P, 0)
+        I = numpy.where(V > 2.0 / 3, P, 0)
 
         # Generate text with result for this study
         number_of_people_affected = numpy.nansum(I.flat)
