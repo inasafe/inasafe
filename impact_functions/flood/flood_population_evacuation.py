cbbc5b77a7e715597d92c67a54a4bb298c4b6129
Tim Sutton <tim@linfiniti.com>
2012-05-24 11:09:26 +0200

    marked table data as translatable

--- impact_functions/flood/flood_population_evacuation.py
+++ impact_functions/flood/flood_population_evacuation.py
@@ -4,6 +4,7 @@ from impact_functions.core import get_hazard_layer, get_exposure_layer
 from impact_functions.styles import flood_population_style as style_info
 from storage.raster import Raster
 from impact_functions.tables import (Table, TableRow, TableCell)
+from storage.utilities import ugettext as _
 
 
 class FloodEvacuationFunction(FunctionProvider):
@@ -75,11 +76,11 @@ class FloodEvacuationFunction(FunctionProvider):
 
         table_header_row = TableRow(['Bantuan', 'Jumlah'], header=True)
         table_body = [table_header_row,
-                      ['Beras [kg]', rice],
-                      ['Air Minum [l]', drinking_water],
-                      ['Air Bersih [l]', water],
-                      ['Kit Keluarga', family_kits],
-                      ['Jamban Keluarga', toilets]]
+                      [_('Beras [kg]'), rice],
+                      [_('Air Minum [l]'), drinking_water],
+                      [_('Air Bersih [l]'), water],
+                      [_('Kit Keluarga'), family_kits],
+                      [_('Jamban Keluarga'), toilets]]
         #table_caption = 'Sumber: BNPB Perka 7/2008'
         impact_table = Table(table_body).toNewlineFreeString()
 
@@ -87,33 +88,33 @@ class FloodEvacuationFunction(FunctionProvider):
 
         iname = inundation.get_name()
         pname = population.get_name()
-        table_caption = ('Apabila terjadi "%s" perkiraan dampak '
+        table_caption = _('Apabila terjadi "%s" perkiraan dampak '
                          'terhadap "%s" kemungkinan yang terjadi&#58;'
                           % (iname, pname))
-        table_body.extend([['Perlu Evakuasi (x 1000)', '%s' % count1000],
-                      [TableCell('Catatan:', header=True, col_span=2)],
-                      [TableCell('Jumlah penduduk Jakarta %s'
-                                         % total1000, col_span=2)],
-                      [TableCell('Jumlah dalam ribuan', col_span=2)],
-                      [TableCell('Penduduk perlu dievakuasi ketika'
-                                        'banjir lebih dari %i m.<br>'
-                                        % threshold, col_span=2)],
-                      [TableCell('Jumlah dalam ribuan', col_span=2)],
-                      [TableCell('Minmum Bantuan per minggu (BNPB Perka'
-                                 ' 7/2008)', col_span=2)]
+        table_body.extend([[_('Perlu Evakuasi (x 1000)'), '%s' % count1000],
+                      [TableCell(_('Catatan:'), header=True, col_span=2)],
+                      [TableCell(_('Jumlah penduduk Jakarta %s'
+                                         % total1000), col_span=2)],
+                      [TableCell(_('Jumlah dalam ribuan'), col_span=2)],
+                      [TableCell(_('Penduduk perlu dievakuasi ketika'
+                                        'banjir lebih dari %i m.'
+                                        % threshold), col_span=2)],
+                      [TableCell(_('Jumlah dalam ribuan'), col_span=2)],
+                      [TableCell(_('Minmum Bantuan per minggu (BNPB Perka'
+                                 ' 7/2008)'), col_span=2)]
                       ])
         impact_summary = Table(table_body, caption=table_caption
                                ).toNewlineFreeString()
 
-        map_title = 'Penduduk yang Mungkin dievakuasi'
+        map_title = _('Penduduk yang Mungkin dievakuasi')
 
-        style_info['legend_title'] = 'Kepadatan Penduduk'
+        style_info['legend_title'] = _('Kepadatan Penduduk')
 
         # Create raster object and return
         R = Raster(I,
                    projection=inundation.get_projection(),
                    geotransform=inundation.get_geotransform(),
-                   name='Penduduk yang %s' % (self.plugin_name.lower()),
+                   name=_('Penduduk yang %s' % (self.plugin_name.lower())),
                    keywords={'impact_summary': impact_summary,
                              'impact_table': impact_table,
                              'map_title': map_title},
