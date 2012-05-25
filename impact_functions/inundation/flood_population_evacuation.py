8304953e9dbb193cd1e2652ad496258cd3b99380
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2012-05-25 17:49:31 +0700

    first pass of translated flood population evacuation - also simplified tables

--- impact_functions/inundation/flood_population_evacuation.py
+++ impact_functions/inundation/flood_population_evacuation.py
@@ -41,6 +41,8 @@ class FloodEvacuationFunction(FunctionProvider):
         # Identify hazard and exposure layers
         inundation = get_hazard_layer(layers)  # Flood inundation [m]
         population = get_exposure_layer(layers)
+        iname = inundation.get_name()
+        pname = population.get_name()
 
         # Extract data as numeric arrays
         D = inundation.get_data(nan=0.0)  # Depth
@@ -48,9 +50,6 @@ class FloodEvacuationFunction(FunctionProvider):
         # Calculate impact as population exposed to depths > threshold
         P = population.get_data(nan=0.0, scaling=True)
         I = numpy.where(D > threshold, P, 0)
-        iname = inundation.get_name()
-        pname = population.get_name()
-
 
         # Count totals
         total = int(numpy.sum(P))
@@ -58,76 +57,43 @@ class FloodEvacuationFunction(FunctionProvider):
         total1000 = str(total / 1000)
         count1000 = str(number_of_people_affected / 1000)
 
-        # Create impact_table based on BNPB Perka 7/2008 minimum bantuan
-        # Weekly needs (see issue #82)
+        # Calculate estimated needs based on BNPB Perka 7/2008 minimum bantuan
         rice = number_of_people_affected * 2.8
         drinking_water = number_of_people_affected * 17.5
         water = number_of_people_affected * 67
         family_kits = number_of_people_affected / 5
         toilets = number_of_people_affected / 20
 
-        #table_header_row = TableRow(['Bantuan', 'Jumlah'], header=True)
-        #table_body = [table_header_row,
-        #              [_('Beras [kg]'), rice],
-        #              [_('Air Minum [l]'), drinking_water],
-        #              [_('Air Bersih [l]'), water],
-        #              [_('Kit Keluarga'), family_kits],
-        #              [_('Jamban Keluarga'), toilets],
-        #              _('Sumber: BNPB Perka 7/2008')]
-
+        # Generate impact report for the pdf map
         table_body = [_('In case of "%s" the estimated impact to '
-                        '"%s" is:' % (iname, pname)),
-                      TableRow([_('People needing evacuation'), '%s (*)' % count1000], header=True),
-                      TableRow(['Needs per week', 'Total (*)'], header=True),
+                        '"%s" is:') % (iname, pname),
+                      TableRow([_('People needing evacuation'),
+                                '%s (*)' % count1000], header=True),
+                      TableRow([_('Needs per week'), _('Total (*)')],
+                               header=True),
                       [_('Rice [kg]'), int(rice / 1000)],
                       [_('Drinking Water [l]'), int(drinking_water / 1000)],
                       [_('Clean Water [l]'), int(water / 1000)],
                       [_('Family Kits'), int(family_kits / 1000)],
                       [_('Toilets'), int(toilets / 1000)]]
-
         impact_table = Table(table_body).toNewlineFreeString()
 
-
-        # Create summary table
-        #table_caption = _('Apabila terjadi "%s" perkiraan dampak '
-        #                 'terhadap "%s" kemungkinan yang terjadi&#58;'
-        #                  % (iname, pname))
-        #table_body.extend([[_('Perlu Evakuasi (x 1000)'), '%s' % count1000],
-        #              [TableCell(_('Catatan:'), header=True, col_span=2)],
-        #              [TableCell(_('Jumlah penduduk Jakarta %s'
-        #                                 % total1000), col_span=2)],
-        #              [TableCell(_('Jumlah dalam ribuan'), col_span=2)],
-        #              [TableCell(_('Penduduk perlu dievakuasi ketika'
-        #                                'banjir lebih dari %i m.'
-        #                                % threshold), col_span=2)],
-        #              [TableCell(_('Minimum Bantuan per minggu (BNPB perka'
-        #                         ' 7/2008)'), col_span=2)]
-        #              ])
+        # Extend impact report for on-screen display
         table_body.extend([TableRow(_('Notes:'), header=True),
-                           _('Total population Jakarta: %s' % total1000),
+                           _('Total population Jakarta: %s') % total1000,
                            _('* Counts are given in thousands'),
-                           _('People need evacuation if flood levels exceed %i m' % threshold),
-                           _('Minimum needs are defined in BNPB regulation 7/2008')])
-
-        impact_summary = Table(table_body)#.toNewlineFreeString()
-
-        fid = open('/home/nielso/test.html', 'w')
-        fid.write(str(impact_summary))
-        fid.close()
-
-        impact_summary = impact_summary.toNewlineFreeString()
-
-        #map_title = _('Penduduk yang Mungkin dievakuasi')
+                           _('People need evacuation if flood levels '
+                             'exceed %(eps)i m') % {'eps': threshold},
+                           _('Minimum needs are defined in BNPB '
+                             'regulation 7/2008')])
+        impact_summary = Table(table_body).toNewlineFreeString()
         map_title = _('People in need of evacuation')
-
-        #style_info['legend_title'] = _('Kepadatan Penduduk')
         style_info['legend_title'] = _('Population Density')
 
         # Create raster object and return
         R = Raster(I,
                    projection=inundation.get_projection(),
                    geotransform=inundation.get_geotransform(),
-                   #name=_('Penduduk yang %s' % (self.plugin_name.lower())),
                    name=_('Population which %s' % (self.plugin_name.lower())),
                    keywords={'impact_summary': impact_summary,
                              'impact_table': impact_table,
