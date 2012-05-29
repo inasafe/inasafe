b4a2d808359fed36d737974ba2a054df575a4fb8
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2012-05-29 10:46:12 +0700

    Updated generic flood building impact function with tables and translation

--- impact_functions/inundation/flood_generic_building_impact.py
+++ impact_functions/inundation/flood_generic_building_impact.py
@@ -1,7 +1,9 @@
 from impact_functions.core import FunctionProvider
 from impact_functions.core import get_hazard_layer, get_exposure_layer
+from impact_functions.core import get_question
 from storage.vector import Vector
 from storage.utilities import ugettext as _
+from impact_functions.tables import (Table, TableRow)
 
 
 class FloodBuildingImpactFunction(FunctionProvider):
@@ -9,7 +11,7 @@ class FloodBuildingImpactFunction(FunctionProvider):
 
     :param requires category=='hazard' and \
                     subcategory in ['flood', 'tsunami'] and \
-                    layertype in ['raster', 'vector']
+                    layertype in 'raster'
 
     :param requires category=='exposure' and \
                     subcategory=='building' and \
@@ -20,7 +22,7 @@ class FloodBuildingImpactFunction(FunctionProvider):
     plugin_name = _('Be temporarily closed')
 
     def run(self, layers):
-        """Risk plugin for tsunami population
+        """Generic building impact
         """
 
         threshold = 1.0  # Flood threshold [m]
@@ -29,23 +31,22 @@ class FloodBuildingImpactFunction(FunctionProvider):
         H = get_hazard_layer(layers)    # Depth
         E = get_exposure_layer(layers)  # Building locations
 
+        question = get_question(H.get_name(),
+                                E.get_name(),
+                                self.plugin_name.lower())
+
         # Interpolate hazard level to building locations
-        I = H.interpolate(E, attribute_name='flood_level')
+        I = H.interpolate(E, attribute_name='depth')
 
         # Extract relevant numerical data
         attributes = I.get_data()
         N = len(I)
 
-        # Calculate population impact
+        # Calculate building impact
         count = 0
         for i in range(N):
-            if H.is_raster:
-                # Get the interpolated depth
-                x = float(attributes[i]['flood_level'])
-                x = x > threshold
-            elif H.is_vector:
-                # Use interpolated polygon attribute
-                x = attributes[i]['Affected']
+            # Get the interpolated depth
+            x = float(attributes[i]['depth']) > threshold
 
             # Tag and count
             if x is True:
@@ -54,27 +55,22 @@ class FloodBuildingImpactFunction(FunctionProvider):
             # Add calculated impact to existing attributes
             attributes[i][self.target_field] = x
 
-        # Create report
-        Hname = H.get_name()
-        Ename = E.get_name()
-        impact_summary = _('<b>In case of "%s" the estimated impact to '
-                           '"%s" is&#58;</b><br><br><p>' % (Hname,
-                                                            Ename))
-        impact_summary += ('<table border="0" width="320px">'
-                   '   <tr><th><b>%s</b></th><th><b>%s</b></th></th>'
-                    '   <tr></tr>'
-                    '   <tr><td>%s &#58;</td><td>%i</td></tr>'
-                    '   <tr><td>%s &#58;</td><td>%i</td></tr>'
-                    '   <tr><td>%s &#58;</td><td>%i</td></tr>'
-                    '</table>' % (_('Status'), _('Number of buildings'),
-                                  _('Closed'), count,
-                                  _('Open'), N - count,
-                                  _('All'), N))
+        # Generate impact report for the pdf map
+        table_body = [question,
+                      TableRow([_('Status'), _('Number of buildings')],
+                               header=True),
+                      TableRow([_('Closed'), count]),
+                      TableRow([_('Open'), N - count]),
+                      TableRow([_('All'), N])]
 
-        impact_summary += '<br>'  # Blank separation row
-        impact_summary += '<b>' + _('Assumptions') + '&#58;</b><br>'
-        impact_summary += _('Buildings will need to close if flood'
-                            ' levels exceed %.1f m' % threshold)
+
+        table_body.append(TableRow(_('Notes:'), header=True))
+        table_body.append(_('Buildings will need to close if flood'
+                            ' levels exceed %.1f m') % threshold)
+
+        impact_summary = Table(table_body).toNewlineFreeString()
+        impact_table = impact_summary
+        map_title = _('Buildings that need to close due flooding')
 
         # Create style
         style_classes = [dict(label=_('Open'), min=0, max=0,
