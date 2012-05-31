d54c9e51b1171a80e08ce825e25bd84684721ca8
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2012-05-31 11:43:17 +0700

    Updated lookup table of layer titles to be translated

--- storage/titles.py
+++ storage/titles.py
@@ -20,6 +20,9 @@ translation system and can appear in the supported languages as with
 other strings in SAFE.
 """
 
+# FIXME (Ole): This approach can be generalised to any strings that are not
+#              statically declared such as attribute values.
+
 from utilities import ugettext as _
 
 titles = {'title1': _('DKI buildings'),       # Bangunan DKI
@@ -30,4 +33,23 @@ titles = {'title1': _('DKI buildings'),       # Bangunan DKI
           'title4': _('Sea wall collapse at Pluit'),  # Dam Pluit Runtuh
           'title5': _('Jakarta flood prone areas'),  # Daerah Rawan Banjir
           'title6': _('Population Jakarta'),  # Penduduk Jakarta
+          'Flood Depth (design) Jakarta': _('Flood Depth (design) Jakarta'),
+          'Flood Depth (current) Jakarta': _('Flood Depth (current) Jakarta'),
+          'Yogyakarta 2006 earthquake': _('Yogyakarta 2006 earthquake'),
+          'Indonesian Earthquake Hazard Map': _('Indonesian Earthquake '
+                                                'Hazard Map'),
+          'Maumere Tsunami Inundation': _('Maumere Tsunami Inundation'),
+          'Sumatran fault Mw 7.8 scenario': _('Sumatran fault Mw 7.8 '
+                                              'scenario'),
+          'Mentawai fault Mw 9.0 scenario': _('Mentawai fault Mw 9.0 scenario'),
+          'Shakemap Padang 2009': _('Shakemap Padang 2009'),
+          'OSM building footprints': _('OSM building footprints'),
+          'AIBEP schools': _('AIBEP schools'),
+          'Population density (5kmx5km)': _('Population density (5kmx5km)'),
           }
+
+#Skenario Gempabumi Sesar Sumatra Mw 7.8
+#Skenario Gempabumi Sesar Mentawai Mw 9.0
+#Gempabumi Padang 2009
+#Bangunan OSM
+#Perkiraan penduduk (5kmx5km)
