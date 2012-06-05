1bbfb01291ff996b6ac96d226d5a4fb157119a89
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2012-06-05 08:28:46 +0700

    pep8

--- gui/utilities_test.py
+++ gui/utilities_test.py
@@ -197,7 +197,10 @@ def setGeoExtent(theBoundingBox):
     myRect = QgsRectangle(*theBoundingBox)
     CANVAS.setExtent(myRect)
 
+
 DEVNULL = open(os.devnull, 'w')
+
+
 class RedirectStdStreams(object):
     """Context manager for redirection of stdout and stderr
 
@@ -226,10 +229,12 @@ class RedirectStdStreams(object):
 
     def __enter__(self):
         self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
-        self.old_stdout.flush(); self.old_stderr.flush()
+        self.old_stdout.flush()
+        self.old_stderr.flush()
         sys.stdout, sys.stderr = self._stdout, self._stderr
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self._stdout.flush(); self._stderr.flush()
+        self._stdout.flush()
+        self._stderr.flush()
         sys.stdout = self.old_stdout
         sys.stderr = self.old_stderr
