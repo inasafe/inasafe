55a28016cdbb32728e07447d528c552bb3ca959d
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2012-06-11 11:56:51 +0700

    Updated translations

--- gui/i18n/inasafe_af.ts
+++ gui/i18n/inasafe_af.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="af_ZA">
+<!DOCTYPE TS><TS version="1.1" language="af_ZA">
 <context>
     <name>@default</name>
     <message>
