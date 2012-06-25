774778052e25f9254f914cb71c27bc6189014ab7
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2012-06-25 14:33:03 +0700

    Removed excessive output from update-translation-strings

--- scripts/update-strings.sh
+++ scripts/update-strings.sh
@@ -6,7 +6,7 @@ POPATH=${PODIR}/inasafe.po
 #keep the current field separator
 oIFS=$IFS
 POFILES=$(egrep -r "import ugettext" . | cut -f 1 -d ':' | grep 'py$' | sort | uniq | tr '\n' ' ')
-echo $POFILES
+#echo $POFILES
 # double brackets deal gracefully if path has spaces
 if [[ ! -f $POPATH ]]
 then
