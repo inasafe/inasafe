d41ed503b7ff2eeda6f31f4b0b2defadfdd7f551
Tim Sutton <tim@linfiniti.com>
2012-08-05 23:07:33 +0200

    Use git datasource for tests rather than SVN. Note the Makefile is configured to run tests against a specific test data version. Adjust the makefile whenever you add / change data in the test data repo.

---
+++ scripts/update-test-data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Simple script to update the test data.
+
+if test -z "$1"
+then
+  echo "usage: $0 <git hash>"
+  echo "e.g. : $0 096fg5"
+  exit
+fi
+
+VERSION=$1
+
+echo "Updating and setting test data to revision: $VERSION"
+
+if [ ! -d ../inasafe-data ]
+then
+  # check the repo out since it does not exist
+  pushd .
+  cd ..
+  git clone git://github.com/AIFDR/inasafe-data.git inasafe_data
+  popd
+fi
+
+pushd .
+cd ../inasafe_data
+git fetch
+git checkout $VERSION
+popd
