2bf1ed13a17cb926cd2cf3c34c0d0b7d76edf6b2
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2012-08-10 11:40:40 +0700

    Made vector object work also for empty layers

--- storage/vector.py
+++ storage/vector.py
@@ -70,6 +70,9 @@ class Vector(Layer):
         If each entry is one set of coordinates the type will be ogr.wkbPoint,
         if it is an array of coordinates the type will be ogr.wkbPolygon.
 
+        To cast array entries as lines set geometry_type explicity to 'line' in
+        the call to Vector. Otherwise, they will default to polygons.
+
         Each polygon or line feature take the form of an Nx2 array representing
         vertices where line segments are joined
         """
@@ -475,6 +478,7 @@ class Vector(Layer):
 
         # Define attributes if any
         store_attributes = False
+        fields = []
         if data is not None:
             if len(data) > 0:
                 try:
@@ -497,9 +501,10 @@ class Vector(Layer):
                         ogrtypes[name] = TYPE_MAP[py_type]
 
             else:
-                msg = ('Input parameter "data" was specified '
-                       'but appears to be empty')
-                raise Exception(msg)
+                #msg = ('Input parameter "data" was specified '
+                #       'but appears to be empty')
+                #raise Exception(msg)
+                pass
 
             # Create attribute fields in layer
             store_attributes = True
