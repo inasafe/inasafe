9d8a2a9d33282f3e78653cdc179fac714fd203f6
Tim Sutton <tim@linfiniti.com>
2012-08-14 20:45:05 +0200

    Added runner script for realtime

---
+++ realtime/make_map.py
@@ -0,0 +1,39 @@
+"""
+InaSAFE Disaster risk assessment tool developed by AusAid and World Bank
+- **Functionality related to shake data files.**
+
+Contact : ole.moller.nielsen@gmail.com
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
+__author__ = 'tim@linfiniti.com'
+__version__ = '0.5.0'
+__date__ = '30/07/2012'
+__copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
+                 'Disaster Reduction')
+
+from shake_data import ShakeData
+
+# Used cached data where available
+myForceFlag = False
+# Get the latest dataset
+myShakeData = ShakeData()
+# Extract the event
+myShakeEvent = myShakeData.shakeEvent(theForceFlag=myForceFlag)
+print 'Latest Event Id: %s' % myShakeEvent
+print '-------------------------------------------'
+# Make contours
+for myAlgorithm in ['average', 'invdist', 'nearest']:
+    myFile = myShakeEvent.mmiDataToContours(theForceFlag=myForceFlag,
+                                   theAlgorithm=myAlgorithm)
+    print 'Created: %s' % myFile
+myFile = myShakeEvent.citiesToShape()
+print 'Created: %s' % myFile
+myFile = myShakeEvent.mmiDataToShapefile(theForceFlag=myForceFlag)
+print 'Created: %s' % myFile
+print '-------------------------------------------'
