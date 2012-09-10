b40b2c8c416a5d3e156016744bff31a4cc6d6ad7
Tim Sutton <tim@linfiniti.com>
2012-09-10 12:10:58 +0200

    Ide changes only

--- scripts/run-single-test.sh
+++ scripts/run-single-test.sh
@@ -5,6 +5,10 @@ echo "To use:"
 echo "$0 <packagename.modulename>"
 echo "e.g."
 echo "$0 gui.test_is_utilities"
-export QGISPATH=/usr/local/
-export PYTHONPATH=/usr/local/share/qgis/python/:`pwd`
-nosetests -v --with-id --with-coverage $1
+export QGISPATH=/usr/local/qgis-master
+export PYTHONPATH=/usr/local/qgis-master/share/qgis/python/:`pwd`
+export LD_LIBRARY_PATH=/usr/local/qgis-master/lib
+export QGIS_DEBUG=0
+export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
+export QGIS_DEBUG_FILE=/tmp/inasafe/realtime/logs/qgis-debug.log
+nosetests -v $1
