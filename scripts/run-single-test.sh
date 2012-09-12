bb9e7beb869e21f7dc2444881369d1660092a3dc
Marco Bernasocchi <marco@opengis.ch>
2012-09-12 16:11:51 +0700

    fixed pythonpath

--- scripts/run-single-test.sh
+++ scripts/run-single-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # A simple bash script to make it easy to run a single test
-# Note it assumes QGIS is in /usr/local
+# Note it defaults to QGIS in /usr/local
 echo "To use:"
 echo "$0 packagename.modulename [/path/to/qgis]"
 echo "e.g."
@@ -8,7 +8,7 @@ echo "$0 safe_qgis.test_utilities /usr/local"
 qgispath=${2-'/usr/local/'}
 echo "Setting QGISPATH to $qgispath..."
 export QGISPATH=$qgispath
-export PYTHONPATH=/usr/local/share/qgis/python/:`pwd`:$PYTHONPATH
+export PYTHONPATH=$QGISPATH/share/qgis/python/:`pwd`:$PYTHONPATH
 export QGIS_DEBUG=0;
 export QGIS_LOG_FILE=/dev/null;
 export QGIS_DEBUG_FILE=/dev/null;
