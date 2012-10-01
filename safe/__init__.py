653dd9a70553c9b43daf3e4de3af000b5ea0b538
Ariel Núñez <ingenieroariel@gmail.com>
2012-09-30 21:13:23 -0400

    Moved logging initialization code to safe_qgis/plugin. This makes importing safe have no side effects and is more friendly with django based logging. Fixes #298

--- safe/__init__.py
+++ safe/__init__.py
@@ -1,7 +1,1 @@
-from safe.common import utilities
 from safe.common.version import get_version
-
-# Note - the version string is obtained in safe.common.version
-#        and stored in metadata.txt
-
-utilities.setup_logger()
