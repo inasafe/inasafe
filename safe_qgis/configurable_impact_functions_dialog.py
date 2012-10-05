a0dfba2b8ad264e9bdd672c306f246f0f97838ce
Oscar Kurniawan <misugijunz@gmail.com>
2012-10-05 16:42:07 +0700

    adhering configurable impact functions dialog base to make space for tool description, changes save to ok

--- safe_qgis/configurable_impact_functions_dialog.py
+++ safe_qgis/configurable_impact_functions_dialog.py
@@ -1,90 +1,9 @@
-"""
-InaSAFE Disaster risk assessment tool developed by AusAid -
-**Impact Functions Dialog.**
+# -*- coding: utf-8 -*-
 
-Contact : ole.moller.nielsen@gmail.com
+# Form implementation generated from reading ui file 'configurable_impact_functions_dialog.ui'
+#
+# Created: Fri Oct  5 16:37:18 2012
+#      by: PyQt4 UI code generator 4.9.1
+#
+# WARNING! All changes made in this file will be lost!
 
-.. note:: This program is free software; you can redistribute it and/or modify
-     it under the terms of the GNU General Public License as published by
-     the Free Software Foundation; either version 2 of the License, or
-     (at your option) any later version.
-
-"""
-
-__author__ = 'oz@tanoshiistudio.com'
-__version__ = '0.5.0'
-__revision__ = '$Format:%H$'
-__date__ = '17/09/2012'
-__copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
-                 'Disaster Reduction')
-
-import ast
-from PyQt4 import (QtGui, QtCore)
-from configurable_impact_functions_dialog_base import (
-            Ui_configurableImpactFunctionsDialogBase)
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    _fromUtf8 = lambda s: s
-
-
-class ConfigurableImpactFunctionsDialog(QtGui.QDialog,
-                Ui_configurableImpactFunctionsDialogBase):
-    """ConfigurableImpactFunctions Dialog for InaSAFE."""
-
-    def __init__(self, theParent=None):
-        """Constructor for the dialog.
-
-                This dialog will show the user the form for editing
-                impact functions parameters if any.
-
-        Args:
-           * theParent - Optional widget to use as parent
-        Returns:
-           not applicable
-        Raises:
-           no exceptions explicitly raised
-        """
-        QtGui.QDialog.__init__(self, theParent)
-        self.setupUi(self)
-
-    def buildFormFromImpactFunctionsParameter(self, theFunction, params):
-        """we build a form from impact functions parameter
-
-        .. note:: see http://tinyurl.com/pyqt-differences
-
-        Args:
-           theFunction - theFunction to be modified
-           params - parameters to be edited
-        Returns:
-           not applicable
-        """
-        self.theFunction = theFunction
-        self.formItemCounters = 0
-        self.keys = params.keys()
-        for key in self.keys:
-            self._addFormItem(key, params[key])
-
-    def _addFormItem(self, key, data):
-        label = QtGui.QLabel(self.formLayoutWidget)
-        label.setObjectName(_fromUtf8(key + "Label"))
-        label.setText(key)
-        self.editableImpactFunctionsFormLayout.setWidget(self.formItemCounters,
-                                        QtGui.QFormLayout.LabelRole, label)
-        lineEdit = QtGui.QLineEdit(self.formLayoutWidget)
-        lineEdit.setText(str(data))
-        lineEdit.setObjectName(_fromUtf8(key + "LineEdit"))
-        self.editableImpactFunctionsFormLayout.setWidget(self.formItemCounters,
-                                        QtGui.QFormLayout.FieldRole, lineEdit)
-        self.formItemCounters += 1
-
-    def accept(self):
-        func = self.theFunction
-        for key in self.keys:
-            lineEdit = self.findChild(QtGui.QLineEdit,
-                                      _fromUtf8(key + "LineEdit"))
-            lineEditText = lineEdit.text()
-            convText = str(lineEditText)
-            func.parameters[key] = ast.literal_eval(convText)
-        self.close()
--- safe_qgis/configurable_impact_functions_dialog_base.py
+++ safe_qgis/configurable_impact_functions_dialog_base.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'configurable_impact_functions_dialog_base.ui'
 #
-# Created: Tue Sep 25 16:15:59 2012
+# Created: Fri Oct  5 16:37:50 2012
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -21,15 +21,19 @@ class Ui_configurableImpactFunctionsDialogBase(object):
         self.editableImpactFunctionsButtonBox = QtGui.QDialogButtonBox(configurableImpactFunctionsDialogBase)
         self.editableImpactFunctionsButtonBox.setGeometry(QtCore.QRect(30, 350, 541, 32))
         self.editableImpactFunctionsButtonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.editableImpactFunctionsButtonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Save)
+        self.editableImpactFunctionsButtonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.editableImpactFunctionsButtonBox.setObjectName(_fromUtf8("editableImpactFunctionsButtonBox"))
         self.formLayoutWidget = QtGui.QWidget(configurableImpactFunctionsDialogBase)
-        self.formLayoutWidget.setGeometry(QtCore.QRect(10, 10, 561, 331))
+        self.formLayoutWidget.setGeometry(QtCore.QRect(10, 80, 561, 261))
         self.formLayoutWidget.setObjectName(_fromUtf8("formLayoutWidget"))
         self.editableImpactFunctionsFormLayout = QtGui.QFormLayout(self.formLayoutWidget)
         self.editableImpactFunctionsFormLayout.setFieldGrowthPolicy(QtGui.QFormLayout.AllNonFixedFieldsGrow)
         self.editableImpactFunctionsFormLayout.setMargin(0)
         self.editableImpactFunctionsFormLayout.setObjectName(_fromUtf8("editableImpactFunctionsFormLayout"))
+        self.webViewConfImpFuncDesc = QtWebKit.QWebView(configurableImpactFunctionsDialogBase)
+        self.webViewConfImpFuncDesc.setGeometry(QtCore.QRect(10, 10, 561, 61))
+        self.webViewConfImpFuncDesc.setUrl(QtCore.QUrl(_fromUtf8("about:blank")))
+        self.webViewConfImpFuncDesc.setObjectName(_fromUtf8("webViewConfImpFuncDesc"))
 
         self.retranslateUi(configurableImpactFunctionsDialogBase)
         QtCore.QObject.connect(self.editableImpactFunctionsButtonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), configurableImpactFunctionsDialogBase.accept)
@@ -39,3 +43,4 @@ class Ui_configurableImpactFunctionsDialogBase(object):
     def retranslateUi(self, configurableImpactFunctionsDialogBase):
         configurableImpactFunctionsDialogBase.setWindowTitle(QtGui.QApplication.translate("configurableImpactFunctionsDialogBase", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
 
+from PyQt4 import QtWebKit
