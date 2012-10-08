3401c24b2a862aac8f536d72bf640ae9460079cf
Marco Bernasocchi <marco@opengis.ch>
2012-10-08 17:59:28 +0700

    renamed to getPostprocessingLayer

--- safe_qgis/dock.py
+++ safe_qgis/dock.py
@@ -623,7 +623,7 @@ class Dock(QtGui.QDockWidget, Ui_DockBase):
         # will be a lot of unneeded looping around as the signal is handled
         self.connectLayerListener()
         self.blockSignals(False)
-        self.getAggregationLayer()
+        self.getPostprocessingLayer()
         return
 
     def _toggleCboAggregation(self):
@@ -776,7 +776,7 @@ class Dock(QtGui.QDockWidget, Ui_DockBase):
         myLayer = QgsMapLayerRegistry.instance().mapLayer(myLayerId)
         return myLayer
 
-    def getAggregationLayer(self):
+    def getPostprocessingLayer(self):
         """Obtain the name of the path to the aggregation file from the
         userrole of the QtCombo for aggregation.
 
