f37b3a5035a0cc1050a6e40d970c70625376b465
Tim Sutton <tim@linfiniti.com>
2012-10-16 23:06:08 +0700

    Update hash

--- safe_qgis/test_map.py
+++ safe_qgis/test_map.py
@@ -115,7 +115,8 @@ class MapTest(unittest.TestCase):
                             '9a4ac96de64bbe1dda2616d01158913d',  # ub12.04-64
                             'ddf3cd2e9059e85c9d5b525d9f00c7dd',  # Jenkins
                             '003575ca10d3533a4ccf2f64d96c36a6',  # ub11.04-64
-                            '5fa1d7a318bbd88066b4b2c555d13a94',  # ub 12.04 5.02
+                            '5fa1d7a318bbd88066b4b2c555d13a94',  # ub 12.04 5.01
+                            '2390a6bd0571943ac89a65bb5fdfede5',  # ub 12.04 502
                             ]
         assertHashesForFile(myExpectedHashes, myImagePath)
 
