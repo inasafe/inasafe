25a5d44823145574c0b0ea30eb8cdedc5b8d00e3
Tim Sutton <tim@linfiniti.com>
2012-10-17 18:16:44 +0700

    Use standard header for map test

--- safe_qgis/test_map.py
+++ safe_qgis/test_map.py
@@ -1,20 +1,13 @@
-"""
-InaSAFE Disaster risk assessment tool developed by AusAid and World Bank
-- **GUI Test Cases.**
-
-Contact : ole.moller.nielsen@gmail.com
-
-.. note:: This program is free software; you can redistribute it and/or modify
-     it under the terms of the GNU General Public License as published by
-     the Free Software Foundation; either version 2 of the License, or
-     (at your option) any later version.
+"""**Tests for map creation in QGIS plugin.**
 
 """
-__author__ = 'tim@linfiniti.com'
-__version__ = '1.0.0'
-__date__ = '10/01/2011'
-__copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
-                 'Disaster Reduction')
+
+__author__ = 'Tim Sutton <tim@linfiniti.com>'
+__revision__ = '$Format:%H$'
+__date__ = '01/11/2010'
+__license__ = "GPL"
+__copyright__ = 'Copyright 2012, Australia Indonesia Facility for '
+__copyright__ += 'Disaster Reduction'
 
 import unittest
 from unittest import expectedFailure
