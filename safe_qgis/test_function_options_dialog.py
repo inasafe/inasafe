132734ab387693453ced1f2267a211650f25d161
Oscar Kurniawan <misugijunz@gmail.com>
2012-10-18 15:01:54 +0700

    add some test imports for test_function_options ~still lots of pylint violation of unused imports

--- safe_qgis/test_function_options_dialog.py
+++ safe_qgis/test_function_options_dialog.py
@@ -17,17 +17,73 @@ __date__ = '15/10/2012'
 __copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
                  'Disaster Reduction')
 import unittest
+import sys
+import os
+import logging
+#from unittest import expectedFailure
+
+from os.path import join
+# Add PARENT directory to path to make test aware of other modules
+pardir = os.path.abspath(join(os.path.dirname(__file__), '..'))
+sys.path.append(pardir)
+
+from PyQt4 import QtCore
+from PyQt4.QtTest import QTest
+
+from safe.common.testing import HAZDATA, EXPDATA, TESTDATA, UNITDATA
+
+from safe_qgis.utilities_test import (getQgisTestApp,
+                                setCanvasCrs,
+                                setPadangGeoExtent,
+                                setBatemansBayGeoExtent,
+                                setJakartaGeoExtent,
+                                setYogyaGeoExtent,
+                                setJakartaGoogleExtent,
+                                setGeoExtent,
+                                GEOCRS,
+                                GOOGLECRS,
+                                loadLayer)
+
+from safe_qgis.dock import Dock
+from safe_qgis.utilities import (setRasterStyle,
+                          qgisVersion,
+                          getDefaults)
+
+
+# pylint: enable=W0611
+LOGGER = logging.getLogger('InaSAFE')
+
+QGISAPP, CANVAS, IFACE, PARENT = getQgisTestApp()
+DOCK = Dock(IFACE)
+
+
+from safe.engine.impact_functions_for_testing.itb_fatality_model_configurable\
+    import ITBFatalityFunctionConfigurable
 
 
 class FunctionOptionsDialogTest(unittest.TestCase):
+    """Test the InaSAFE GUI for Configurable Impact Functions"""
 
     def setUp(self):
+        """Fixture run before all tests"""
+        os.environ['LANG'] = 'en'
+        pass
+
+    def tearUp(self):
+        """Fixture run before each test"""
         pass
 
     def tearDown(self):
+        """Fixture run after each test"""
         pass
 
-    def testName(self):
+    def test_defaults(self):
+        pass
+
+    def test_hasParametersButtonEnabled(self):
+        pass
+
+    def test_noParametersButtonDisabled(self):
         pass
 
 
