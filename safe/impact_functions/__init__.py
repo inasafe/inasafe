26fbd9d1f6e9b9d12f845b9d886a4b5b300c5a3b
Ismail Sunni <imajimatika@gmail.com>
2012-11-09 16:30:54 +0700

    set api

--- safe/impact_functions/__init__.py
+++ safe/impact_functions/__init__.py
@@ -23,3 +23,4 @@ from safe.impact_functions.core import get_plugin
 from safe.impact_functions.core import get_admissible_plugins
 from safe.impact_functions.core import compatible_layers
 from safe.impact_functions.core import get_function_title
+from safe.impact_functions.core import get_documentation
