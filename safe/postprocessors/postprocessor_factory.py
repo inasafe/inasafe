0e1536665c9c79e25db80d04e5249d7062a8aac0
Marco Bernasocchi <marco@opengis.ch>
2012-11-14 17:30:51 +0700

    added get_postprocessor_human_name

--- safe/postprocessors/postprocessor_factory.py
+++ safe/postprocessors/postprocessor_factory.py
@@ -2,8 +2,8 @@
 """**Postprocessors package.**
 
 .. tip::
-   import like this from safe.postprocessors import get_post_processors and
-   then call get_post_processors(requested_postprocessors)
+   import like this from safe.postprocessors import get_postprocessors and
+   then call get_postprocessors(requested_postprocessors)
 
 """
 
@@ -20,15 +20,19 @@ import logging
 from gender_postprocessor import GenderPostprocessor
 from age_postprocessor import AgePostprocessor
 from aggregation_postprocessor import AggregationPostprocessor
-from building_type_postprocessor import BuildingTypePostprocessor
+from osm_building_type_postprocessor import OSMBuildingTypePostprocessor
 # pylint: enable=W0611
 
 LOGGER = logging.getLogger('InaSAFE')
 #this _must_reflect the imported classes above
-AVAILABLE_POSTPTOCESSORS = ['Gender', 'Age', 'Aggregation', 'BuildingType']
+AVAILABLE_POSTPTOCESSORS = {'Gender': 'Gender',
+                            'Age': 'Age',
+                            'Aggregation': 'Aggregation',
+                            'OSMBuildingType': 'Open Street Map building type'
+                            }
 
 
-def get_post_processors(requested_postprocessors):
+def get_postprocessors(requested_postprocessors):
     """
     Creates a dictionary of applicable postprocessor instances
 
@@ -63,7 +67,7 @@ def get_post_processors(requested_postprocessors):
         constr_id = name + 'Postprocessor'
         try:
             if values['on']:
-                if name in AVAILABLE_POSTPTOCESSORS:
+                if name in AVAILABLE_POSTPTOCESSORS.keys():
                     #http://stackoverflow.com/a/554462
                     constr = globals()[constr_id]
                     instance = constr()
@@ -78,3 +82,16 @@ def get_post_processors(requested_postprocessors):
             LOGGER.debug(constr_id + ' has no "on" key, skipping it')
 
     return postprocessor_instances
+
+
+def get_postprocessor_human_name(postprocesor):
+    """
+    Returns the human readable name of  post processor
+
+    Args:
+        * postprocessor: Machine name of the postprocessor
+
+    Returns:
+        str with the human readable name
+    """
+    return AVAILABLE_POSTPTOCESSORS[postprocesor]
