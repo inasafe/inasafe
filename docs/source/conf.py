f36e05bb6e97276d33a08236097023becadc2aca
Tim Sutton <tim@linfiniti.com>
2012-11-23 13:41:59 +0200

    Fix module path for autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,13 +16,16 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('../..'))
 
 # -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.pngmath']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
+              'sphinx.ext.coverage', 'sphinx.ext.viewcode',
+              'sphinx.ext.pngmath']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['../templates']
@@ -131,7 +134,7 @@ html_short_title = 'InsSAFE Docs'
 # The name of an image file (within the /static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'img/favicon.ico'
 
 # Add any paths that contain custom /static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin /static files,
