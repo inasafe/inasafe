8d1630debfe32da765086468bc280c241cd36319
Ismail Sunni <imajimatika@gmail.com>
2012-12-07 16:52:11 +0700

    initial commit

--- safe/storage/test_utilities.py
+++ safe/storage/test_utilities.py
@@ -120,5 +120,6 @@ class CommonUtilitiesTest(unittest.TestCase):
         self.assertEquals(keywords, expected_keywords, msg)
         LOGGER.debug(keywords)
 
+    def test
 if __name__ == '__main__':
     unittest.main()
