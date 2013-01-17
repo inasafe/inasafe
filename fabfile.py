1352546f82a295e3a772ed91c6e227340e46881e
Tim Sutton <tim@linfiniti.com>
2013-01-17 11:57:57 +0200

    Whole package deployment automated and tested

--- fabfile.py
+++ fabfile.py
@@ -9,7 +9,7 @@ from fabric.contrib.files import contains, exists, append, sed
 
 # Usage fab localhost [command]
 #    or fab remote [command]
-#  e.g. fab localhost initialise_repo
+#  e.g. fab localhost update_qgis_plugin_repo
 
 # Global fabric settings
 
@@ -21,7 +21,7 @@ def localhost():
     """Set up things so that commands run locally."""
     env.run = captured_local
     env.hosts = ['localhost']
-    all()
+    _all()
 
 def remote():
     """Set up things so that commands run remotely.
@@ -31,9 +31,9 @@ def remote():
 
     """
     env.run = run
-    all()
+    _all()
 
-def all():
+def _all():
     """Things to do regardless of whether command is local or remote."""
     site_names = {
         'waterfall': 'inasafe-nightly.localhost',
@@ -46,22 +46,31 @@ def all():
             exit
         else:
             env.site_name = site_names[env.hostname]
-            env.dest_path = '/home/web/inasafe-nightly'
+            env.plugin_repo_path = '/home/web/inasafe-nightly'
+            env.home = os.path.join('/home/', env.user)
+            env.repo_path = os.path.join(env.home,
+                                         'dev',
+                                         'python')
+            env.git_url = 'git://github.com/AIFDR/inasafe.git'
+            env.repo_alias = 'inasafe-nightly'
             show_environment()
 
 ###############################################################################
-# Next section contains actual tasks
+# Next section contains helper methods tasks
 ###############################################################################
 
-def initialise_repo():
-    """Initialise a local repo for nightly builds"""
+def update_qgis_plugin_repo():
+    """Initialise a QGIS plugin repo where we host nightly builds."""
+    code_path = os.path.join(env.repo_path, env.repo_alias)
+    local_path = '%s/scripts/nightly-build-repo' % code_path
 
-    fab_path = os.path.dirname(__file__)
-    local_path = '%s/scripts/nightly-build-repo' % fab_path
-    local('mkdir -p %s' % env.dest_path)
-    freshen_repo()
+    if not exists(env.plugin_repo_path):
+        sudo('mkdir -p %s' % env.plugin_repo_path)
+        sudo('chown %s.%s %s' % (env.user, env.user, env.plugin_repo_path))
 
-    local('cp %(local_path)s/inasafe-nightly.conf.templ '
+    env.run('cp %s/plugin* %s' % (local_path, env.plugin_repo_path))
+    env.run('cp %s/icon* %s' % (code_path, env.plugin_repo_path))
+    env.run('cp %(local_path)s/inasafe-nightly.conf.templ '
         '%(local_path)s/inasafe-nightly.conf' % {'local_path': local_path})
 
     sed('%s/inasafe-nightly.conf' % local_path,
@@ -76,7 +85,7 @@ def initialise_repo():
 
         sudo('ln -s %s/inasafe-nightly.conf .' % local_path)
 
-    # Add a hosts entry for local testing
+    # Add a hosts entry for local testing - only really useful for localhost
     hosts = '/etc/hosts'
     if not contains(hosts, 'inasafe-nightly'):
         append(hosts, '127.0.0.1 %s' % env.site_name, use_sudo=True)
@@ -85,71 +94,73 @@ def initialise_repo():
     sudo('service apache2 reload')
 
 
-def freshen_git():
-    """Make sure there is a read only git checkout."""
+def update_git_checkout():
+    """Make sure there is a read only git checkout.
 
-    git_url = 'git://github.com/AIFDR/inasafe.git'
-    
+    To run e.g.::
 
-
-def freshen_repo():
-    """Copy all content files from git repo to web repo.
-
-    .. note:: You should have run initialise_repo at least once first.
+        fab -H 188.40.123.80:8697 remote update_git_checkout
 
     """
-    fab_path = os.path.dirname(__file__)
-    local_path = '%s/scripts/nightly-build-repo' % fab_path
 
-    if exists(env.dest_path):
-        put('%s/plugin*' % local_path, env.dest_path)
-        put('%s/icon*' % local_path, env.dest_path)
+    if not exists(os.path.join(env.repo_path, env.repo_alias)):
+        fastprint('Repo checkout does not exist, creating.')
+        env.run('mkdir -p %s' % (env.repo_path))
+        with cd(env.repo_path):
+            clone = env.run('git clone %s %s' % (env.git_url, env.repo_alias))
     else:
-        fastprint('Repo does not exist - run initialise_repo first')
+        fastprint('Repo checkout does exist, updating.')
+        with cd(os.path.join(env.repo_path, env.repo_alias)):
+            clone = env.run('git reset --hard')
+            clone = env.run('git checkout master')
+            clone = env.run('git pull')
 
+###############################################################################
+# Next section contains actual tasks
+###############################################################################
 
 def build_nightly():
-    """Create a nightly package and publish it in our repo."""
-    dir_name = os.path.dirname(__file__)
-    dir_name = os.path.split(dir_name)[-1]
-    fastprint('Dir is: %s' % dir_name)
-    # Get git version and write it to a text file in case we need to cross
-    # references it for a user ticket.
-    sha = local('git rev-parse HEAD', capture=True)
-    fastprint('Git revision: %s' % sha)
-    sha_path = 'git_revision.txt'
-    sha_file = file(sha_path, 'wt')
-    sha_file.write(sha)
-    sha_file.close()
+    """Create a nightly package and publish it in our repo.
 
-    freshen_repo()
+    To run e.g.::
 
-    metadata_file = file('metadata.txt', 'rt')
-    metadata_text = metadata_file.readlines()
-    metadata_file.close()
-    for line in metadata_text:
-        line = line.rstrip()
-        if 'version=' in line:
-            plugin_version = line.replace('version=', '')
-        if 'status=' in line:
-            status = line.replace('status=', '')
+        fab -H 188.40.123.80:8697 remote build_nightly
+    """
 
-    local('scripts/release.sh %s' % plugin_version)
-    package_name = '%s.%s.zip' % ('inasafe', plugin_version)
-    source = '/tmp/%s' % package_name
-    fastprint('Source: %s' % source)
-    put(source, env.dest_path)
+    update_git_checkout()
+    update_qgis_plugin_repo()
 
-    plugins_xml = os.path.join(env.dest_path, 'plugins.xml')
-    sed(plugins_xml, '\[VERSION\]', plugin_version)
-    sed(plugins_xml, '\[FILE_NAME\]', package_name)
-    sed(plugins_xml, '\[URL\]', 'http://%s/%s' % (env.site_name, package_name))
-    sed(plugins_xml, '\[DATE\]', str(datetime.now()))
+    dir_name = os.path.join(env.repo_path, env.repo_alias)
+    with cd(dir_name):
+        # Get git version and write it to a text file in case we need to cross
+        # reference it for a user ticket.
+        sha = env.run('git rev-parse HEAD > git_revision.txt')
+        fastprint('Git revision: %s' % sha)
 
-    os.remove(sha_path)
+        metadata_file = file('metadata.txt', 'rt')
+        metadata_text = metadata_file.readlines()
+        metadata_file.close()
+        for line in metadata_text:
+            line = line.rstrip()
+            if 'version=' in line:
+                plugin_version = line.replace('version=', '')
+            if 'status=' in line:
+                status = line.replace('status=', '')
 
-    fastprint('Add http://%s/plugins.xml to QGIS plugin manager to use this.'
-        % sha)
+        env.run('scripts/release.sh %s' % plugin_version)
+        package_name = '%s.%s.zip' % ('inasafe', plugin_version)
+        source = '/tmp/%s' % package_name
+        fastprint('Source: %s' % source)
+        env.run('cp %s %s' % (source, env.plugin_repo_path))
+
+        plugins_xml = os.path.join(env.plugin_repo_path, 'plugins.xml')
+        sed(plugins_xml, '\[VERSION\]', plugin_version)
+        sed(plugins_xml, '\[FILE_NAME\]', package_name)
+        sed(plugins_xml, '\[URL\]', 'http://%s/%s' % (env.site_name, package_name))
+        sed(plugins_xml, '\[DATE\]', str(datetime.now()))
+
+        fastprint('Add http://%s/plugins.xml to QGIS plugin manager to use this.'
+            % env.site_name)
 
 def show_environment():
     """For diagnostics - show any pertinent info about server."""
@@ -157,5 +168,9 @@ def show_environment():
     fastprint('User: %s\n' % env.user)
     fastprint('Host: %s\n' % env.hostname)
     fastprint('Site Name: %s\n' % env.site_name)
-    fastprint('Dest Path: %s\n' % env.dest_path)
+    fastprint('Dest Path: %s\n' % env.plugin_repo_path)
+    fastprint('Home Path: %s\n' % env.home)
+    fastprint('Repo Path: %s\n' % env.repo_path)
+    fastprint('Git Url: %s\n' % env.git_url)
+    fastprint('Repo Alias: %s\n' % env.repo_alias)
     fastprint('-------------------------------------------------\n')
