7f3c3d6960652a8619aa114d336d5fd63e24e7f8
Ismail Sunni <imajimatika@gmail.com>
2013-02-12 10:56:46 +0700

    Add UI for converter grid to raster.

---
+++ safe_qgis/converter_dialog.py
@@ -0,0 +1,20 @@
+"""
+InaSAFE Disaster risk assessment tool developed by AusAid -
+**Converter Dialog.**
+
+Contact : ole.moller.nielsen@gmail.com
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
+__author__ = 'imajimatika@gmail.com'
+__revision__ = '$Format:%H$'
+__date__ = '12/2/2013'
+__license__ = "GPL"
+__copyright__ = 'Copyright 2013, Australia Indonesia Facility for '
+__copyright__ += 'Disaster Reduction'
+
---
+++ safe_qgis/converter_dialog_base.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'converter_dialog_base.ui'
+#
+# Created: Tue Feb 12 10:55:19 2013
+#      by: PyQt4 UI code generator 4.9.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_ConverterDialogBase(object):
+    def setupUi(self, ConverterDialogBase):
+        ConverterDialogBase.setObjectName(_fromUtf8("ConverterDialogBase"))
+        ConverterDialogBase.resize(399, 342)
+        self.buttonBox = QtGui.QDialogButtonBox(ConverterDialogBase)
+        self.buttonBox.setGeometry(QtCore.QRect(10, 290, 381, 32))
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.label_1 = QtGui.QLabel(ConverterDialogBase)
+        self.label_1.setGeometry(QtCore.QRect(10, 10, 381, 34))
+        self.label_1.setWordWrap(True)
+        self.label_1.setObjectName(_fromUtf8("label_1"))
+        self.lblInput = QtGui.QLabel(ConverterDialogBase)
+        self.lblInput.setEnabled(True)
+        self.lblInput.setObjectName(_fromUtf8("lblInput"))
+        self.leInputPath = QtGui.QLineEdit(ConverterDialogBase)
+        self.leInputPath.setEnabled(True)
+        self.leInputPath.setGeometry(QtCore.QRect(10, 90, 351, 27))
+        self.leInputPath.setObjectName(_fromUtf8("leInputPath"))
+        self.tBtnOpenInput = QtGui.QToolButton(ConverterDialogBase)
+        self.tBtnOpenInput.setEnabled(True)
+        self.tBtnOpenInput.setGeometry(QtCore.QRect(370, 90, 23, 25))
+        self.tBtnOpenInput.setObjectName(_fromUtf8("tBtnOpenInput"))
+        self.lblOutput = QtGui.QLabel(ConverterDialogBase)
+        self.lblOutput.setEnabled(True)
+        self.lblOutput.setObjectName(_fromUtf8("lblOutput"))
+        self.tBtnOpenOuput = QtGui.QToolButton(ConverterDialogBase)
+        self.tBtnOpenOuput.setEnabled(True)
+        self.tBtnOpenOuput.setGeometry(QtCore.QRect(370, 180, 23, 25))
+        self.tBtnOpenOuput.setObjectName(_fromUtf8("tBtnOpenOuput"))
+        self.leOutputPath = QtGui.QLineEdit(ConverterDialogBase)
+        self.leOutputPath.setEnabled(True)
+        self.leOutputPath.setGeometry(QtCore.QRect(10, 180, 351, 27))
+        self.leOutputPath.setObjectName(_fromUtf8("leOutputPath"))
+        self.cBDefaultOuputLocation = QtGui.QCheckBox(ConverterDialogBase)
+        self.cBDefaultOuputLocation.setObjectName(_fromUtf8("cBDefaultOuputLocation"))
+        self.cBLoadLayer = QtGui.QCheckBox(ConverterDialogBase)
+        self.cBLoadLayer.setObjectName(_fromUtf8("cBLoadLayer"))
+
+        self.retranslateUi(ConverterDialogBase)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), ConverterDialogBase.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), ConverterDialogBase.reject)
+        QtCore.QMetaObject.connectSlotsByName(ConverterDialogBase)
+
+    def retranslateUi(self, ConverterDialogBase):
+        ConverterDialogBase.setWindowTitle(QtGui.QApplication.translate("ConverterDialogBase", "InaSAFE - Converter", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_1.setText(QtGui.QApplication.translate("ConverterDialogBase", "Convert raw gird file (.xml) to raster file (.tif). Please select your raw grid file:", None, QtGui.QApplication.UnicodeUTF8))
+        self.lblInput.setText(QtGui.QApplication.translate("ConverterDialogBase", "Input : Grid file", None, QtGui.QApplication.UnicodeUTF8))
+        self.tBtnOpenInput.setText(QtGui.QApplication.translate("ConverterDialogBase", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.lblOutput.setText(QtGui.QApplication.translate("ConverterDialogBase", "Output : Raster file", None, QtGui.QApplication.UnicodeUTF8))
+        self.tBtnOpenOuput.setText(QtGui.QApplication.translate("ConverterDialogBase", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.cBDefaultOuputLocation.setText(QtGui.QApplication.translate("ConverterDialogBase", "Use default location", None, QtGui.QApplication.UnicodeUTF8))
+        self.cBLoadLayer.setText(QtGui.QApplication.translate("ConverterDialogBase", "Load output file to QGIS", None, QtGui.QApplication.UnicodeUTF8))
+
