36cc7d36b5565ed47c8fa183082dc6c4faa35c33
Ismail Sunni <imajimatika@gmail.com>
2013-02-22 13:27:28 +0700

    change qgis-master to realtime for flood forecast. *before i forget

--- scripts/make-all-flood-forecasts.sh
+++ scripts/make-all-flood-forecasts.sh
@@ -3,10 +3,10 @@
 export QGIS_DEBUG=0
 export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
 export QGIS_DEBUG_FILE=/tmp/inasafe/realtime/logs/qgis-debug.log
-export QGIS_PREFIX_PATH=/usr/local/qgis-master/
-# export PYTHONPATH=/usr/local/qgis-master/share/qgis/python/:`pwd`
+export QGIS_PREFIX_PATH=/usr/local/qgis-realtime/
+# export PYTHONPATH=/usr/local/qgis-realtime/share/qgis/python/:`pwd`
 export PYTHONPATH=~/Documents/inasafe/inasafe-dev:`pwd`
-export LD_LIBRARY_PATH=/usr/local/qgis-master/lib
+export LD_LIBRARY_PATH=/usr/local/qgis-realtime/lib
 
 export INASAFE_LOCALE=id
 cd /home/sunnii/Documents/inasafe/inasafe-dev/
