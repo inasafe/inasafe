bc7cbe2f4b2d15975683a6ca2e9ef952c0edd3e8
Ismail Sunni <imajimatika@gmail.com>
2013-03-07 10:39:36 +0700

    Add documentation to ITBFatalityFunction.

--- safe/impact_functions/earthquake/itb_earthquake_fatality_model.py
+++ safe/impact_functions/earthquake/itb_earthquake_fatality_model.py
@@ -87,6 +87,48 @@ class ITBFatalityFunction(FunctionProvider):
     """
 
     title = tr('Die or be displaced')
+    synopsis = tr('To asses the impact of earthquake on population based on '
+                  'earthquake model developed by ITB')
+    citations = [tr('Indonesian Earthquake Building-Damage and Fatality '
+                    'Models and Post Disaster Survey Guidelines Development '
+                    'Bali, 27-28 February 2012, 54pp.'),
+                 tr('Allen, T. I., Wald, D. J., Earle, P. S., Marano, K. D., '
+                    'Hotovec, A. J., Lin, K., and Hearne, M., 2009. An Atlas '
+                    'of ShakeMaps and population exposure catalog for '
+                    'earthquake loss modeling, Bull. Earthq. Eng. 7, '
+                    '701-718.'),
+                 tr('Jaiswal, K., and Wald, D., 2010. An empirical model for '
+                    'global earthquake fatality estimation, Earthq. Spectra '
+                    '26, 1017-1037.')]
+    limitations = [tr('The model is based on limited number of observed '
+                      'fatality rates during 4 past fatal events.'),
+                   tr('The model clearly over-predicts the fatality rates at'
+                      'intensities higher than VIII.'),
+                   tr('The model only estimates the expected fatality rate '
+                      'for a given intensity level; however the associated '
+                      'uncertainty for the proposed model is not addressed.'),
+                   tr('There are few known mistakes in developing the current '
+                      'model:\n'
+                      '- rounding MMI values to the nearest 0.5,\n'
+                      '- Implementing Finite-Fault models of candidate '
+                      'events, and\n'
+                      '- consistency between selected GMPEs with those in '
+                      'use by BMKG.')]
+    actions = tr('Provide details about the population will be die or '
+                 'displaced')
+    detailed_description = \
+        tr('This model was developed by Institut Tecknologi Bandung (ITB) '
+           'and implemented by Dr Hadi Ghasemi, Geoscience Australia\n'
+           'Algorithm:\n'
+           'In this study, the same functional form as Allen (2009) is '
+           'adopted o express fatality rate as a function of intensity '
+           '(see Eq. 10 in the report). The Matlab built-in function '
+           '(fminsearch) for  Nelder-Mead algorithm was used to estimate '
+           'the model parameters. The objective function (L2G norm) that '
+           'is minimized during the optimisation is the same as the one '
+           'used by Jaiswal et al. (2010).\n'
+           'The coefficients used in the indonesian model are x=0.62275231, '
+           'y=8.03314466, zeta=2.15')
     defaults = get_defaults()
     parameters = OrderedDict([
         ('x', 0.62275231), ('y', 8.03314466),  # Model coefficients
