ccd4c1a6f622bd4052fd04b055e05c1b86a36365
Tim Sutton <tim@linfiniti.com>
2013-03-11 09:57:36 +0200

    Added which utility from twisted for #529

--- safe_qgis/utilities.py
+++ safe_qgis/utilities.py
@@ -1196,7 +1196,7 @@ def isLayerPolygonal(theLayer):
 
 
 def isLayerPoint(theLayer):
-    """Tell if a QGIS layer is vector and its geometries are polygons.
+    """Tell if a QGIS layer is vector and its geometries are points.
 
    Args:
        the theLayer
@@ -1212,3 +1212,44 @@ def isLayerPoint(theLayer):
             theLayer.geometryType() == QGis.Point)
     except AttributeError:
         return False
+
+
+def which(name, flags=os.X_OK):
+    """Search PATH for executable files with the given name.
+
+    ..note:: This function was taken verbatim from the twisted framework,
+      licence available here:
+      http://twistedmatrix.com/trac/browser/tags/releases/twisted-8.2.0/LICENSE
+
+    On newer versions of MS-Windows, the PATHEXT environment variable will be
+    set to the list of file extensions for files considered executable. This
+    will normally include things like ".EXE". This fuction will also find files
+    with the given name ending with any of these extensions.
+
+    On MS-Windows the only flag that has any meaning is os.F_OK. Any other
+    flags will be ignored.
+
+    @type name: C{str}
+    @param name: The name for which to search.
+
+    @type flags: C{int}
+    @param flags: Arguments to L{os.access}.
+
+    @rtype: C{list}
+    @param: A list of the full paths to files found, in the
+    order in which they were found.
+    """
+    result = []
+    exts = filter(None, os.environ.get('PATHEXT', '').split(os.pathsep))
+    path = os.environ.get('PATH', None)
+    if path is None:
+        return []
+    for p in os.environ.get('PATH', '').split(os.pathsep):
+        p = os.path.join(p, name)
+        if os.access(p, flags):
+            result.append(p)
+        for e in exts:
+            pext = p + e
+            if os.access(pext, flags):
+                result.append(pext)
+    return result
