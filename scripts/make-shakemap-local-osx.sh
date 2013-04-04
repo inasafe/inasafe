8469fad5bad1731c07ac61b014161645f667684e
Tim Sutton <tim@linfiniti.com>
2013-04-04 20:57:25 +0200

    Tweaked local shake script to work with Kristy's generated pop dataset

--- scripts/make-shakemap-local-osx.sh
+++ scripts/make-shakemap-local-osx.sh
@@ -16,6 +16,7 @@ export QGIS_DEBUG_FILE=/tmp/inasafe/realtime/logs/qgis-debug.log
 
 
 export INASAFE_WORK_DIR=/tmp/quake
+export INASAFE_POPULATION_PATH=`pwd`/realtime/fixtures/exposure/pop_density_25m2_comLZW.tif
 export INASAFE_POPULATION_PATH=`pwd`/realtime/fixtures/exposure/population.tif
 export INASAFE_LOCALE=id
 
