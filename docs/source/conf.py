1cf953810f7102b259d8b22736835c06e2afe94d
Tim Sutton <tim@linfiniti.com>
2013-04-09 08:12:48 +0700

    Dont try to use rst2pdf builder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ sys.path.append(os.path.abspath('../..'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
               'sphinx.ext.coverage', 'sphinx.ext.viewcode',
-              'sphinx.ext.pngmath', 'rst2pdf.pdfbuilder']
+              'sphinx.ext.pngmath' ]
 
 
 # Add any paths that contain templates here, relative to this directory.
