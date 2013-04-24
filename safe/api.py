d55dec92a6e00d80ecad1179863552bea832ba47
Ismail Sunni <imajimatika@gmail.com>
2013-04-24 10:18:04 +0700

    Add try exception for import related to testing module.

--- safe/api.py
+++ safe/api.py
@@ -61,5 +61,9 @@ from safe.common.polygon import in_and_outside_polygon
 from safe.common.tables import Table, TableCell, TableRow
 from safe.postprocessors import (get_postprocessors,
                                  get_postprocessor_human_name)
-from safe.common.testing import HAZDATA, EXPDATA, TESTDATA, UNITDATA, BOUNDDATA
+try:
+    from safe.common.testing import (
+        HAZDATA, EXPDATA, TESTDATA, UNITDATA, BOUNDDATA)
+except ImportError:
+    pass
 # pylint: enable=W0611
