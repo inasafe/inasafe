176d2636658e3c457575406480e38d77551441e8
Tim Sutton <tim@linfiniti.com>
2013-05-24 15:26:14 +0200

    Added message class stubs

---
+++ safe/messaging/table.py
@@ -0,0 +1,26 @@
+"""
+InaSAFE Disaster risk assessment tool developed by AusAid - **Table Module.**
+
+Contact : ole.moller.nielsen@gmail.com
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+"""
+
+__author__ = 'tim@linfiniti.com'
+__revision__ = '$Format:%H$'
+__date__ = '24/05/2013'
+__copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
+                 'Disaster Reduction')
+
+import logging
+
+
+LOGGER = logging.getLogger('InaSAFE')
+#from pydev import pydevd
+
+
+class Table():
+    pass
