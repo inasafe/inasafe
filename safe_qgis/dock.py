54f058d44a8cc844c730920db082c9a695ccecb2
Tim Sutton <tim@linfiniti.com>
2013-06-07 14:33:07 +0200

    Css updates and added logo on welcome screen

--- safe_qgis/dock.py
+++ safe_qgis/dock.py
@@ -92,7 +92,7 @@ PROGRESS_UPDATE_STYLE = styles.PROGRESS_UPDATE_STYLE
 INFO_STYLE = styles.INFO_STYLE
 WARNING_STYLE = styles.WARNING_STYLE
 KEYWORD_STYLE = styles.KEYWORD_STYLE
-
+LOGO_ELEMENT = m.Image('qrc:/plugins/inasafe/logo.svg')
 LOGGER = logging.getLogger('InaSAFE')
 
 #from pydev import pydevd
@@ -411,6 +411,7 @@ class Dock(QtGui.QDockWidget, Ui_DockBase):
                 to get started.
         """
         myMessage = m.Message()
+        myMessage.add(LOGO_ELEMENT)
         myMessage.add(m.Heading('Getting started', **INFO_STYLE))
         myNotes = m.Paragraph(
             self.tr(
