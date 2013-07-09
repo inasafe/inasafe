49359946a5d84e31cba6d98c889cf3488487da7d
Marco Bernasocchi <marco@opengis.ch>
2013-07-09 09:38:32 +0200

    close #589

--- safe_qgis/dock.py
+++ safe_qgis/dock.py
@@ -1076,7 +1076,7 @@ class Dock(QtGui.QDockWidget, Ui_DockBase):
         # prompt for them. if a prompt is shown run method is called by the
         # accepted signal of the keywords dialog
         self.aggregator.validateKeywords()
-        if self.aggregator.aoiMode and self.aggregator.isValid:
+        if self.aggregator.isValid:
             self.run()
         else:
             self.runtimeKeywordsDialog.setLayer(self.aggregator.layer)
