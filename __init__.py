c5c166f8ea8898b1dfd473e881af54a7e3370a4c
Alexander Bruy <alexander.bruy@gmail.com>
2013-07-24 11:20:16 +0300

    cleanup __init__.py and update metadata.txt

--- __init__.py
+++ __init__.py
@@ -17,48 +17,11 @@ __author__ = 'tim@linfiniti.com'
 __date__ = '10/01/2011'
 __copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
                  'Disaster Reduction')
-import os
 
 #
 # Note: Don't use function docstrings more than one line - they
 #       break the plugins.qgis.org validator when uploading!
 #
-
-
-def name():
-    """A user friendly name for the plugin."""
-    return 'InaSAFE'
-
-
-def author():
-    """Author name."""
-    return 'BNPB, AusAID and the World Bank'
-
-
-def email():
-    """Email contact details."""
-    return 'ole.moller.nielsen@gmail.com'
-
-
-def description():
-    """A one line description for the plugin."""
-    return ('InaSAFE Disaster Scenario Assessment for Emergencies'
-            ' tool developed by BNPB, AusAID, World Bank')
-
-
-def version():
-    """Version of the plugin."""
-    return 'Version 1.2.0-8'
-
-
-def qgisMinimumVersion():
-    """Minimum version of QGIS needed to run this plugin."""
-    return '1.7'
-
-
-def icon():
-    """Icon path for the plugin - metadata.txt will override this."""
-    return 'icon.png'
 
 
 # noinspection PyDocstring
