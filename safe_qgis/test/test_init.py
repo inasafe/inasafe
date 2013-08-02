7913593a149fe33f8287ec09475de8047662ed8f
Alexander Bruy <alexander.bruy@gmail.com>
2013-08-02 13:27:02 +0300

    update test_init.py to validate only metadata.txt

--- safe_qgis/test/test_init.py
+++ safe_qgis/test/test_init.py
@@ -10,9 +10,9 @@ __copyright__ = 'Copyright 2012, Australia Indonesia Facility for '
 __copyright__ += 'Disaster Reduction'
 
 import os
-import re
 import unittest
 import logging
+import ConfigParser
 
 LOGGER = logging.getLogger('InaSAFE')
 
@@ -44,43 +44,21 @@ class TestInit(unittest.TestCase):
 
         myFilePath = os.path.abspath(
             os.path.join(os.path.dirname(__file__), os.pardir,
-                         '../__init__.py'))
+                         '../metadata.txt'))
         LOGGER.info(myFilePath)
-        myFile = file(myFilePath, 'rt')
-        myContent = myFile.read()
-        myFile.close()
         myMetadata = []
-        myCounter = 0
-        myLines = myContent.split('\n')
-        while myCounter < len(myLines):
-            if re.search(r'def\s+([^\(]+)', myLines[myCounter]):
-                myMatch = re.search(r'def\s+([^\(]+)',
-                                    myLines[myCounter]).groups()[0]
-                myCounter += 1
-                while myCounter < len(myLines) and myLines[myCounter] != '':
-                    if re.search(r'return\s+["\']?([^"\']+)["\']?',
-                                 myLines[myCounter]):
-                        myMetadata.append(
-                            (myMatch,
-                                re.search(r'return\s+["\']?([^"\']+)["\']?',
-                                          myLines[myCounter]).groups()[0]))
-                        break
-                    myCounter += 1
-            myCounter += 1
-        if not len(myMetadata):
-            assert False, 'Metadata could not be read'
+        parser = ConfigParser.ConfigParser()
+        parser.optionxform = str
+        parser.read(myFilePath)
+        myMessage = 'Cannot find a section named "general" in %s' % myFilePath
+        assert parser.has_section('general'), myMessage
+        myMetadata.extend(parser.items('general'))
 
-        for myItem in myRequiredMetadata:
-            if not myItem in dict(myMetadata) or not dict(myMetadata)[myItem]:
-                assert False, ('Cannot find myMetadata "%s" '
-                               'in myMetadata source (%s). Please bear in mind'
-                               ' that the current implementation of the '
-                               '__init__.py validator is based on regular '
-                               'expressions, check that your myMetadata '
-                               'functions directly return myMetadata values as'
-                               ' strings.') % (myItem,
-                                               dict(myMetadata).get(
-                                                   'metadata_source'))
+        for md in myRequiredMetadata:
+            myMessage = 'Cannot find myMetadata "%s" '\
+                        'in myMetadata source (%s).' % (md, myFilePath)
+            assert md in dict(myMetadata) or dict(myMetadata)[md],\
+                myMessage
 
 if __name__ == '__main__':
     unittest.main()
