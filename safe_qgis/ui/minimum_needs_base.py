a0f52507f4499a07a872f7d7ab82b8352b7eba4a
Tim Sutton <tim@linfiniti.com>
2013-08-07 08:43:10 +0200

    Added help button in function report dialog

--- safe_qgis/ui/minimum_needs_base.py
+++ safe_qgis/ui/minimum_needs_base.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'minimum_needs_base.ui'
 #
-# Created: Wed Jun 26 13:57:36 2013
-#      by: PyQt4 UI code generator 4.9.3
+# Created: Mon Aug  5 12:11:38 2013
+#      by: PyQt4 UI code generator 4.10
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,7 +12,16 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    _fromUtf8 = lambda s: s
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_MinimumNeedsBase(object):
     def setupUi(self, MinimumNeedsBase):
@@ -21,7 +30,7 @@ class Ui_MinimumNeedsBase(object):
         self.gridLayout = QtGui.QGridLayout(MinimumNeedsBase)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.webView = QtWebKit.QWebView(MinimumNeedsBase)
-        self.webView.setUrl(QtCore.QUrl(_fromUtf8("about:blank")))
+        self.webView.setProperty("url", QtCore.QUrl(_fromUtf8("about:blank")))
         self.webView.setObjectName(_fromUtf8("webView"))
         self.gridLayout.addWidget(self.webView, 0, 0, 1, 1)
         self.gridLayout_2 = QtGui.QGridLayout()
@@ -39,20 +48,20 @@ class Ui_MinimumNeedsBase(object):
         self.cboFields.setObjectName(_fromUtf8("cboFields"))
         self.gridLayout_2.addWidget(self.cboFields, 1, 1, 1, 1)
         self.gridLayout.addLayout(self.gridLayout_2, 1, 0, 1, 1)
-        self.buttonBox = QtGui.QDialogButtonBox(MinimumNeedsBase)
-        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
-        self.gridLayout.addWidget(self.buttonBox, 2, 0, 1, 1)
+        self.button_box = QtGui.QDialogButtonBox(MinimumNeedsBase)
+        self.button_box.setOrientation(QtCore.Qt.Horizontal)
+        self.button_box.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Help|QtGui.QDialogButtonBox.Ok)
+        self.button_box.setObjectName(_fromUtf8("button_box"))
+        self.gridLayout.addWidget(self.button_box, 2, 0, 1, 1)
 
         self.retranslateUi(MinimumNeedsBase)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), MinimumNeedsBase.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), MinimumNeedsBase.reject)
+        QtCore.QObject.connect(self.button_box, QtCore.SIGNAL(_fromUtf8("accepted()")), MinimumNeedsBase.accept)
+        QtCore.QObject.connect(self.button_box, QtCore.SIGNAL(_fromUtf8("rejected()")), MinimumNeedsBase.reject)
         QtCore.QMetaObject.connectSlotsByName(MinimumNeedsBase)
 
     def retranslateUi(self, MinimumNeedsBase):
-        MinimumNeedsBase.setWindowTitle(QtGui.QApplication.translate("MinimumNeedsBase", "Minumum Needs Calculator", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("MinimumNeedsBase", "Affected people Layer", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_3.setText(QtGui.QApplication.translate("MinimumNeedsBase", "Affected people field", None, QtGui.QApplication.UnicodeUTF8))
+        MinimumNeedsBase.setWindowTitle(_translate("MinimumNeedsBase", "Minumum Needs Calculator", None))
+        self.label.setText(_translate("MinimumNeedsBase", "Affected people Layer", None))
+        self.label_3.setText(_translate("MinimumNeedsBase", "Affected people field", None))
 
 from PyQt4 import QtWebKit
