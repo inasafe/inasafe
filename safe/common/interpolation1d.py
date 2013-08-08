239d8dfb42dc567700f2b3bc626ef34af763ac84
Christian Christelis <c.christelis@gmail.com>
2013-08-08 16:48:30 +0200

    Interpolation refactored and coding standas applied

--- safe/common/interpolation1d.py
+++ safe/common/interpolation1d.py
@@ -14,6 +14,8 @@ See interpolation2d.py for documentation of the mathematical derivation used.
 """
 
 import numpy
+
+from safe.common.interpolation import validate_inputs, validate_mode
 # pylint: disable=W0105
 
 
@@ -55,7 +57,9 @@ def interpolate1d(x, z, points, mode='linear', bounds_error=False):
     """
 
     # Input checks
-    x, z, xi = check_inputs(x, z, points, mode, bounds_error)
+    validate_mode(mode)
+    x, z, xi = validate_inputs(
+        x=x, z=z, points=points, bounds_error=bounds_error)
 
     # Identify elements that are outside interpolation domain or NaN
     outside = (xi < x[0]) + (xi > x[-1])
@@ -119,88 +123,6 @@ def interpolate1d(x, z, points, mode='linear', bounds_error=False):
 
     return r
 
-
-def check_inputs(x, z, points, mode, bounds_error):
-    """Check inputs for interpolate1d function
-
-    :param x: 1D array of x-coordinates on which to interpolate
-    :type x: numpy.ndarray
-
-    :param z: 1D array of values for each x
-    :type z: numpy.ndarray
-
-    :param points: 1D array of coordinates where interpolated values are sought
-    :type points: numpy.ndarray
-
-    :param mode: Determines the interpolation order.
-        Options are:
-            * 'constant' - piecewise constant nearest neighbour interpolation
-            * 'linear' - bilinear interpolation using the two nearest \
-              neighbours (default)
-    :type mode: str
-
-    :param bounds_error: Flag to indicate whether an exception will be raised
-        when interpolated values are requested outside the domain of the
-        input data. If False, nan is returned for those values.
-    :type bounds_error: bool
-
-    :returns: x, z and points
-
-    :raises: RuntimeError, Exception
-    """
-
-    msg = 'Only mode "linear" and "constant" are implemented. I got %s' % mode
-    if mode not in ['linear', 'constant']:
-        raise RuntimeError(msg)
-
-    x = numpy.array(x)
-
-    msg = ('Input vector x must be monotoneously increasing. I got '
-           'min(x) == %.15f, but x[0] == %.15f' % (min(x), x[0]))
-    if not min(x) == x[0]:
-        raise RuntimeError(msg)
-
-    msg = ('Input vector x must be monotoneously increasing. I got '
-           'max(x) == %.15f, but x[-1] == %.15f' % (max(x), x[-1]))
-    if not max(x) == x[-1]:
-        raise RuntimeError(msg)
-
-    msg = 'Z must be a 1d numpy array'
-    try:
-        z = numpy.array(z)
-    except Exception, e:
-        msg += '%s: %s' % (msg, e)
-        raise Exception(msg)
-
-    if not len(z.shape) == 1:
-        raise RuntimeError(msg)
-
-    m = len(z)
-    Nx = len(x)
-    msg = ('Input array z must have same length as x (%i).'
-           'However, Z has length %i.' % (Nx, m))
-    if not Nx == m:
-        raise RuntimeError(msg)
-
-    # Get interpolation points
-    points = numpy.array(points)
-    msg = 'Interpolation points must be a 1d array'
-    if not len(points.shape) == 1:
-        raise RuntimeError(msg)
-    xi = points[:]
-
-    if bounds_error:
-        msg = ('Interpolation point %f was less than the smallest value in '
-               'domain %f and bounds_error was requested.' % (xi[0], x[0]))
-        if xi[0] < x[0]:
-            raise Exception(msg)
-
-        msg = ('Interpolation point %f was greater than the largest value in '
-               'domain %f and bounds_error was requested.' % (xi[-1], x[-1]))
-        if xi[-1] > x[-1]:
-            raise Exception(msg)
-
-    return x, z, xi
 
 # Mathematical derivation of the interpolation formula used
 # noinspection PyStatementEffect
