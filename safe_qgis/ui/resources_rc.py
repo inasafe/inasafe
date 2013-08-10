e9b8927371dc1a2011290bd8a8b54bb43f54a56b
Tim Sutton <tim@linfiniti.com>
2013-08-10 13:53:52 +0200

    Updated translation strings

--- safe_qgis/ui/resources_rc.py
+++ safe_qgis/ui/resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Mon Aug 5 12:11:38 2013
+# Created: Sat Aug 10 13:36:59 2013
 #      by: The Resource Compiler for PyQt (Qt v4.8.4)
 #
 # WARNING! All changes made in this file will be lost!
