a5e02671da713fcfdc472d50ff9515ccdd8d4ed1
Tim Sutton <tim@linfiniti.com>
2013-10-08 09:31:20 +0200

    Added pycharm launcher for osx

---
+++ run-osx-pycharm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Launcher to PyCharm with QGIS libs available to python
+#
+# Tim Sutton, June 2013
+
+QGISPATH=/Applications/QGIS.app
+export QGIS_PREFIX_PATH=${QGISPATH}/contents/MacOS
+echo "QGIS PATH: $QGIS_PREFIX_PATH"
+PYTHONPATH=${PYTHONPATH}:"${QGISPATH}/Contents/Resources/python"
+PYTHONPATH=${PYTHONPATH}:'/Library/Frameworks/GDAL.framework/Versions/1.10/Python/2.7/site-packages'
+export PYTHONPATH
+
+export QGIS_DEBUG=0
+export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
+
+export INASAFE_WORK_DIR=/tmp/inasafe
+export INASAFE_POPULATION_PATH=`pwd`/realtime/fixtures/exposure/population.tif
+export INASAFE_LOCALE=id
+
+echo "PYTHON PATH: $PYTHONPATH"
+
+/Applications/PyCharm.app/Contents/MacOS/pycharm
