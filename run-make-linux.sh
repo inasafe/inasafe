d7c61ad0f94c957109d8674bae79f0556bc7b248
Akbar Gumbira <akbargumbira@gmail.com>
2013-10-22 15:27:20 +0700

    Use QGIS 2.0 instead of QGIS Realtime for all scripts

--- run-make-linux.sh
+++ run-make-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Call make with local env set for hand build qgis master
-export QGIS_PREFIX_PATH=/usr/local/qgis-master/
+export QGIS_PREFIX_PATH=/usr/local/qgis-2.0
 export LD_LIBRARY_PATH=${QGIS_PREFIX_PATH}/lib
 export PYTHONPATH=${QGIS_PREFIX_PATH}/qgis/python:${PYTHONPATH}
 make $1
--- scripts/make-flood-forecast.sh
+++ scripts/make-flood-forecast.sh
@@ -3,10 +3,10 @@
 export QGIS_DEBUG=0
 export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
 export QGIS_DEBUG_FILE=/tmp/inasafe/realtime/logs/qgis-debug.log
-export QGIS_PREFIX_PATH=/usr/local/qgis-realtime/
-# export PYTHONPATH=/usr/local/qgis-realtime/share/qgis/python/:`pwd`
+export QGIS_PREFIX_PATH=/usr/local/qgis-2.0/
+# export PYTHONPATH=/usr/local/qgis-2.0/share/qgis/python/:`pwd`
 export PYTHONPATH=~/Documents/inasafe/inasafe-dev:`pwd`
-export LD_LIBRARY_PATH=/usr/local/qgis-realtime/lib
+export LD_LIBRARY_PATH=/usr/local/qgis-2.0/lib
 
 export INASAFE_LOCALE=id
 cd /home/sunnii/Documents/inasafe/inasafe-dev/
