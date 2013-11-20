d0b3e7ce6f0c1c1d7a4ec8d9b6c1e1a39801f817
Marco Bernasocchi <marco@opengis.ch>
2013-11-20 17:45:38 +0800

    fix invalid py 2.7 only syntax

--- safe/messaging/item/text.py
+++ safe/messaging/item/text.py
@@ -164,8 +164,7 @@ class PlainText(Text):
         # Deal with long file names that prevent wrapping
         wrappable_text = self.to_text().replace(os.sep, '<wbr>' + os.sep)
         if icon is not '' and attributes is not '':
-            return '<span%s>%s%s</span>' % {
-                attributes, icon, wrappable_text}
+            return '<span%s>%s%s</span>' % (attributes, icon, wrappable_text)
         else:
             return self.to_text()
 
