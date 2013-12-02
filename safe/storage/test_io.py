e7aba853998f910183f3ea098f57db44816b2272
Tim Sutton <tim@linfiniti.com>
2013-12-02 14:02:28 +0200

    Fixes for failing tests in quicktest

--- safe/storage/test_io.py
+++ safe/storage/test_io.py
@@ -1729,7 +1729,10 @@ class Test_IO(unittest.TestCase):
                    'get_geometry_type',
                    'get_geometry_name',
                    'to_vector_points',
-                   'to_vector_layer']
+                   'to_vector_layer',
+                   'as_qgis_native',  # added in InaSAFE 2.0
+                   'read_from_qgis_native'  # added in InaSAFE 2.0
+                   ]
 
         V = Vector()  # Empty vector instance
         R = Raster()  # Empty raster instance
