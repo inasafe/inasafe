3780ecbcf382abb35dabce6c9386fea9ac623892
Tim Sutton <tim@linfiniti.com>
2013-12-04 16:48:38 +0200

    Also add QGIS bin dir to path when setting env for linux

--- run-env-linux.sh
+++ run-env-linux.sh
@@ -10,6 +10,7 @@ export QGIS_DEBUG=0
 export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
 export QGIS_DEBUG_FILE=/tmp/inasafe/realtime/logs/qgis-debug.log
 
+export PATH=${QGIS_PREFIX_PATH}/bin:$PATH
 
 export INASAFE_WORK_DIR=/tmp/quake
 export INASAFE_POPULATION_PATH=`pwd`/realtime/fixtures/exposure/population.tif
