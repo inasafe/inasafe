57d9ee8211c20b3cd7632d658b44701559e05626
Tim Sutton <tim@linfiniti.com>
2013-12-12 09:33:28 +0200

    Resolved merge conflitcs after reverting ff9ddace6abf48304a2dd4b7497b189a95bbc246 which causes tests to stall.

--- safe_qgis/widgets/message_viewer.py
+++ safe_qgis/widgets/message_viewer.py
@@ -16,7 +16,6 @@ __date__ = '27/05/2013'
 __copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
                  'Disaster Reduction')
 import logging
-import time
 
 from PyQt4.QtCore import QUrl
 
@@ -76,28 +75,17 @@ class MessageViewer(QtWebKit.QWebView):
         self.report_path = None
         self._impact_path = None
 
-        self._html_loaded_flag = False
-
         #base_dir = os.path.dirname(__file__)
         #self.header = header.replace('PATH', base_dir)
 
     @property
     def impact_path(self):
-        """Get the location of the impact file.
-
-        :returns: Path to the impact layer.
-        :rtype: str
-        """
         return self._impact_path
 
     @impact_path.setter
-    def impact_path(self, path):
-        """Set the location of the impact file.
-        :param path: Path to the impact layer.
-        :type path: str
-        """
-        self._impact_path = path
-        if path is None:
+    def impact_path(self, value):
+        self._impact_path = value
+        if value is None:
             self.action_show_report.setEnabled(False)
             self.action_show_log.setEnabled(False)
             self.report_path = None
@@ -133,8 +121,7 @@ class MessageViewer(QtWebKit.QWebView):
         # add show in browser
         action_page_to_html_file = QtGui.QAction(
             self.tr('Open in web browser'), None)
-        action_page_to_html_file.triggered.connect(
-            self.open_current_in_browser)
+        action_page_to_html_file.triggered.connect(self.open_current_in_browser)
         context_menu.addAction(action_page_to_html_file)
 
         # add load report
@@ -231,7 +218,6 @@ class MessageViewer(QtWebKit.QWebView):
         # self.scrollToDiv()
 
     def clear_dynamic_messages_log(self):
-        """Clear all dynamic messages."""
         self.dynamic_messages_log = []
 
     def scroll_to_div(self):
@@ -263,7 +249,7 @@ class MessageViewer(QtWebKit.QWebView):
                 string += html
 
         string += html_footer()
-        self.load_and_wait_html(html=string)
+        self.setHtml(string)
         #self.scroll_to_div()
 
     def to_message(self):
@@ -290,7 +276,6 @@ class MessageViewer(QtWebKit.QWebView):
         print self.page_to_text()
 
     def save_report_to_html(self):
-        """Write out the current report to html."""
         html = self.page().mainFrame().toHtml()
         if self.report_path is not None:
             html_to_file(html, self.report_path)
@@ -299,7 +284,6 @@ class MessageViewer(QtWebKit.QWebView):
             raise InvalidParameterError(msg)
 
     def save_log_to_html(self):
-        """Write out the current log as an html file."""
         html = html_header()
         html += ('<img src="qrc:/plugins/inasafe/inasafe-logo.svg" '
                  'title="InaSAFE Logo" alt="InaSAFE Logo" />')
@@ -315,22 +299,16 @@ class MessageViewer(QtWebKit.QWebView):
             raise InvalidParameterError(msg)
 
     def show_report(self):
-        """Show the report for this analysis.
-        """
         self.action_show_report.setEnabled(False)
         self.action_show_log.setEnabled(True)
-        self.load_and_wait_html(file_path=self.report_path)
+        self.load_html_file(self.report_path)
 
     def show_log(self):
-        """Show the work log for this analysis.
-        """
         self.action_show_report.setEnabled(True)
         self.action_show_log.setEnabled(False)
-        self.load_and_wait_html(file_path=self.log_path)
+        self.load_html_file(self.log_path)
 
     def open_current_in_browser(self):
-        """Open the content of the message log in the browser.
-        """
         if self.impact_path is None:
             html = self.page().mainFrame().toHtml()
             html_to_file(html, open_browser=True)
@@ -341,56 +319,5 @@ class MessageViewer(QtWebKit.QWebView):
             else:
                 open_in_browser(self.report_path)
 
-    # TODO (MB) this and the similar method in html_renderer could be
-    # refactored out to the utilities
-    def load_and_wait_html(self, file_path=None, html=None):
-        # noinspection PyUnresolvedReferences
-        """Load a page and block till it is loaded.
-
-        To use call with either a valid file path or a valid html string.
-
-        :param file_path: Optional file URI for the page to load.
-        :type file_path: str
-
-        :param html: Optional html string to load
-        :type html: str
-
-        :raise RuntimeError:
-        """
-        if file_path is None and html is None:
-            raise RuntimeError(self.tr('file_path or html need to be set'))
-
-        self._html_loaded_flag = False
-        self.loadFinished.connect(self.html_loaded_slot)
-
-        if file_path is not None:
-            # noinspection PyCallByClass,PyTypeChecker,PyArgumentList
-            self.setUrl(QUrl.fromLocalFile(file_path))
-        elif html is not None:
-            self.setHtml(html)
-
-        my_counter = 0
-        my_sleep_period = 0.1  # sec
-        my_timeout = 10  # sec
-        while not self._html_loaded_flag and my_counter < my_timeout:
-            # Block until the event loop is done printing the page
-            my_counter += my_sleep_period
-            time.sleep(my_sleep_period)
-            # noinspection PyArgumentList
-            QtCore.QCoreApplication.processEvents()
-
-        if not self._html_loaded_flag:
-            LOGGER.error('Failed to load html')
-
-        # noinspection PyUnresolvedReferences
-        self.loadFinished.disconnect(self.html_loaded_slot)
-
-    def html_loaded_slot(self, ok):
-        """Slot called when the page is loaded.
-
-        :param ok: Flag indicating if the page loaded ok
-        :type ok: bool
-        """
-        #print "html_loaded_slot set to %s" % ok
-        self._html_loaded_flag = ok
-        LOGGER.debug('html_loaded_slot slot called')
+    def load_html_file(self, file_path):
+        self.setUrl(QtCore.QUrl.fromLocalFile(file_path))
