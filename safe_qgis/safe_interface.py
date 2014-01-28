99f3d253225d098f4d25589eb56c77686e0ac457
Akbar Gumbira <akbargumbira@gmail.com>
2014-01-28 10:46:40 +0700

    Follow coding standards.

--- safe_qgis/safe_interface.py
+++ safe_qgis/safe_interface.py
@@ -301,7 +301,7 @@ def readKeywordsFromLayer(theLayer, keyword):
     return value
 
 
-def read_file_keywords(theLayerPath, theKeyword=None):
+def read_file_keywords(layer_path, keyword=None):
     """Get metadata from the keywords file associated with a local
      file in the file system.
 
@@ -311,56 +311,58 @@ def read_file_keywords(theLayerPath, theKeyword=None):
 
     .. seealso:: readKeywordsFromLayer
 
-    Args:
-
-       * theLayerPath - a string representing a path to a layer
+    :param: layer_path: a string representing a path to a layer
            (e.g. '/tmp/foo.shp', '/tmp/foo.tif')
-       * theKeyword - optional - the metadata keyword to retrieve e.g. 'title'
+    :type layer_path: str
 
-    Returns:
-       A string containing the retrieved value for the keyword if
-       the keyword argument is specified, otherwise the
-       complete keywords dictionary is returned.
+    :param keyword: optional - the metadata keyword to retrieve e.g. 'title'
+    :type keyword: str
 
-    Raises:
-       KeywordNotFoundError if the keyword is not recognised.
-       NoKeywordsFoundError if no keyword file exists.
-       InvalidParameterError if the layer does not exist.
+    :return: A string containing the retrieved value for the keyword if
+             the keyword argument is specified, otherwise the
+             complete keywords dictionary is returned.
+
+    :raises: KeywordNotFoundError, NoKeywordsFoundError, InvalidParameterError
+
+    Note:
+        * KeywordNotFoundError - occurs when the keyword is not recognised.
+        * NoKeywordsFoundError - occurs when no keyword file exists.
+        * InvalidParameterError - occurs when the layer does not exist.
     """
     # check the source layer path is valid
-    if not os.path.isfile(theLayerPath):
+    if not os.path.isfile(layer_path):
         message = tr('Cannot get keywords from a non-existent file.'
-                       '%s does not exist.' % theLayerPath)
+                     '%s does not exist.' % layer_path)
         raise InvalidParameterError(message)
 
     # check there really is a keywords file for this layer
-    keywordFilePath = os.path.splitext(theLayerPath)[0]
-    keywordFilePath += '.keywords'
-    if not os.path.isfile(keywordFilePath):
-        message = tr('No keywords file found for %s' % keywordFilePath)
+    keyword_file_path = os.path.splitext(layer_path)[0]
+    keyword_file_path += '.keywords'
+    if not os.path.isfile(keyword_file_path):
+        message = tr('No keywords file found for %s' % keyword_file_path)
         raise NoKeywordsFoundError(message)
 
     # now get the requested keyword using the inasafe library
-    myDictionary = None
+    dictionary = None
     try:
-        myDictionary = read_keywords(keywordFilePath)
+        dictionary = read_keywords(keyword_file_path)
     except Exception, e:
         message = \
             tr('Keyword retrieval failed for %s (%s) \n %s' % (
-                keywordFilePath, theKeyword, str(e)))
+                keyword_file_path, keyword, str(e)))
         raise KeywordNotFoundError(message)
 
     # if no keyword was supplied, just return the dict
-    if theKeyword is None:
-        return myDictionary
-    if not theKeyword in myDictionary:
+    if keyword is None:
+        return dictionary
+    if not keyword in dictionary:
         message = \
             tr('No value was found in file %s for keyword %s' % (
-                keywordFilePath, theKeyword))
+                keyword_file_path, keyword))
         raise KeywordNotFoundError(message)
 
     try:
-        value = myDictionary[theKeyword]
+        value = dictionary[keyword]
     except:
         raise
     return value
