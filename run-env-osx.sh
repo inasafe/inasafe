0a041eb26064d2ece1d58cb143b4aa60433ac06d
Tim Sutton <tim@linfiniti.com>
2014-02-12 18:15:05 +0700

    Fixed but in osx python path

--- run-env-osx.sh
+++ run-env-osx.sh
@@ -10,7 +10,7 @@
 export PATH=$PATH:/usr/local/bin/
 
 SITES=`find /usr/local/ -name site-packages`
-for SITE in SITES
+for SITE in $SITES
 do
 	PYTHONPATH=$PYTHONPATH:$SITE
 done
@@ -20,9 +20,6 @@ export PYTHONPATH
 QGISPATH=`find /usr/local/ -name QGIS.app`
 export QGIS_PREFIX_PATH=${QGISPATH}/contents/MacOS
 echo "QGIS PATH: $QGIS_PREFIX_PATH"
-#PYTHONPATH=${PYTHONPATH}:"${QGISPATH}/Contents/Resources/python"
-#PYTHONPATH=${PYTHONPATH}:'/Library/Frameworks/GDAL.framework/Versions/1.10/Python/2.7/site-packages'
-#export PYTHONPATH
 
 export QGIS_DEBUG=0
 export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
