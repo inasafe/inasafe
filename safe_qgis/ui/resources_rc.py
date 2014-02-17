3aaf739f33d6825f109c33776682283f4ae83d87
Akbar Gumbira <akbargumbira@gmail.com>
2014-02-17 16:54:56 +0700

    Updated ID translation.

--- safe_qgis/ui/resources_rc.py
+++ safe_qgis/ui/resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Mon Feb 17 15:16:01 2014
+# Created: Mon Feb 17 16:53:28 2014
 #      by: The Resource Compiler for PyQt (Qt v4.8.2)
 #
 # WARNING! All changes made in this file will be lost!
