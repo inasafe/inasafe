a59781f364ae751ed68e61bd08d85a51391a7e91
Marco Bernasocchi <marco@opengis.ch>
2014-02-20 16:00:50 +0100

    allow to pass custom qgis install location to runenv-linux.sh

--- run-env-linux.sh
+++ run-env-linux.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-export QGIS_PREFIX_PATH=/usr/local/qgis-2.0
-export QGIS_PATH=QGIS_PREFIX_PATH
+QGIS_PREFIX_PATH=/usr/local/qgis-2.0
+if [ -n "$1" ]; then
+    QGIS_PREFIX_PATH=$1
+fi
+
+echo $QGIS_PREFIX_PATH
+
+
+export QGIS_PREFIX_PATH=$QGIS_PREFIX_PATH
+export QGIS_PATH=$QGIS_PREFIX_PATH
 export LD_LIBRARY_PATH=${QGIS_PREFIX_PATH}/lib
-export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python:${PYTHONPATH}
+export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python:${QGIS_PREFIX_PATH}/share/qgis/python/plugins:${PYTHONPATH}
 
 echo "QGIS PATH: $QGIS_PREFIX_PATH"
 export QGIS_DEBUG=0
@@ -19,7 +27,7 @@ export INASAFE_LOCALE=id
 echo "This script is intended to be sourced to set up your shell to"
 echo "use a QGIS 2.0 built in $QGIS_PREFIX_PATH"
 echo
-echo "To use it do:" 
-echo "source $0"
+echo "To use it do:"
+echo "source $BASH_SOURCE /your/optional/install/path"
 echo
 echo "Then use the make file supplied here e.g. make guitest"
