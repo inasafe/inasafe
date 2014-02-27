8c5a5916ad7ec79f4804b1e5ced6fad54d1488ab
Akbar Gumbira <akbargumbira@gmail.com>
2014-02-27 11:52:39 +0700

    Fix #839 Make QTextEdit of all the tabs on the about dialog to ReadOnly.

--- safe_qgis/ui/resources_rc.py
+++ safe_qgis/ui/resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Thu Feb 27 10:08:48 2014
+# Created: Thu Feb 27 11:49:43 2014
 #      by: The Resource Compiler for PyQt (Qt v4.8.2)
 #
 # WARNING! All changes made in this file will be lost!
