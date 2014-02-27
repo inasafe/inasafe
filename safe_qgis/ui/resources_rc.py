57ed878f2b3040cb76f9bcf1e9c7f5f4a745340a
Akbar Gumbira <akbargumbira@gmail.com>
2014-02-27 10:09:09 +0700

    Compiled translation strings.

--- safe_qgis/ui/resources_rc.py
+++ safe_qgis/ui/resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Wed Feb 26 17:28:09 2014
+# Created: Thu Feb 27 10:08:48 2014
 #      by: The Resource Compiler for PyQt (Qt v4.8.2)
 #
 # WARNING! All changes made in this file will be lost!
