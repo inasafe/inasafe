c45c0cb2461742a0f449050d1e7fedf8db6116a6
Tim Sutton <tim@linfiniti.com>
2014-03-08 22:53:49 +0200

    Bumped version to 2.0.3

--- safe_qgis/ui/resources_rc.py
+++ safe_qgis/ui/resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Mon Mar 3 00:27:34 2014
+# Created: Sat Mar 8 00:40:54 2014
 #      by: The Resource Compiler for PyQt (Qt v4.8.4)
 #
 # WARNING! All changes made in this file will be lost!
