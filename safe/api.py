3fce452ce855787a584a007d3eed3c7926cacb81
Ismail Sunni <imajimatika@gmail.com>
2014-03-20 14:33:25 +0700

    Add ImpactFunctionManager to safe.api

--- safe/api.py
+++ safe/api.py
@@ -23,6 +23,7 @@ __copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
 
 # pylint: disable=W0611
 #noinspection PyUnresolvedReferences
+from safe.impact_functions.impact_function_manager import ImpactFunctionManager
 from safe.storage.vector import Layer
 from safe.storage.vector import Vector
 from safe.storage.raster import Raster
