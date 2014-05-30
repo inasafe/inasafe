dc90fc4e7fae16b38577d6b7b8ba2d45539be388
Akbar Gumbira <akbargumbira@gmail.com>
2014-05-30 17:17:27 +0700

    [Realtime] Update make local map.

--- scripts/make-shakemap-local.sh
+++ scripts/make-shakemap-local.sh
@@ -4,11 +4,11 @@ export QGIS_DEBUG=0
 export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
 export QGIS_DEBUG_FILE=/tmp/inasafe/realtime/logs/qgis-debug.log
 
-export QGIS_PREFIX_PATH=/usr/local/qgis-2.0
-export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python/:`pwd`
+export QGIS_PREFIX_PATH=/usr/local/qgis-2.2
+export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python/:${QGIS_PREFIX_PATH}/share/qgis/python/plugins:`pwd`
 export LD_LIBRARY_PATH=${QGIS_PREFIX_PATH}/lib
 
-export INASAFE_WORK_DIR=/tmp/inasafe/realtime
+export INASAFE_WORK_DIR=/tmp/quake
 export INASAFE_POPULATION_PATH=`pwd`/realtime/fixtures/exposure/population.tif
 export INASAFE_LOCALE=id
 
