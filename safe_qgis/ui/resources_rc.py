fe0097342b8f15de3fc4f1a4f361d6de5b9c8d03
Ismail Sunni <imajimatika@gmail.com>
2014-06-04 11:34:21 +0700

    Fix failed unittests.

--- safe_qgis/ui/resources_rc.py
+++ safe_qgis/ui/resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Wed Jun 4 08:41:41 2014
+# Created: Wed Jun 4 10:22:36 2014
 #      by: The Resource Compiler for PyQt (Qt v4.8.1)
 #
 # WARNING! All changes made in this file will be lost!
