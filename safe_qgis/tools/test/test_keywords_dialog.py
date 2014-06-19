3c69be17d34d1ffe0c5d433ec304088e538e5dc6
Akbar Gumbira <akbargumbira@gmail.com>
2014-06-19 15:07:19 +0700

    Don't use our singleton object on instantiating Wizard Dialog on tests.

--- safe_qgis/tools/test/test_keywords_dialog.py
+++ safe_qgis/tools/test/test_keywords_dialog.py
@@ -46,7 +46,7 @@ from safe.common.testing import get_qgis_app
 QGIS_APP, CANVAS, IFACE, PARENT = get_qgis_app()
 
 from safe_qgis.utilities.utilities_for_testing import (
-    test_data_path, clone_shp_layer, remove_temp_file)
+    test_data_path, clone_shp_layer, remove_vector_temp_file)
 from safe_qgis.safe_interface import (
     read_file_keywords,
     unique_filename,
@@ -518,7 +518,7 @@ class KeywordsDialogTest(unittest.TestCase):
         layer = clone_shp_layer(
             name='kabupaten_jakarta',
             include_keywords=True,
-            directory=BOUNDDATA)
+            source_directory=BOUNDDATA)
         dialog = KeywordsDialog(PARENT, IFACE, layer=layer)
 
         # Load existing keywords
@@ -590,7 +590,11 @@ class KeywordsDialogTest(unittest.TestCase):
         message = 'Expected %s but I got %s' % (False, good_sum_ratio)
         self.assertEqual(False, good_sum_ratio, message)
 
-        remove_temp_file(layer.source())
+        # We need to delete reference to layer on Windows before removing the files
+        layer_path = layer.source()
+        del layer
+        del dialog.layer
+        remove_vector_temp_file(layer_path)
 
 if __name__ == '__main__':
     suite = unittest.makeSuite(KeywordsDialogTest)
