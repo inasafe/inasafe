6f477d1123cf3fcebdd3652488e32824b2f96db6
dianne <diannebencito@essc.org.ph>
2014-10-29 18:46:46 +0800

    added 'categorised' for unit options

--- safe_qgis/tools/test/test_wizard_dialog.py
+++ safe_qgis/tools/test/test_wizard_dialog.py
@@ -806,7 +806,7 @@ class WizardDialogTest(unittest.TestCase):
         self.check_current_step(step_unit, dialog)
 
         # check the values of units options
-        expected_units = ['normalised', 'metres', 'feet']
+        expected_units = ['normalised', 'categorised', 'metres', 'feet']
         self.check_list(expected_units, dialog.lstUnits)
 
         # choosing metres
