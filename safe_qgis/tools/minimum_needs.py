48c5e27cb369decd5525101f0e8444e98a42bdeb
Christian Christelis <c.christelis@gmail.com>
2014-11-03 15:16:14 +0200

    using the QFile and QDirectory api to access files relative to '.qgis2'.

--- safe_qgis/tools/minimum_needs.py
+++ safe_qgis/tools/minimum_needs.py
@@ -7,11 +7,10 @@ __date__ = '05/10/2014'
 __copyright__ = ('Copyright 2014, Australia Indonesia Facility for '
                  'Disaster Reduction')
 
-from PyQt4.QtCore import QSettings
+from PyQt4.QtCore import QSettings, QFile, QDir
+from qgis.core import QgsApplication
 from safe.common.minimum_needs import MinimumNeeds
-import os
-from os.path import expanduser, exists
-import shutil
+import json
 
 
 class QMinimumNeeds(MinimumNeeds):
@@ -33,7 +32,8 @@ class QMinimumNeeds(MinimumNeeds):
         if minimum_needs is None:
             profiles = self.get_profiles()[0]
             self.read_from_file(
-                expanduser('~/.qgis2/minimum_needs/%s.json' % profiles))
+                QFile('%s/minimum_needs/%s.json' % (
+                    QgsApplication.qgisSettingsDirPath(), profiles)))
         if self.minimum_needs is None:
             self.minimum_needs = self._defaults()
 
@@ -44,7 +44,8 @@ class QMinimumNeeds(MinimumNeeds):
         :type profile: basestring, str
         """
         self.read_from_file(
-            expanduser('~/.qgis2/minimum_needs/%s.json' % profile))
+            QFile('%s/minimum_needs/%s.json' % (
+                QgsApplication.qgisSettingsDirPath(), profile)))
 
     def save_profile(self, profile):
         """Save the current minimum needs into a new profile.
@@ -53,7 +54,8 @@ class QMinimumNeeds(MinimumNeeds):
         :type profile: basestring, str
         """
         self.write_to_file(
-            expanduser('~/.qgis2/minimum_needs/%s.json' % profile))
+            QFile('%s/minimum_needs/%s.json' % (
+                QgsApplication.qgisSettingsDirPath(), profile)))
 
     def save(self):
         """Save the minimum needs to the QSettings object.
@@ -77,17 +79,65 @@ class QMinimumNeeds(MinimumNeeds):
         :returns: The minimum needs by name.
         :rtype: list
         """
-        if not exists(expanduser('~/.qgis2/minimum_needs/')):
-            if not exists(
-                    '~/.qgis2/python/plugins/inasafe/files/minimum_needs/'):
+        local_minimum_needs_dir = QDir(
+            '%s/minimum_needs/' % QgsApplication.qgisSettingsDirPath())
+        plugins_minimum_needs_dir = QDir(
+            '%s/python/plugins/inasafe/files/minimum_needs/' %
+            QgsApplication.qgisSettingsDirPath())
+        print "Does the local dear exist?"
+        print local_minimum_needs_dir.exists()
+        if not local_minimum_needs_dir.exists():
+            if not plugins_minimum_needs_dir.exists():
+                print "Does the plugin dir exist?"
+                print plugins_minimum_needs_dir.exists()
                 """This is specifically to get Travis working."""
                 return [self._defaults()['profile']]
-            shutil.copytree(
-                expanduser(
-                    '~/.qgis2/python/plugins/inasafe/files/minimum_needs/'),
-                expanduser('~/.qgis2/minimum_needs'))
+            QDir(QgsApplication.qgisSettingsDirPath()).mkdir('minimum_needs')
+            for file_name in plugins_minimum_needs_dir.entryList():
+                print "Trying to copy this file: ", file_name
+                source_file = QFile(
+                    '%s/python/plugins/inasafe/files/minimum_needs/%s' %
+                    (QgsApplication.qgisSettingsDirPath(), file_name))
+                source_file.copy(
+                    '%s/minimum_needs/%s' %
+                    (QgsApplication.qgisSettingsDirPath(), file_name))
         profiles = [
             profile.rstrip('.json') for profile in
-            os.listdir(expanduser('~/.qgis2/minimum_needs/')) if
+            local_minimum_needs_dir.entryList() if
             profile[-5:] == '.json']
         return profiles
+
+    def read_from_file(self, qfile):
+        """Read from an existing json file.
+
+        :param qfile: The object to be read from.
+        :type qfile: QFile
+
+        :returns: Success status. -1 for unsuccessful 0 for success
+        :rtype: int
+        """
+        if not qfile.exists():
+            return -1
+        qfile.open(QFile.ReadOnly)
+        needs_json = qfile.readAll()
+        try:
+            minimum_needs = json.loads(needs_json)
+        except (TypeError, ValueError):
+            minimum_needs = None
+
+        if not minimum_needs:
+            return -1
+
+        return self.update_minimum_needs(minimum_needs)
+
+    def write_to_file(self, qfile):
+        """Write minimum needs as json to a file.
+
+        :param qfile: The file to be written to.
+        :type qfile: QFile
+        """
+        if not qfile.exists():
+            return -1
+        qfile.open(QFile.WriteOnly)
+        needs_json = json.dumps(self.minimum_needs)
+        qfile.write(needs_json)
