0f1dfb7653507bd4e5a3179820be6366f10cbf53
Marco Bernasocchi <marco@opengis.ch>
2014-11-04 01:22:12 +0100

    Quick fix last quicktests - @cchristelis you need to check this

--- safe/engine/test_engine.py
+++ safe/engine/test_engine.py
@@ -3027,13 +3027,16 @@ class Test_Engine(unittest.TestCase):
         evacuated = float(keywords['evacuated'])
         total_needs = keywords['total_needs']
 
+        #FIXME this is an HACK
+        total_needs = dict(eval(total_needs[12:-1]))
+
         expected_evacuated = 63400
         assert evacuated == expected_evacuated
-        assert total_needs['rice'] == 177520
-        assert total_needs['family_kits'] == 12680
-        assert total_needs['drinking_water'] == 1109500
-        assert total_needs['toilets'] == 3170
-        assert total_needs['water'] == 4247800
+        assert total_needs['Rice [kg]'] == 177520
+        assert total_needs['Family Kits'] == 12680
+        assert total_needs['Drinking Water [l]'] == 1109500
+        assert total_needs['Toilets'] == 3170
+        assert total_needs['Clean Water [l]'] == 4247800
 
     def test_flood_population_evacuation_polygon(self):
         """Flood population evacuation (flood is polygon)
