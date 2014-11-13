894383513bab9b4997be6f5ce0eeb253f4cc29ac
Ismail Sunni <imajimatika@gmail.com>
2014-11-13 09:48:22 +0700

    Unit test for #358.

---
+++ safe/common/test/test_version.py
@@ -0,0 +1,54 @@
+# coding=utf-8
+"""InaSAFE Disaster risk assessment tool developed by AusAid -
+  **Test class for version.py.**
+
+Contact : ole.moller.nielsen@gmail.com
+
+.. note:: This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+"""
+__author__ = 'imajimatika@gmail.com'
+__version__ = '2.2.0'
+__revision__ = '$Format:%H$'
+__date__ = '11/13/14'
+__copyright__ = 'Copyright 2012, Australia Indonesia Facility for '
+__copyright__ += 'Disaster Reduction'
+
+
+import unittest
+import sys
+from safe.common.version import get_git_changeset, get_version
+
+
+class TestVersion(unittest.TestCase):
+    def test_get_git_changeset(self):
+        """Test for get_git_changeset."""
+        if not ('win32' in sys.platform or 'darwin' in sys.platform):
+            changeset = get_git_changeset()
+            self.assertEqual(len(changeset), 14)
+
+    def test_get_version(self):
+        """Test for get_version."""
+        version_tuple = ('2', '2', '0', 'alpha', '0')
+        version = get_version(version_tuple)
+        self.assertIn('dev', version, 'There should be dev in version.')
+
+        # Version tuple doesn't have length == 5
+        version_tuple = ('2', '2', '0', 'alpha')
+        self.assertRaises(RuntimeError, get_version, version_tuple)
+
+        # Version tuple item 4th is not alpha, beta, rc, final
+        version_tuple = ('2', '2', '0', 'avocado', '0')
+        self.assertRaises(RuntimeError, get_version, version_tuple)
+
+        # Final version
+        version_tuple = ('2', '2', '0', 'final', '0')
+        version = get_version(version_tuple)
+        self.assertEqual(version, '2.2.0', 'The version should be 2.2.0')
+
+
+if __name__ == '__main__':
+    unittest.main()
