6045598303230a23aedbd25ed455b429ed4f8b36
Akbar Gumbira <akbargumbira@gmail.com>
2014-12-15 10:30:44 +0700

    Address FIXME Ole: Add test for Volcano Point on Population.

--- safe/impact_functions/volcanic/test/test_volcano_population_evacuation_polygon.py
+++ safe/impact_functions/volcanic/test/test_volcano_population_evacuation_polygon.py
@@ -17,13 +17,12 @@ __date__ = '12/12/2014'
 __copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
                  'Disaster Reduction')
 
-import numpy
 import unittest
 
 from safe.storage.core import read_layer
 from safe.engine.core import calculate_impact
 from safe.impact_functions import get_plugin
-from safe.common.testing import TESTDATA
+from safe.common.testing import TESTDATA, EXPDATA
 from safe.impact_functions.core import population_rounding
 from safe.common.utilities import format_int
 
@@ -67,7 +66,35 @@ class TestVolcanoPolygonHazardPopulationFunction(unittest.TestCase):
                    % (x, summary))
             assert x in summary, msg
 
-            # FIXME (Ole): Should also have test for concentric circle
-            #              evacuation zones
-
     test_volcano_population_evacuation_impact.slow = True
+
+    def test_volcano_circle_population_impact(self):
+        """Volcano function runs circular evacuation zone."""
+        # Name file names for hazard level, exposure and expected fatalities
+        hazard_filename = '%s/Merapi_alert.shp' % TESTDATA
+        exposure_filename = ('%s/glp10ag.asc' % EXPDATA)
+
+        # Calculate impact using API
+        hazard_layer = read_layer(hazard_filename)
+        exposure_layer = read_layer(exposure_filename)
+
+        plugin_name = 'Volcano Polygon Hazard Population'
+        impact_function = get_plugin(plugin_name)
+
+        impact_layer = calculate_impact(
+            layers=[hazard_layer, exposure_layer], impact_fcn=impact_function)
+        impact_filename = impact_layer.get_filename()
+
+        impact_layer = read_layer(impact_filename)
+        keywords = impact_layer.get_keywords()
+        print keywords
+        # This is the expected number of people affected
+        # Distance [km]	Total	Cumulative
+        # 3	     15.800	15.800
+        # 5	     17.300	33.100
+        # 10	125.000	158.000
+        message = 'Result not as expected'
+        impact_summary = keywords['impact_summary']
+        self.assertTrue(format_int(15800) in impact_summary, message)
+        self.assertTrue(format_int(17300) in impact_summary, message)
+        self.assertTrue(format_int(125000) in impact_summary, message)
