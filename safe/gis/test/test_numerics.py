c65758d7d6147586ea7e9b6093a5fa39bdc9fe84
Akbar Gumbira <akbargumbira@gmail.com>
2014-12-16 17:01:46 +0700

    Renamed the test numeric module.

--- safe/gis/test/test_numerics.py
+++ safe/gis/test/test_numerics.py
@@ -1,4 +1,17 @@
 # coding=utf-8
+"""
+InaSAFE Disaster risk assessment tool developed by AusAid -
+  **Numeric module test cases.**
+
+Contact : ole.moller.nielsen@gmail.com
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
 import unittest
 import numpy
 
