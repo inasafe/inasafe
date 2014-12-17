6e5e8b4e69acf4fb6da9c63c92ccad2c57778092
Ismail Sunni <imajimatika@gmail.com>
2014-12-17 10:26:39 +0700

    [Safe Extras] Remove test from parameters.

--- safe_extras/parameters/qt_widgets/test/custom_parameter/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-"""Docstring for this file."""
-__author__ = 'ismailsunni'
-__project_name = 'parameters'
-__filename = '__init__.py'
-__date__ = '12/15/14'
-__copyright__ = 'imajimatika@gmail.com'
-__doc__ = ''
--- safe_extras/parameters/test/test_registry.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-"""Tests for parameter registry."""
-
-from unittest import TestCase
-
-
-class TestRegistry(TestCase):
-    pass
