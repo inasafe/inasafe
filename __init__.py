7d338e46fefd4ba1efa7341a13d45edd9937c790
Tim Sutton <tim@linfiniti.com>
2015-01-26 13:54:24 +0200

    Fixed support for QGIS plugin reloader. You can now debug your code more easily against a running QGIS instance without needing to restart QGIS all the time.

--- __init__.py
+++ __init__.py
@@ -21,17 +21,41 @@ __copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
 
 import sys
 import os
-sys.path.append(os.path.dirname(__file__))
+import logging
 
-PARAMETER_DIR = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), 'safe_extras', 'parameters'))
-if PARAMETER_DIR not in sys.path:
-    sys.path.append(PARAMETER_DIR)
+#
+# Please do not put any application logic in the global space
+# it will break the plugin reloader because QGIS will load stuff
+# in our package tree whenever it scans for plugins.
+#
+# Also do not import anything from our package tree here!
+#
 
 
-# noinspection PyDocstring
 # noinspection PyDocstring,PyPep8Naming
 def classFactory(iface):
-    """Load Plugin class from file Plugin."""
+    """Load Plugin class from file Plugin.
+
+    Note that we do all the path manipulation and imports inside the factor
+    so that when QGIS is just scanning plugin folders it does not trigger
+    all modules being loaded.
+    """
+    #  sys.path.append(os.path.dirname(__file__))
+    from safe.utilities.i18n import load_translation
+    from safe.utilities.custom_logging import setup_logger
+    from safe.common.exceptions import TranslationLoadError
+    setup_logger()
+    LOGGER = logging.getLogger('InaSAFE')
+    parameter_package = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), 'safe_extras', 'parameters'))
+    if parameter_package not in sys.path:
+        sys.path.append(parameter_package)
+
+    import pydevd
+    pydevd.settrace('localhost', port=5678, stdoutToServer=True, stderrToServer=True)
+    try:
+        load_translation()
+    except TranslationLoadError, e:
+        LOGGER.info(e.message)
     from safe.plugin import Plugin
     return Plugin(iface)
