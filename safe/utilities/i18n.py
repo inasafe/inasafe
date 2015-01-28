0011c083889b7ab4eb843fe20cbb24efca56ae18
Tim Sutton <tim@linfiniti.com>
2015-01-28 14:59:45 +0200

    More fixes to make i18n work

--- safe/utilities/i18n.py
+++ safe/utilities/i18n.py
@@ -1,5 +1,5 @@
 
-from PyQt4.QtCore import QCoreApplication
+from PyQt4.QtCore import QCoreApplication, QSettings, QLocale
 
 
 __author__ = 'timlinux'
@@ -22,3 +22,22 @@ def tr(text):
     text = str(text)
     # noinspection PyCallByClass,PyTypeChecker,PyArgumentList
     return QCoreApplication.translate('@default', text)
+
+
+def locale():
+    """Get the name of the currently active locale.
+
+    :returns: Name of hte locale e.g. 'id'
+    :rtype: stre
+    """
+    override_flag = QSettings().value(
+        'locale/overrideFlag', True, type=bool)
+
+    if override_flag:
+        locale_name = QSettings().value('locale/userLocale', 'en_US', type=str)
+    else:
+        locale_name = QLocale.system().name()
+        # NOTES: we split the locale name because we need the first two
+        # character i.e. 'id', 'af, etc
+        locale_name = str(locale_name).split('_')[0]
+    return locale_name
