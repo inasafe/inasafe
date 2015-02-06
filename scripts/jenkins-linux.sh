5aa63e674493edb6726ac3db7d8e54f359e694fb
Akbar Gumbira <akbargumbira@gmail.com>
2015-02-06 15:54:01 +0700

    Put log and debug file into a more proper dir.

--- scripts/jenkins-linux.sh
+++ scripts/jenkins-linux.sh
@@ -62,8 +62,8 @@ export LD_LIBRARY_PATH=${QGIS_PREFIX_PATH}/lib
 export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python:${QGIS_PREFIX_PATH}/share/qgis/python/plugins:${PYTHONPATH}
 echo "QGIS PATH: $QGIS_PREFIX_PATH"
 export QGIS_DEBUG=0
-export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
-export QGIS_DEBUG_FILE=/tmp/inasafe/realtime/logs/qgis-debug.log
+export QGIS_LOG_FILE=/tmp/inasafe/logs/qgis.log
+export QGIS_DEBUG_FILE=/tmp/inasafe/logs/qgis-debug.log
 export PATH=${QGIS_PREFIX_PATH}/bin:$PATH
 export INASAFE_POPULATION_PATH=/var/lib/jenkins/jobs/InaSAFE-QGIS2/exposure/population.tif
 
