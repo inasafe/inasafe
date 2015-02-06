0b4a7c7cf10f3c93f63ad5b3cf2fd2134c0807fc
Akbar Gumbira <akbargumbira@gmail.com>
2015-02-06 15:51:36 +0700

    Update Jenkins to use QGIS 2.6

--- scripts/jenkins-linux.sh
+++ scripts/jenkins-linux.sh
@@ -56,10 +56,15 @@ in_array () {
 
 
 # Set up needed QGIS environment variables
-export QGIS_PREFIX_PATH=/usr/local/qgis-2.4/
-export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python/:${QGIS_PREFIX_PATH}/share/qgis/python/plugins:`pwd`
-echo "PYTHONPATH: $PYTHONPATH" > /tmp/path.txt
+export QGIS_PREFIX_PATH=/usr/local/qgis-2.6/
+export QGIS_PATH=$QGIS_PREFIX_PATH
 export LD_LIBRARY_PATH=${QGIS_PREFIX_PATH}/lib
+export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python:${QGIS_PREFIX_PATH}/share/qgis/python/plugins:${PYTHONPATH}
+echo "QGIS PATH: $QGIS_PREFIX_PATH"
+export QGIS_DEBUG=0
+export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
+export QGIS_DEBUG_FILE=/tmp/inasafe/realtime/logs/qgis-debug.log
+export PATH=${QGIS_PREFIX_PATH}/bin:$PATH
 export INASAFE_POPULATION_PATH=/var/lib/jenkins/jobs/InaSAFE-QGIS2/exposure/population.tif
 
 in_array "$1" "${ALLOWED_TEST_PACKAGES}"
