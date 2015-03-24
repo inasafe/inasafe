795102487602a4982564e802a5fcbee2c935383b
Christian Christelis <christian@kartoza.com>
2015-03-24 10:54:36 +0200

    Report standardization

--- safe/impact_reports/building_exposure_report_mixin.py
+++ safe/impact_reports/building_exposure_report_mixin.py
@@ -1,52 +1,64 @@
 from safe.common.utilities import format_int
 
+from safe.utilities.i18n import tr
+
 
 class BuildingExposureReportMixin(object):
-    def __init__(self):
-        self.affected_buildings = {}
 
     @property
-    def action_checklist(self):
-        schools_closed = 0
-        hospitals_closed = 0
-        for usage in self.affected_buildings:
-            if usage.lower() == 'school':
-                schools_closed = self.affected_buildings[usage]
-            if usage.lower() == 'hospital':
-                hospitals_closed = self.affected_buildings[usage]
+    def action_checklist(self, affected_buildings):
+        """Breakdown by building type.
 
+        :param affected_buildings: The affected buildings
+        :type affected_buildings: OrderedDict
+
+        :returns: The buildings breakdown report.
+        :rtype: list
+
+        ..Notes:
+        Expect affected buildings to be given as following:
+            affected_buildings = OrderedDict([
+                (category, {building_type: amount}),
+            e.g.
+                (inundated, {residential: 1000, school: 0 ...}),
+                (wet, {residential: 12, school: 2 ...}),
+                (dry, {residential: 50, school: 50})
+            ])
+        """
+        schools_closed = self.schools_closed(affected_buildings)
+        hospitals_closed = self.hospitals_closed(affected_buildings)
         return [
             {
-                'text': tr('Action Checklist:'),
+                'content': tr('Action Checklist:'),
                 'header': True
             },
             {
-                'text': tr('Are the critical facilities still open?')
+                'content': tr('Are the critical facilities still open?')
             },
             {
-                'text': tr(
+                'content': tr(
                     'Which structures have warning capacity '
                     '(eg. sirens, speakers, etc.)?')},
             {
-                'text': tr('Which buildings will be evacuation centres?')
+                'content': tr('Which buildings will be evacuation centres?')
             },
             {
-                'text': tr('Where will we locate the operations centre?')
+                'content': tr('Where will we locate the operations centre?')
             },
             {
-                'text': tr(
+                'content': tr(
                     'Where will we locate warehouse and/or distribution '
                     'centres?')
             },
             {
-                'text': tr(
+                'content': tr(
                     'Where will the students from the %s closed schools go to '
                     'study?'),
                 'arguments': (format_int(schools_closed),),
                 'condition': schools_closed > 0
             },
             {
-                'text': tr(
+                'content': tr(
                     'Where will the patients from the %s closed hospitals go '
                     'for treatment and how will we transport them?'),
                 'arguments': (format_int(hospitals_closed),),
@@ -77,24 +89,92 @@ class BuildingExposureReportMixin(object):
             ])
             buildings = {residential: 1062, school: 52 ...}
         """
+        buildings_breakdown_report = []
         category_names = affected_buildings.keys()
         table_headers = [tr('Building type')]
         table_headers += [tr(x) for x in category_names]
         table_headers += [tr('Total')]
-        building_list = []
+        buildings_breakdown_report.append(
+            {
+                'content': table_headers,
+                'header': True
+            })
         # Let's sort alphabetically first
-        building_types = [building_type for building_type in buildings
+        building_types = [building_type for building_type in buildings]
         building_types.sort()
         for building_type in building_types:
-            building_type = usage.replace('_', ' ')
+            building_type = building_type.replace('_', ' ')
             affected_by_usage = []
             for category in category_names:
-                if usage in affected_buildings[category]:
+                if building_type in affected_buildings[category]:
                     affected_by_usage.append(
-                        format_int(affected_buildings[category][usage]))
+                        format_int(
+                            affected_buildings[category][building_type]))
                 else:
                     affected_by_usage.append(format_int(0))
-            building_list.appen(
-                # building type                 categories            total
-                [building_type.capitalize()] + affected_by_usage + [format_int(buildings[usage])])
+            building_detail = (
+                # building type
+                [building_type.capitalize()] +
+                # categories
+                affected_by_usage +
+                # total
+                [format_int(buildings[building_type])])
+            buildings_breakdown_report.append(
+                {
+                    'content': building_detail
+                })
+
+        return buildings_breakdown_report
+
+    #This could be a property if we make affected_buildings a class property
+    def schools_closed(self, affected_buildings):
+        """Get the number of schools
+
+        :param affected_buildings: The affected buildings
+        :type affected_buildings: OrderedDict
+
+        :returns: The buildings breakdown report.
+        :rtype: list
+
+        ..Notes:
+        Expect affected buildings to be given as following:
+            affected_buildings = OrderedDict([
+                (category, {building_type: amount}),
+            e.g.
+                (inundated, {residential: 1000, school: 0 ...}),
+                (wet, {residential: 12, school: 2 ...}),
+                (dry, {residential: 50, school: 50})
+            ])
+        """
+        return self._count_usage('school', affected_buildings)
+
+    #This could also be a property if we make affected_buildings a class property
+    def hospitals_closed(self, affected_buildings):
+        """Get the number of schools
+
+        :param affected_buildings: The affected buildings
+        :type affected_buildings: OrderedDict
+
+        :returns: The buildings breakdown report.
+        :rtype: list
+
+        ..Notes:
+        Expect affected buildings to be given as following:
+            affected_buildings = OrderedDict([
+                (category, {building_type: amount}),
+            e.g.
+                (inundated, {residential: 1000, school: 0 ...}),
+                (wet, {residential: 12, school: 2 ...}),
+                (dry, {residential: 50, school: 50})
+            ])
+        """
+        return self._count_usage('hospital', affected_buildings)
+
+    def _count_usage(self, usage, affected_buildings):
+        count = 0
+        for cagegory, category_breakdown in affected_buildings:
+            for current_usage in category_breakdown:
+                if current_usage.lower() == usage.lower():
+                    count += affected_buildings[current_usage]
+        return count
 
