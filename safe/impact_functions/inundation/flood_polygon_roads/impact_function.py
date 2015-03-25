ab28bd682ad03075fe254c5403c175493c7a45bb
Jannes123 <jannes@kartoza.com>
2015-03-25 15:11:14 +0200

    stash commiit

--- safe/impact_functions/inundation/flood_polygon_roads/impact_function.py
+++ safe/impact_functions/inundation/flood_polygon_roads/impact_function.py
@@ -32,6 +32,7 @@ class FloodVectorRoadsExperimentalFunction(ImpactFunction):
     def __init__(self):
         """Constructor."""
         super(FloodVectorRoadsExperimentalFunction, self).__init__()
+        LOGGER.debug('INIT : FloodVectorRoadsExperimentalFunction')
 
     def _tabulate(self, flooded_len, question, road_len, roads_by_type):
         table_body = [
