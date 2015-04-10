6cb8d501d65417f641090e2b2e75ca4d49693c9e
Jannes123 <jannes@kartoza.com>
2015-04-10 11:17:26 +0200

    debugging

--- safe/impact_functions/inundation/flood_population_evacuation_raster_hazard/impact_function.py
+++ safe/impact_functions/inundation/flood_population_evacuation_raster_hazard/impact_function.py
@@ -143,7 +143,7 @@ class FloodEvacuationRasterHazardFunction(ImpactFunction):
 
         # Determine depths above which people are regarded affected [m]
         # Use thresholds from inundation layer if specified
-        thresholds = self.parameters['thresholds [m]']
+        thresholds = self.parameters['thresholds [m]'].value
 
         verify(
             isinstance(thresholds, list),
