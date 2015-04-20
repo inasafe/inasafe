e24dc4d01b91c516f6ed13c834fbcfb6edc9210d
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2015-04-21 05:52:26 +0700

    Fix pep8 errors

--- safe/gui/widgets/dock.py
+++ safe/gui/widgets/dock.py
@@ -720,7 +720,7 @@ class Dock(QtGui.QDockWidget, FORM_CLASS):
                 title=self.tr('Error while saving'),
                 message=self.tr("The destination location must be writable."))
 
-        except Exception: # pylint: disable=broad-except
+        except Exception:  # pylint: disable=broad-except
             display_critical_message_bar(
                 title=self.tr('Error while saving'),
                 message=self.tr("Something went wrong."))
