f3a3d9d227aa02b37a6585445da3608ae8ae26a4
Jannes123 <jannes@kartoza.com>
2015-05-26 16:19:26 +0200

    cleanup and remove test data

--- accent.py
+++ accent.py
@@ -23,11 +23,7 @@ from safe.test.utilities import get_qgis_app
 from safe.impact_functions.impact_function_manager import ImpactFunctionManager
 from qgis.core import (
     QgsRasterLayer,
-    QgsRaster,
-    QgsPoint,
-    QgsVectorLayer,
-    QgsVectorFileWriter,
-    QgsRectangle)
+    QgsVectorLayer)
 from safe.utilities.keyword_io import KeywordIO
 from safe.utilities.gis import qgis_version
 import os
@@ -45,19 +41,8 @@ version = None
 show_list = None
 extent = None
 
-# directories
-# defaults look like cli\--hazard
-#                       |--exposure
-#                       |--results
-
 default_cli_dir = os.path.abspath(
     os.path.realpath(os.path.dirname(__file__)))
-# default_hazard_dir = os.path.abspath(
-#     os.path.join(default_cli_dir, 'hazard'))
-# default_exposure_dir = os.path.abspath(
-#     os.path.join(default_cli_dir, 'exposure'))
-# default_results_dir = os.path.abspath(
-#     os.path.join(default_cli_dir, 'results'))
 usage_dir = os.environ['InaSAFEQGIS']
 usage = r""
 usage_file = file(os.path.join(usage_dir, 'usage.txt'))
@@ -81,73 +66,6 @@ def show_names(ifs):
         print impact_function.__name__
 
 
-def get_qgis_app():
-    LOGGER.debug('-- get qgis app --')
-    """ Start one QGIS application.
-
-    :returns: Handle to QGIS app, canvas, iface and parent. If there are any
-        errors the tuple members will be returned as None.
-    :rtype: (QgsApplication, CANVAS, IFACE, PARENT)
-
-    If QGIS is already running the handle to that app will be returned.
-    """
-
-    try:
-        from qgis.core import QgsApplication
-        from qgis.gui import QgsMapCanvas  # pylint: disable=no-name-in-module
-        # noinspection PyPackageRequirements
-        from PyQt4 import QtGui, QtCore  # pylint: disable=W0621
-        # noinspection PyPackageRequirements
-        from PyQt4.QtCore import QCoreApplication, QSettings
-        from safe.gis.qgis_interface import QgisInterface
-    except ImportError:
-        return None, None, None, None
-
-    global QGIS_APP  # pylint: disable=W0603
-
-    gui_flag = False  # run qgis in gui mode? does this make a difference?
-
-    # AG: For testing purposes, we use our own configuration file instead
-    # of using the QGIS apps conf of the host
-    # noinspection PyCallByClass,PyArgumentList
-    QCoreApplication.setOrganizationName('QGIS')
-    # noinspection PyCallByClass,PyArgumentList
-    QCoreApplication.setOrganizationDomain('qgis.org')
-    # noinspection PyCallByClass,PyArgumentList
-    QCoreApplication.setApplicationName('CLI')
-
-    # noinspection PyPep8Naming
-    QGIS_APP = QgsApplication(sys.argv, gui_flag)
-
-    # Make sure QGIS_PREFIX_PATH is set in your env if needed!
-    QGIS_APP.initQgis()
-    s = QGIS_APP.showSettings()
-    LOGGER.debug(s)
-
-    # Save some settings
-    settings = QSettings()
-    settings.setValue('locale/overrideFlag', True)
-    settings.setValue('locale/userLocale', 'en_US')
-
-    global PARENT  # pylint: disable=W0603
-    # noinspection PyPep8Naming
-    PARENT = QtGui.QWidget()
-
-    global CANVAS  # pylint: disable=W0603
-    # noinspection PyPep8Naming
-    CANVAS = QgsMapCanvas(PARENT)
-    CANVAS.resize(QtCore.QSize(400, 400))
-
-    global IFACE  # pylint: disable=W0603
-    # QgisInterface is a stub implementation of the QGIS plugin interface
-    # noinspection PyPep8Naming
-    IFACE = QgisInterface(CANVAS)
-    register_impact_functions()
-
-    return QGIS_APP, CANVAS, IFACE, PARENT
-
-
-# now I want to use absolute paths so check first before joining
 # all paths are made to be absolute
 def join_if_relative(path_argument):
     if not os.path.isabs(path_argument):
@@ -208,14 +126,11 @@ def run_if():
     impact_function_manager = ImpactFunctionManager()
     impact_function = impact_function_manager.get(
         shell_arguments['--impact-function'])
-    LOGGER.debug(shell_arguments['--impact-function'])
-
     keyword_io = KeywordIO()
-
     try:
         from safe.utilities.analysis import Analysis
     except ImportError:
-        LOGGER.debug('**Import error**')
+        LOGGER.debug('Import error for Analysis module')
         print ImportError.message
         return None, None, None, None
     analysis = Analysis()
@@ -229,6 +144,7 @@ def run_if():
         analysis.show_intermediate_layers = False
         analysis.run_in_thread_flag = False
         analysis.map_canvas = CANVAS
+        # WIP
         # analysis.user_extent_crs(qexposure.extent)
         # analysis.user_extent(extent[0], extent[1], extent[2], extent[3])
         analysis.impact_function = impact_function
@@ -242,12 +158,10 @@ def run_if():
         LOGGER.debug("end analysis :)")
     except Exception as exc:
         print exc.message
-
     try:
         impact_layer = analysis.get_impact_layer()
     except Exception as exc:
         print exc.message
-
     # analysis result output
     if impact_layer is None:
         print "Error : No impact layer generated"
@@ -270,7 +184,7 @@ if __name__ == '__main__':
     print ""
 
     try:
-        # Parse arguments, use file docstring as a parameter definition
+        # Parse arguments, use usage.txt as a parameter definition
         shell_arguments = docopt.docopt(usage)
     except docopt.DocoptExit as e:
         print e.message
