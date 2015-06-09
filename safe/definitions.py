3a8c03f1896333856f05e8e8615af5d3d666717c
Charlotte Morgan <charlotte.morgan@ga.gov.au>
2015-06-09 11:21:51 +0700

    test from desktop

--- safe/definitions.py
+++ safe/definitions.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 """
-InaSAFE Disaster risk assessment tool developed by AusAid -
+InaSAFE Disaster risk assessment tool developed by AusAid / DFAT -
 **New Metadata for SAFE.**
 
 Contact : ole.moller.nielsen@gmail.com
