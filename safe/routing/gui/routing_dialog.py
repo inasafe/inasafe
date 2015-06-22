fedbb505dddbf1c81dde1cbff594ac8eb6632954
Etienne Trimaille <etienne.trimaille@gmail.com>
2015-06-22 17:32:55 +0200

    start accept method in the routing analysis

--- safe/routing/gui/routing_dialog.py
+++ safe/routing/gui/routing_dialog.py
@@ -1,12 +1,16 @@
 # coding=utf-8
 
-import os
 import logging
+from os.path import dirname, join
 
+from processing.core.Processing import Processing
+from processing.modeler import ModelerAlgorithm
+from processing import runalg
 from qgis.gui import QgsMapToolPan
 from qgis.core import QgsMapLayerRegistry
 from PyQt4.QtGui import QDialog
 from PyQt4 import uic
+from PyQt4.QtCore import Qt
 
 from safe.utilities.qgis_utilities import display_information_message_box
 from safe.utilities.keyword_io import KeywordIO
@@ -15,7 +19,7 @@ from safe.utilities.utilities import add_ordered_combo_item
 
 LOGGER = logging.getLogger('InaSAFE')
 
-ui_file = os.path.join(os.path.dirname(__file__), 'routing_dialog_base.ui')
+ui_file = join(dirname(__file__), 'routing_dialog_base.ui')
 FORM_CLASS, BASE_CLASS = uic.loadUiType(ui_file)
 
 
@@ -114,3 +118,83 @@ class RoutingDialog(QDialog, FORM_CLASS):
 
             elif layer_purpose == 'idp':
                 add_ordered_combo_item(self.cbo_idp_layer, title, source)
+
+    def get_hazard_layer(self):
+        """Get the QgsMapLayer currently selected in the hazard combo.
+
+        Obtain QgsMapLayer id from the userrole of the QtCombo for hazard
+        and return it as a QgsMapLayer.
+
+        :returns: The currently selected map layer in the hazard combo.
+        :rtype: QgsMapLayer
+
+        """
+        index = self.cbo_hazard_layer.currentIndex()
+        if index < 0:
+            return None
+        layer_id = self.cbo_hazard_layer.itemData(
+            index, Qt.UserRole)
+        layer = QgsMapLayerRegistry.instance().mapLayer(layer_id)
+        return layer
+
+    def get_idp_layer(self):
+        """Get the QgsMapLayer currently selected in the idp combo.
+
+        Obtain QgsMapLayer id from the userrole of the QtCombo for idp
+        and return it as a QgsMapLayer.
+
+        :returns: Currently selected map layer in the exposure combo.
+        :rtype: QgsMapLayer
+        """
+
+        index = self.cbo_idp_layer.currentIndex()
+        if index < 0:
+            return None
+        layer_id = self.cbo_idp_layer.itemData(
+            index, Qt.UserRole)
+        layer = QgsMapLayerRegistry.instance().mapLayer(layer_id)
+        return layer
+
+    def get_roads_layer(self):
+        """Get the QgsMapLayer currently selected in the exposure combo.
+
+        Obtain QgsMapLayer id from the userrole of the QtCombo for roads
+        and return it as a QgsMapLayer.
+
+        :returns: Currently selected map layer in the exposure combo.
+        :rtype: QgsMapLayer
+        """
+
+        index = self.cbo_roads_layer.currentIndex()
+        if index < 0:
+            return None
+        layer_id = self.cbo_roads_layer.itemData(
+            index, Qt.UserRole)
+        layer = QgsMapLayerRegistry.instance().mapLayer(layer_id)
+        return layer
+
+    @staticmethod
+    def add_processing_model(file_path):
+        model = ModelerAlgorithm.ModelerAlgorithm.fromFile(file_path)
+        model.provider = Processing.modeler
+        Processing.modeler.algs.append(model)
+        command_line = model.commandLineName()
+        Processing.algs['model'][command_line] = model
+        return command_line
+
+    def accept(self):
+        roads_layer = self.get_roads_layer()
+        flood_layer = self.get_hazard_layer()
+        idp_layer = self.get_idp_layer()
+
+        routable_model = join(
+            dirname(dirname(__file__)), 'models', 'routable_layer.model')
+        model = self.add_processing_model(routable_model)
+
+        flood_value_map = self.keyword_io.read_keywords(
+            flood_layer, 'value_map')
+        wet_value = flood_value_map['wet'][0]
+
+        flood_field = self.keyword_io.read_keywords(flood_layer, 'field')
+
+        # TODO
