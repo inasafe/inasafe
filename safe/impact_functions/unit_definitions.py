4817e8757370e54cb2434c521d5daa4a2efc8dce
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2015-07-08 13:38:29 +0700

    Refactor unit definitions

--- safe/impact_functions/unit_definitions.py
+++ safe/impact_functions/unit_definitions.py
@@ -1,48 +1,32 @@
 # coding=utf-8
-from safe.utilities.i18n import tr
+from safe.definitions import unit_generic, unit_metres, unit_mmi, \
+    unit_percentage
 from safe_extras.parameters.unit import Unit
 
 __author__ = 'Rizky Maulana Nugraha "lucernae" <lana.pcfre@gmail.com>'
 __date__ = '11/06/15'
 
 
-def parameter_unit_generic():
+def convert_to_parameter_unit(unit_definition):
     unit = Unit()
-    unit.name = tr('generic')
-    unit.plural = tr('generics')
-    unit.abbreviation = tr('generic')
-    unit.description = tr(
-        'Impact function with generic unit means it can accept the value '
-        'regardless of any unit the data represents')
+    unit.name = unit_definition.get('name')
+    unit.plural = unit_definition.get('plural_name')
+    unit.abbreviation = unit_definition.get('abbreviation')
+    unit.description = unit_definition.get('description')
     return unit
+
+
+def parameter_unit_generic():
+    return convert_to_parameter_unit(unit_generic)
 
 
 def parameter_unit_metres():
-    unit = Unit()
-    unit.name = tr('meter')
-    unit.plural = tr('metres')
-    unit.abbreviation = tr('m')
-    unit.description = tr('Length in meter.')
-    return unit
+    return convert_to_parameter_unit(unit_metres)
 
 
 def parameter_unit_mmi():
-    unit = Unit()
-    unit.name = tr('MMI')
-    unit.plural = tr('MMI')
-    unit.abbreviation = tr('MMI')
-    unit.description = tr(
-        'MMI stands for Modified Mercalli Intensity Scale, to measure the '
-        'intensity of earthquake.')
-    return unit
+    return convert_to_parameter_unit(unit_mmi)
 
 
 def parameter_unit_percentage():
-    unit = Unit()
-    unit.name = tr('percentage')
-    unit.plural = tr('percentages')
-    unit.abbreviation = tr('%%')
-    unit.description = tr(
-        'Percentage values ranges from 0 to 100. It represents a ratio of '
-        'hundred.')
-    return unit
+    return convert_to_parameter_unit(unit_percentage)
