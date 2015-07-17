d23ed99ae2a081ac18570d155db6aa25db52c807
Christian Christelis <christian@kartoza.com>
2015-07-17 19:26:20 +0200

    Adding building post processing to volcano point IF.

--- safe/impact_functions/volcanic/volcano_point_building/metadata_definitions.py
+++ safe/impact_functions/volcanic/volcano_point_building/metadata_definitions.py
@@ -11,6 +11,7 @@ Contact : ole.moller.nielsen@gmail.com
 
 """
 from safe.common.utilities import OrderedDict
+from safe.defaults import building_type_postprocessor
 from safe.impact_functions.impact_function_metadata import \
     ImpactFunctionMetadata
 from safe.impact_functions.volcanic.volcano_point_building\
@@ -108,7 +109,9 @@ class VolcanoPointBuildingFunctionMetadata(ImpactFunctionMetadata):
                 # The list of radii in km for volcano point hazard
                 ('distances', distance()),
                 # The attribute for name of the volcano in hazard layer
-                ('volcano name attribute', volcano_name_attribute())
+                ('volcano name attribute', volcano_name_attribute()),
+                ('postprocessors', OrderedDict([
+                    ('BuildingType', building_type_postprocessor())]))
             ])
         }
         return dict_meta
