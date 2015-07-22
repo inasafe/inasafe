79f7a7de58fbf383dcd44cb11e6d4fe593d1862f
Christian Christelis <christian@kartoza.com>
2015-07-22 22:06:12 +0200

    Improved changelog message. Removed redundant code.

--- safe/impact_functions/volcanic/volcano_polygon_building/metadata_definitions.py
+++ safe/impact_functions/volcanic/volcano_polygon_building/metadata_definitions.py
@@ -12,9 +12,6 @@ Contact : ole.moller.nielsen@gmail.com
 """
 from safe.common.utilities import OrderedDict
 from safe.defaults import building_type_postprocessor
-from safe.impact_functions.impact_function_metadata import \
-    ImpactFunctionMetadata
-from safe.utilities.i18n import tr
 from safe.definitions import (
     hazard_definition,
     hazard_volcano,
