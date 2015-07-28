7339ca2039a270884f74482376764f79b40952c2
Ismail Sunni <imajimatika@gmail.com>
2015-07-28 11:23:33 +0700

    Volcano polygon building uses layer's keywords.

--- safe/impact_functions/volcanic/volcano_polygon_building/impact_function.py
+++ safe/impact_functions/volcanic/volcano_polygon_building/impact_function.py
@@ -19,7 +19,7 @@ from safe.storage.vector import Vector
 from safe.utilities.i18n import tr
 from safe.impact_functions.volcanic.volcano_polygon_building\
     .metadata_definitions import VolcanoPolygonBuildingFunctionMetadata
-from safe.common.exceptions import InaSAFEError
+from safe.common.exceptions import InaSAFEError, KeywordNotFoundError
 from safe.common.utilities import (
     get_thousand_separator,
     get_osm_building_usage)
@@ -83,6 +83,13 @@ class VolcanoPolygonBuildingFunction(
         # Get parameters from layer's keywords
         hazard_zone_attribute = self.hazard.keyword('field')
         name_attribute = self.hazard.keyword('volcano_name_field')
+        # Try to get the value from keyword, if not exist, it will not fail,
+        # but use the old get_osm_building_usage
+        try:
+            structure_class_field = self.exposure.keyword(
+                'structure_class_field')
+        except KeywordNotFoundError:
+            structure_class_field = None
 
         # Input checks
         if not self.hazard.layer.is_polygon_data:
@@ -133,7 +140,11 @@ class VolcanoPolygonBuildingFunction(
             if not hazard_value:
                 hazard_value = self._not_affected_value
             features[i][self.target_field] = hazard_value
-            usage = get_osm_building_usage(attribute_names, features[i])
+            if (structure_class_field and
+                    structure_class_field in attribute_names):
+                usage = features[i][structure_class_field]
+            else:
+                usage = get_osm_building_usage(attribute_names, features[i])
             if usage in [None, 'NULL', 'null', 'Null', 0]:
                 usage = tr('Unknown')
             if usage not in self.buildings:
