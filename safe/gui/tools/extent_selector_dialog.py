40663a3c7229e7851ead5be0161123191ef8d330
Ismail Sunni <imajimatika@gmail.com>
2015-09-21 15:56:19 +0700

    Add help for extent selector dialog

--- safe/gui/tools/extent_selector_dialog.py
+++ safe/gui/tools/extent_selector_dialog.py
@@ -43,11 +43,11 @@ from qgis.core import (
     QgsApplication,
     QgsCoordinateTransform)
 
-from safe import messaging as m
 from safe.utilities.resources import html_header, html_footer, get_ui_class
 
 from safe.gui.tools.rectangle_map_tool import RectangleMapTool
 from safe.messaging import styles
+from safe.gui.tools.help.extent_selector_help import extent_selector_help
 
 INFO_STYLE = styles.INFO_STYLE
 LOGGER = logging.getLogger('InaSAFE')
@@ -202,31 +202,8 @@ class ExtentSelectorDialog(QDialog, FORM_CLASS):
 
         string = header
 
-        heading = m.Heading(self.tr('User Extents Tool'), **INFO_STYLE)
-        body = self.tr(
-            'This tool allows you to specify exactly which geographical '
-            'region should be used for your analysis. You can either '
-            'enter the coordinates directly into the input boxes below '
-            '(using the same CRS as the canvas is currently set to), or '
-            'you can interactively select the area by using the \'select '
-            'on map\' button - which will temporarily hide this window and '
-            'allow you to drag a rectangle on the map. After you have '
-            'finished dragging the rectangle, this window will reappear. '
-            'You can also use one of your bookmarks to set the region. '
-            'If you enable the \'Toggle scenario outlines\' tool on the '
-            'InaSAFE toolbar, your user defined extent will be shown on '
-            'the map as a blue rectangle. Please note that when running '
-            'your analysis, the effective analysis extent will be the '
-            'intersection of the hazard extent, exposure extent and user '
-            'extent - thus the entire user extent area may not be used for '
-            'analysis.'
+        message = extent_selector_help()
 
-        )
-
-        message = m.Message()
-        message.add(m.Brand())
-        message.add(heading)
-        message.add(body)
         string += message.to_html()
         string += footer
 
---
+++ safe/gui/tools/help/extent_selector_help.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+__author__ = 'ismailsunni'
+
+from safe.utilities.i18n import tr
+from safe import messaging as m
+from safe.messaging import styles
+
+INFO_STYLE = styles.INFO_STYLE
+
+def extent_selector_help():
+    """Help message for extent selector dialog.
+
+    :returns: A message object containing helpful information.
+    :rtype: messaging.message.Message
+    """
+    heading = m.Heading(tr('User Extents Tool'), **INFO_STYLE)
+    body = tr(
+        'This tool allows you to specify exactly which geographical '
+        'region should be used for your analysis. You can either '
+        'enter the coordinates directly into the input boxes below '
+        '(using the same CRS as the canvas is currently set to), or '
+        'you can interactively select the area by using the \'select '
+        'on map\' button - which will temporarily hide this window and '
+        'allow you to drag a rectangle on the map. After you have '
+        'finished dragging the rectangle, this window will reappear. '
+        'You can also use one of your bookmarks to set the region. '
+        'If you enable the \'Toggle scenario outlines\' tool on the '
+        'InaSAFE toolbar, your user defined extent will be shown on '
+        'the map as a blue rectangle. Please note that when running '
+        'your analysis, the effective analysis extent will be the '
+        'intersection of the hazard extent, exposure extent and user '
+        'extent - thus the entire user extent area may not be used for '
+        'analysis.'
+
+    )
+
+    message = m.Message()
+    message.add(m.Brand())
+    message.add(heading)
+    message.add(body)
+
+    return message
