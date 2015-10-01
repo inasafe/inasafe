538cf3e29991e46909972eff4853923e0b459977
Ismail Sunni <imajimatika@gmail.com>
2015-10-01 19:58:35 +0700

    Use messaging for notes and action list.

--- safe/impact_reports/road_exposure_report_mixin.py
+++ safe/impact_reports/road_exposure_report_mixin.py
@@ -16,6 +16,7 @@ from safe.utilities.i18n import tr
 from safe.common.utilities import format_int
 import safe.messaging as m
 from safe.impact_reports.report_mixin_base import ReportMixin
+from safe.messaging import styles
 
 
 class RoadExposureReportMixin(ReportMixin):
@@ -122,6 +123,40 @@ class RoadExposureReportMixin(ReportMixin):
 
         message.add(table)
 
+        return message
+
+    def action_checklist(self):
+        """Action checklist for the itb earthquake fatality report.
+
+        .. versionadded:: 3.2.1
+
+        :returns: The action checklist
+        :rtype: safe.messaging.Message
+        """
+        message = m.Message(style_class='container')
+        message.add(m.Heading(tr('Action checklist'), **styles.INFO_STYLE))
+        checklist = m.BulletedList()
+        checklist.add(tr(
+            'Try to Identify which potential roads that able to '
+            'access for evacuation and logistic distribution purpose. '
+            'What type of transportation reliable to use in that order?'))
+        checklist.add(tr(
+            'Identify what equipment needed to open access of '
+            'affected roads. Where the resources or equipment to open '
+            'access affected roads located? who/what department '
+            'responsible to mobilize?'))
+        checklist.add(tr(
+            'Which roads can be used to evacuate people or to '
+            'distribute logistics?'))
+        checklist.add(tr(
+            'What type of vehicles can use the unaffected roads?'))
+        checklist.add(tr(
+            'What sort of equipment will be needed to reopen roads & '
+            'where will we get it?'))
+        checklist.add(tr(
+            'Which government department is responsible for supplying '
+            'equipment ?'))
+        message.add(checklist)
         return message
 
     @property
