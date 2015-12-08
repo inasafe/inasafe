71482d961b4be8f27554514677a6d0ec4ca88a50
Ismail Sunni <imajimatika@gmail.com>
2015-12-08 17:16:15 +0700

    Add test for old .keywords.

--- safe/utilities/test/test_utilities.py
+++ safe/utilities/test/test_utilities.py
@@ -4,6 +4,7 @@
 import unittest
 import os
 import codecs
+import shutil
 from unittest import expectedFailure
 
 from safe.definitions import inasafe_keyword_version
@@ -18,7 +19,8 @@ from safe.utilities.utilities import (
 from safe.utilities.gis import qgis_version
 from safe.test.utilities import (
     test_data_path,
-    get_qgis_app
+    get_qgis_app,
+    clone_raster_layer
 )
 from safe.common.exceptions import KeywordNotFoundError
 from safe.storage.utilities import bbox_intersection
@@ -129,27 +131,28 @@ class UtilitiesTest(unittest.TestCase):
 
     def test_get_keyword_from_file(self):
         """Get keyword from a filesystem file's .keyword file."""
-        raster_shake_path = test_data_path(
-            'hazard', 'jakarta_flood_design.tif')
-        vector_path = test_data_path(
-            'exposure', 'buildings_osm_4326.shp')
-        raster_tsunami_path = test_data_path(
-            'hazard', 'tsunami_wgs84.tif')
+        raster_layer = clone_raster_layer(
+            'jakarta_flood_design', '.tif', False, test_data_path('hazard'))
 
-        keyword = read_file_keywords(raster_shake_path, 'layer_purpose')
+        raster_layer_path = raster_layer.source()
+
+        keyword_file = test_data_path('other', 'jakarta_flood_design.keywords')
+
+        raster_keyword_path = raster_layer_path.split('.')[0] + '.keywords'
+
+        shutil.copy2(keyword_file, raster_keyword_path)
+
+        keyword = read_file_keywords(raster_layer_path, 'layer_purpose')
         expected_keyword = 'hazard'
-        message = (
-            'The keyword "layer_purpose" for %s is %s. Expected keyword is: '
-            '%s') % (raster_shake_path, keyword, expected_keyword)
-        self.assertEqual(keyword, expected_keyword, message)
+        self.assertEqual(keyword, expected_keyword)
 
         # Test we get an exception if keyword is not found
         self.assertRaises(
             KeywordNotFoundError,
-            read_file_keywords, raster_shake_path, 'boguskeyword')
+            read_file_keywords, raster_layer_path, 'boguskeyword')
 
         # Test if all the keywords are all ready correctly
-        keywords = read_file_keywords(raster_shake_path)
+        keywords = read_file_keywords(raster_layer_path)
         expected_keywords = {
             'hazard_category': 'single_event',
             'hazard': 'flood',
@@ -159,37 +162,8 @@ class UtilitiesTest(unittest.TestCase):
             'title': 'Jakarta flood like 2007 with structural improvements',
             'keyword_version': inasafe_keyword_version
         }
-        message = 'Expected:\n%s\nGot:\n%s\n' % (expected_keywords, keywords)
-        self.assertDictEqual(keywords, expected_keywords, message)
+        self.assertDictEqual(keywords, expected_keywords)
 
-        # Test reading keywords from vector layer
-        keywords = read_file_keywords(vector_path)
-        expected_keywords = {
-            'keyword_version': inasafe_keyword_version,
-            'structure_class_field': 'FLOODED',
-            'title': 'buildings_osm_4326',
-            'layer_geometry': 'polygon',
-            'layer_purpose': 'exposure',
-            'layer_mode': 'classified',
-            'exposure': 'structure'
-        }
-        message = 'Expected:\n%s\nGot:\n%s\n' % (expected_keywords, keywords)
-        self.assertDictEqual(keywords, expected_keywords, message)
-
-        # tsunami example
-        keywords = read_file_keywords(raster_tsunami_path)
-        expected_keywords = {
-            'hazard_category': 'single_event',
-            'title': 'Tsunami',
-            'hazard': 'tsunami',
-            'continuous_hazard_unit': 'metres',
-            'layer_geometry': 'raster',
-            'layer_purpose': 'hazard',
-            'layer_mode': 'continuous',
-            'keyword_version': inasafe_keyword_version
-        }
-        message = 'Expected:\n%s\nGot:\n%s\n' % (expected_keywords, keywords)
-        self.assertEqual(keywords, expected_keywords, message)
 
     def test_compare_version(self):
         """Test for compare_version"""
