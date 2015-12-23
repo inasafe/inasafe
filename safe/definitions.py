7168ee44dc8a8624aaf686e55d0640be51b4b982
Ismail Sunni <imajimatika@gmail.com>
2015-12-23 09:26:26 +0700

    Use different keyword version, not taking from metadata.txt

--- safe/definitions.py
+++ safe/definitions.py
@@ -19,10 +19,9 @@ __copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
 
 # Please group them and sort them alphabetical
 from safe.utilities.i18n import tr
-from safe.common.version import get_version
 
 inasafe_keyword_version_key = 'keyword_version'
-inasafe_keyword_version = get_version()
+inasafe_keyword_version = '3.3'
 
 # constants
 small_number = 2 ** -53  # I think this is small enough
