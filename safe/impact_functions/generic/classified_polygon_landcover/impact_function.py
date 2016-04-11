65096403d5a87c071aef5f694b4a603190ba1193
Martin Dobias <wonder.sk@gmail.com>
2016-04-11 18:34:29 +0800

    Experimental bar chart for each land cover class in report

--- safe/impact_functions/generic/classified_polygon_landcover/impact_function.py
+++ safe/impact_functions/generic/classified_polygon_landcover/impact_function.py
@@ -218,23 +218,40 @@ class ClassifiedPolygonHazardLandCoverFunction(
 
 # non-member private functions used within this module
 
+def _svg_bar_chart_hazard(levels, max_level):
 
-def format_pivot_table(pivot_table, caption=None, header_text='', total_columns=False, total_rows=False):
+    levels_percent = [ round(level * 100. / max_level) for level in levels ]
+    return """
+    <svg width="100%%" height="16">
+      <rect x="0" y="0" width="%d%%" height="4" style="fill:rgb(255,180,180)" />
+      <rect x="0" y="6" width="%d%%" height="4" style="fill:rgb(240,240,50)" />
+      <rect x="0" y="12" width="%d%%" height="4" style="fill:rgb(180,255,180)" />
+    </svg>""" % (levels_percent[0], levels_percent[1], levels_percent[2])
+
+
+def format_pivot_table(pivot_table, caption=None, header_text='', total_columns=False, total_rows=False, bar_chart=False):
 
     table = m.Table(style_class='table table-condensed table-striped')
     table.caption = caption
 
     row = m.Row()
     row.add(m.Cell(header_text, header=True))
+    if bar_chart:
+        row.add(m.Cell('', header=True, attributes='width="100%"'))
     for column_name in pivot_table.columns:
         row.add(m.Cell(column_name, header=True))
     if total_rows:
         row.add(m.Cell(tr('All'), header=True))
     table.add(row)
 
+    max_value = max(max(row) for row in pivot_table.data)
+
     for row_name, data_row, total_row in zip(pivot_table.rows, pivot_table.data, pivot_table.total_rows):
         row = m.Row()
         row.add(m.Cell(row_name))
+        if bar_chart:
+            svg = _svg_bar_chart_hazard(data_row, max_value)
+            row.add(m.Cell(svg, header=False))
         for column_value in data_row:
             row.add(m.Cell(format_decimal(0.1, column_value), align='right'))
         if total_rows:
@@ -244,6 +261,8 @@ def format_pivot_table(pivot_table, caption=None, header_text='', total_columns=
     if total_columns:
         row = m.Row()
         row.add(m.Cell(tr('All'), header=True))
+        if bar_chart:
+            row.add(m.Cell('', header=False))
         for column_value in pivot_table.total_columns:
             row.add(m.Cell(format_decimal(0.1, column_value), align='right', header=True))
         if total_rows:
@@ -315,7 +334,8 @@ def _format_report(report_data):
 
     table = format_pivot_table(report_data['impacted'],
                                header_text=affected_text,
-                               total_columns=True)
+                               total_columns=True,
+                               bar_chart=True)
     message.add(table)
 
     if 'impacted_zones' in report_data:
@@ -323,7 +343,8 @@ def _format_report(report_data):
             message.add(m.Heading(zone, **INFO_STYLE))
             m_table = format_pivot_table(table,
                                          header_text=affected_text,
-                                         total_columns=True)
+                                         total_columns=True,
+                                         bar_chart=True)
             message.add(m_table)
 
     return message.to_html(suppress_newlines=True)
