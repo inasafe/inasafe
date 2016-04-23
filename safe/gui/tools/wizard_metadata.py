d4c48463c06b30339aa472ef3533dd1f831d18c0
Ismail Sunni <imajimatika@gmail.com>
2016-04-23 13:30:54 +0700

    Update structure and road value mapping.

--- safe/gui/tools/wizard_metadata.py
+++ safe/gui/tools/wizard_metadata.py
@@ -20,143 +20,151 @@ __date__ = '12/04/2016'
 __copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
                  'Disaster Reduction')
 
-
 from safe.utilities.i18n import tr
 
-road_class_mapping = [{
-    'key': 'Motorway / highway',
-    'name': tr('Motorway / highway'),
-    'description': tr('Motorway / highway'),
-    'string_defaults': ['Motorway or highway']
-}, {
-    'key': 'Motorway link',
-    'name': tr('Motorway link'),
-    'description': tr('Motorway link'),
-    'string_defaults': ['Motorway link']
-}, {
-    'key': 'Primary road',
-    'name': tr('Primary road'),
-    'description': tr('Primary road'),
-    'string_defaults': ['Primary road']
-}, {
-    'key': 'Primary link',
-    'name': tr('Primary link'),
-    'description': tr('Primary link'),
-    'string_defaults': ['Primary link']
-}, {
-    'key': 'Tertiary',
-    'name': tr('Tertiary'),
-    'description': tr('Tertiary'),
-    'string_defaults': ['Tertiary']
-}, {
-    'key': 'Tertiary link',
-    'name': tr('Tertiary link'),
-    'description': tr('Tertiary link'),
-    'string_defaults': ['Tertiary link']
-}, {
-    'key': 'Secondary',
-    'name': tr('Secondary'),
-    'description': tr('Secondary'),
-    'string_defaults': ['Secondary']
-}, {
-    'key': 'Secondary link',
-    'name': tr('Secondary link'),
-    'description': tr('Secondary link'),
-    'string_defaults': ['Secondary link']
-}, {
-    'key': 'Road, residential, living street, etc.',
-    'name': tr('Road, residential, living street, etc.'),
-    'description': tr('Road, residential, living street, etc.'),
-    'string_defaults': ['Road, residential, living street, etc.']
-}, {
-    'key': 'Track',
-    'name': tr('Track'),
-    'description': tr('Track'),
-    'string_defaults': ['Track']
-}, {
-    'key': 'Cycleway, footpath, etc.',
-    'name': tr('Cycleway, footpath, etc.'),
-    'description': tr('Cycleway, footpath, etc.'),
-    'string_defaults': ['Cycleway, footpath, etc.']
-}, {
-    'key': 'Other',
-    'name': tr('Other'),
-    'description': tr('Other'),
-    'string_defaults': []
-}]
+road_class_mapping = [
+    {
+        'key': 'motorway',
+        'name': tr('Motorway'),
+        'description': tr('A road to solve the traffic and have a fare.'),
+        'string_defaults': ['Motorway']
+    },
+    {
+        'key': 'primary',
+        'name': tr('Primary'),
+        'description': tr('A road that service the main transportation with a '
+                          'long distance travel characteristic and high '
+                          'average velocity.'),
+        'string_defaults': ['Primary']
+    },
+    {
+        'key': 'secondary',
+        'name': tr('Secondary'),
+        'description': tr('A road that service the transportation with a '
+                          'medium distance travel characteristic and medium '
+                          'average velocity.'),
+        'string_defaults': ['Secondary']
+    },
+    {
+        'key': 'local',
+        'name': tr('Local'),
+        'description': tr('A road that service the transportation with short '
+                          'distance travel and low average velocity.'),
+        'string_defaults': ['Local']
+    },
+    {
+        'key': 'other',
+        'name': tr('Other'),
+        'description': tr('A road that service the transportation with short '
+                          'travel and low average velocity.'),
+        'string_defaults': ['Other']
+    },
+    {
+        'key': 'path',
+        'name': tr('Path'),
+        'description': tr('A road to walk on foot aim.'),
+        'string_defaults': ['Path', 'Track']
+    }
+]
 
-
-structure_class_mapping = [{
-    'key': 'Medical',
-    'name': tr('Medical'),
-    'description': tr('Medical'),
-    'string_defaults': ['Clinic/Doctor', 'Hospital']
-}, {
-    'key': 'Schools',
-    'name': tr('Schools'),
-    'description': tr('Schools'),
-    'string_defaults': ['School', 'University/College']
-}, {
-    'key': 'Places of worship',
-    'name': tr('Places of worship'),
-    'description': tr('Places of worship'),
-    'string_defaults': ['Place of Worship - Unitarian',
-                        'Place of Worship - Islam',
-                        'Place of Worship - Buddhist',
-                        'Place of Worship']
-}, {
-    'key': 'Residential',
-    'name': tr('Residential'),
-    'description': tr('Residential'),
-    'string_defaults': ['Residential']
-}, {
-    'key': 'Government',
-    'name': tr('Government'),
-    'description': tr('Government'),
-    'string_defaults': ['Government']
-}, {
-    'key': 'Public Building',
-    'name': tr('Public Building'),
-    'description': tr('Public Building'),
-    'string_defaults': ['Public Building']
-}, {
-    'key': 'Fire Station',
-    'name': tr('Fire Station'),
-    'description': tr('Fire Station'),
-    'string_defaults': ['Fire Station']
-}, {
-    'key': 'Police Station',
-    'name': tr('Police Station'),
-    'description': tr('Police Station'),
-    'string_defaults': ['Police Station']
-}, {
-    'key': 'Supermarket',
-    'name': tr('Supermarket'),
-    'description': tr('Supermarket'),
-    'string_defaults': ['Supermarket']
-}, {
-    'key': 'Commercial',
-    'name': tr('Commercial'),
-    'description': tr('Commercial'),
-    'string_defaults': ['Commercial']
-}, {
-    'key': 'Industrial',
-    'name': tr('Industrial'),
-    'description': tr('Industrial'),
-    'string_defaults': ['Industrial']
-}, {
-    'key': 'Utility',
-    'name': tr('Utility'),
-    'description': tr('Utility'),
-    'string_defaults': ['Utility']
-}, {
-    'key': 'Sports Facility',
-    'name': tr('Sports Facility'),
-    'description': tr('Sports Facility'),
-    'string_defaults': ['Sports Facility']
-}, {
-    'key': 'Other',
-    'name': tr('Other'),
-    'description': tr('Other'),
-    'string_defaults': []
-}]
+structure_class_mapping = [
+    {
+        'key': 'education',
+        'name': tr('Education'),
+        'description': tr('An object that has a service in education sector.'),
+        'string_defaults': [
+            'Kindergarten', 'College', 'School', 'University'
+        ]
+    },
+    {
+        'key': 'health',
+        'name': tr('Health'),
+        'description': tr(
+            'An object that has a service and facility in health sector.'),
+        'string_defaults': [
+            'Clinic', 'Doctor', 'Hospital', 'Dentist', 'Pharmacy'
+        ]
+    },
+    {
+        'key': 'transportation',
+        'name': tr('Transportation'),
+        'description': tr(
+            'An object that has a service and facility in public '
+            'transportation.'),
+        'string_defaults': [
+            'bus stop', 'bus station', 'station', 'ferry terminal',
+            'aerodrome', 'airport', 'terminal']
+    },
+    {
+        'key': 'place of worship',
+        'name': tr('Place of Worship'),
+        'description': tr(
+            'An object that used to pray or related to religion activity.'),
+        'string_defaults': [
+            'Place of Worship - Islam',
+            'Place of Worship - Buddhist',
+            'Place of Worship - Christian',
+            'Place of Worship - Hindu',
+            'Place of Worship'
+        ]
+    },
+    {
+        'key': 'government',
+        'name': tr('Government'),
+        'description': tr(
+            'A building that used to doing government activity in public '
+            'service or the other government activity.'),
+        'string_defaults': ['Government']
+    },
+    {
+        'key': 'economy',
+        'name': tr('Economy'),
+        'description': tr(
+            'A building that used to trade / buy and sell activity or an '
+            'object that has an economy activity.'),
+        'string_defaults': [
+            'Supermarket', 'shop', 'market', 'tailor', 'warehouse', 'works',
+            'convenience', 'seafood', 'atm', 'mall', 'clothes', 'shoes'
+        ]
+    },
+    {
+        'key': 'recreation and entertainment',
+        'name': tr('Recreation and Entertainment'),
+        'description': tr(
+            'An Object that provide an entertainment or recreation '
+            'facilities.'),
+        'string_defaults': [
+            'amusement arcade', 'cinema', 'zoo', 'museum', 'theatre'
+        ]
+    },
+    {
+        'key': 'sport',
+        'name': tr('Sport'),
+        'description': tr(
+            'An object that has a sport facility and people can use it.'),
+        'string_defaults': ['stadium', 'sport centre', 'pitch']
+    },
+    {
+        'key': 'public facility',
+        'name': tr('Public Facility'),
+        'description': tr(
+            'An object that provide a service or facility to public like '
+            'toilet, library, convention hall, etc.'),
+        'string_defaults': ['library', 'toiler', 'convention hall', 'prison']
+    },
+    {
+        'key': 'accommodation',
+        'name': tr('Accommodation'),
+        'description': tr(
+            'An object that provide an accommodation / lodging or food '
+            'services.'),
+        'string_defaults': ['restaurant', 'cafe', 'fast food', 'hotel']
+    },
+    {
+        'key': 'other',
+        'name': tr('Other'),
+        'description': tr(
+            'An object that be found in Indonesia, and frequently mapped.'),
+        'string_defaults': ['Animal Boarding', 'Water Well', 'Lighthouse']
+    }
+]
