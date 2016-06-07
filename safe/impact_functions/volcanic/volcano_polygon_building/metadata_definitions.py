8b3fd19e07ca834d12b8012d51d8205e5b898e3c
Etienne Trimaille <etienne.trimaille@gmail.com>
2016-06-07 21:26:35 +0200

    move layer name and map parameters to metadata in volcano IF

--- safe/impact_functions/volcanic/volcano_polygon_building/metadata_definitions.py
+++ safe/impact_functions/volcanic/volcano_polygon_building/metadata_definitions.py
@@ -10,7 +10,7 @@ Contact : ole.moller.nielsen@gmail.com
      (at your option) any later version.
 
 """
-from safe.common.utilities import OrderedDict
+from safe.common.utilities import OrderedDict, get_thousand_separator
 from safe.defaults import building_type_postprocessor
 from safe.impact_functions.impact_function_metadata import \
     ImpactFunctionMetadata
@@ -81,6 +81,13 @@ class VolcanoPolygonBuildingFunctionMetadata(ImpactFunctionMetadata):
                 'within each hazard zone.'),
             'limitations': [],
             'citations': [],
+            'map_title': tr('Buildings affected by volcanic hazard zone'),
+            'legend_title': tr('Building count'),
+            'legend_units': tr('(building)'),
+            'legend_notes': tr(
+                'Thousand separator is represented by %s' %
+                get_thousand_separator()),
+            'layer_name': tr('Buildings affected by volcanic hazard zone'),
             'layer_requirements': {
                 'hazard': {
                     'layer_mode': layer_mode_classified,
