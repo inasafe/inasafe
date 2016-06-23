7c2aa13f509e8021c1b9bbfbd8c1e097cd2259fe
Etienne Trimaille <gustrimaille@yahoo.fr>
2016-06-23 14:45:38 +0200

    fix qgis_testrunner output

--- qgis_testrunner.sh
+++ qgis_testrunner.sh
@@ -7,15 +7,14 @@ TEST_NAME=$1
 
 cd /tests_directory
 echo "Running test $1 ..."
-OUTPUT=$(`unbuffer qgis --nologo --code /usr/bin/qgis_testrunner.py $TEST_NAME  2>/dev/null | tee /dev/tty`)
+OUTPUT=$(unbuffer qgis --nologo --code /usr/bin/qgis_testrunner.py $TEST_NAME  2>/dev/null | tee /dev/tty)
 EXIT_CODE="$?"
 if [ -z "$OUTPUT" ]; then
     echo "ERROR: no output from the test runner! (exit code: ${EXIT_CODE})"
     exit 1
 fi
-echo $OUTPUT | grep -q FAILED
+echo $OUTPUT | grep -q "FAILED (failures"
 RESULT_CODE="$?"
-echo "$OUTPUT"
 echo "Finished running test $1."
 if [ "$RESULT_CODE" != "0" ];
     then exit 0;
