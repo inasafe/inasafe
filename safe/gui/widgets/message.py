f08a60b6dbd3cec2b816ddbbcf63e04d22921c2e
Ismail Sunni <imajimatika@gmail.com>
2016-07-12 10:56:02 +0700

    Add header.

--- safe/gui/widgets/message.py
+++ safe/gui/widgets/message.py
@@ -1,3 +1,18 @@
+# coding=utf-8
+"""
+InaSAFE Disaster risk assessment tool developed by AusAid -
+**Special Message Sender.**
+
+Contact : ole.moller.nielsen@gmail.com
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+.. todo:: Check raster is single band
+
+"""
 __author__ = 'ismailsunni'
 __project_name__ = 'inasafe-dev'
 __filename__ = 'message.py'
