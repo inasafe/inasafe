b04125d2a3ce4b6db79d1a88328efa8f85f9ff22
Ismail Sunni <imajimatika@gmail.com>
2016-07-13 17:56:30 +0700

    Cleaner approach.

--- safe/impact_functions/ash/ash_raster_population/impact_function.py
+++ safe/impact_functions/ash/ash_raster_population/impact_function.py
@@ -16,24 +16,20 @@ from safe.common.exceptions import ZeroImpactException
 
 from safe.impact_functions.bases.continuous_rh_continuous_re import \
     ContinuousRHContinuousRE
-from safe.utilities.utilities import ranges_according_thresholds_list
-import safe.messaging as m
 
 from safe.impact_functions.core import (
-    population_rounding,
-    has_no_data
+    has_no_data,
+    no_population_impact_message
 )
-from safe.impact_functions.impact_function_manager import ImpactFunctionManager
 from safe.storage.raster import Raster
 from safe.utilities.i18n import tr
 from safe.common.utilities import (
-    verify,
     humanize_class,
     create_classes,
     create_label)
 
 from safe.gui.tools.minimum_needs.needs_profile import add_needs_parameters, \
-    filter_needs_parameters, get_needs_provenance_value
+    filter_needs_parameters
 from safe.impact_reports.population_exposure_report_mixin import \
     PopulationExposureReportMixin
 
@@ -73,29 +69,15 @@ class AshRasterPopulationFunction(
 
     def run(self):
         """Run the impact function.
-
-        :returns: A vector layer with affected areas marked.
-        :type: safe_layer
         """
         # Range for ash hazard
         group_parameters = self.parameters['group_threshold']
-        ver_low_unit = group_parameters.value_map['very_low_threshold'].unit
-        unaffected_threshold = group_parameters.value_map[
-            'unaffected_threshold']
-        unaffected_max = unaffected_threshold.value
+        unaffected_max = group_parameters.value_map[
+            'unaffected_threshold'].value
         very_low_max = group_parameters.value_map['very_low_threshold'].value
         low_max = group_parameters.value_map['low_threshold'].value
         medium_max = group_parameters.value_map['moderate_threshold'].value
         high_max = group_parameters.value_map['high_threshold'].value
-        unit_abbrev = ver_low_unit.abbreviation
-
-        hazard_level_range = [
-            unaffected_max,
-            very_low_max,
-            low_max,
-            medium_max,
-            high_max,
-        ]
 
         # Extract hazard data as numeric arrays
         ash = self.hazard.layer.get_data(nan=True)  # Thickness
@@ -107,61 +89,83 @@ class AshRasterPopulationFunction(
         if has_no_data(population):
             self.no_data_warning = True
 
-        # merely initialize
-        impact = None
-        for i, lo in enumerate(hazard_level_range):
-            thresholds_name = self.hazard_classes[i]
-            if i == len(hazard_level_range) - 1:
-                # The last threshold
-                impact = medium = numpy.where(ash >= hazard_level_range[0], population, 0)
-                self.impact_category_ordering.append(thresholds_name)
-                self._evacuation_category = thresholds_name
-            else:
-                # Intermediate thresholds
-                hi = hazard_level_range[i + 1]
-                medium = numpy.where(
-                    (ash >= lo) * (ash < hi), population, 0)
+        # Create 5 data for each hazard level. Get the value of the exposure
+        # if the exposure is in the hazard zone, else just assign 0
+        unaffected_exposure = numpy.where(ash < unaffected_max, population, 0)
+        very_low_exposure = numpy.where(
+            (ash >= unaffected_max) & (ash < very_low_max), population, 0)
+        low_exposure = numpy.where(
+            (ash >= very_low_max) & (ash < low_max), population, 0)
+        medium_exposure = numpy.where(
+            (ash >= low_max) & (ash < medium_max), population, 0)
+        high_exposure = numpy.where(
+            (ash >= medium_max) & (ash < high_max), population, 0)
+        very_high_exposure = numpy.where(ash >= high_max, population, 0)
 
-            # Count
-            val = int(numpy.nansum(medium))
-            self.affected_population[thresholds_name] = val
-
-        self.impact_category_ordering.reverse()
-
-        # Carry the no data values forward to the impact layer.
-        impact = numpy.where(numpy.isnan(population), numpy.nan, impact)
-        impact = numpy.where(numpy.isnan(ash), numpy.nan, impact)
+        impacted_exposure = (
+            very_low_exposure +
+            low_exposure +
+            medium_exposure +
+            high_exposure +
+            very_high_exposure
+        )
 
         # Count totals
         self.total_population = int(numpy.nansum(population))
-        self.unaffected_population = (
-            self.total_population - self.total_affected_population)
+        self.affected_population[
+            tr('Population in very low hazard zone')] = int(
+            numpy.nansum(very_low_exposure))
+        self.affected_population[
+            tr('Population in low hazard zone')] = int(
+            numpy.nansum(low_exposure))
+        self.affected_population[
+            tr('Population in medium hazard zone')] = int(
+            numpy.nansum(medium_exposure))
+        self.affected_population[
+            tr('Population in high hazard zone')] = int(
+            numpy.nansum(high_exposure))
+        self.affected_population[
+            tr('Population in very high hazard zone')] = int(
+            numpy.nansum(very_high_exposure))
+        self.unaffected_population = int(
+            numpy.nansum(unaffected_exposure))
 
+        # check for zero impact
+        if self.total_affected_population == 0:
+            message = no_population_impact_message(self.question)
+            raise ZeroImpactException(message)
+
+        # Don't show digits less than a 1000
         self.minimum_needs = [
             parameter.serialize() for parameter in
             filter_needs_parameters(self.parameters['minimum needs'])
             ]
+        total_needs = self.total_needs
 
-        # check for zero impact
-        if sum(self.affected_population.values()) == 0:
-            message = m.Message()
-            message.add(self.question)
-            message.add(
-                tr('No people in %.1f m of water') % hazard_level_range[-1])
-            message = message.to_html(suppress_newlines=True)
-            raise ZeroImpactException(message)
-
-        # Create style
+        # Style for impact layer
         colours = [
-            '#2C6BA4', '#00A4D8', '#FFEF36', '#EFA951',
-            '#D62631']
-        classes = create_classes(impact.flat[:], len(colours))
+            '#FFFFFF', '#38A800', '#79C900', '#CEED00',
+            '#FFCC00', '#FF6600', '#FF0000', '#7A0000']
+        classes = create_classes(impacted_exposure.flat[:], len(colours))
         interval_classes = humanize_class(classes)
         style_classes = []
 
         for i in xrange(len(colours)):
             style_class = dict()
-            label = self.hazard_classes[i]
+            if i == 1:
+                label = create_label(
+                    interval_classes[i],
+                    tr('Low Population [%i people/cell]' % classes[i]))
+            elif i == 4:
+                label = create_label(
+                    interval_classes[i],
+                    tr('Medium Population [%i people/cell]' % classes[i]))
+            elif i == 7:
+                label = create_label(
+                    interval_classes[i],
+                    tr('High Population [%i people/cell]' % classes[i]))
+            else:
+                label = create_label(interval_classes[i])
             style_class['label'] = label
             style_class['quantity'] = classes[i]
             style_class['transparency'] = 0
@@ -175,21 +179,19 @@ class AshRasterPopulationFunction(
 
         impact_data = self.generate_data()
 
-
         extra_keywords = {
             'map_title': self.metadata().key('map_title'),
             'legend_notes': self.metadata().key('legend_notes'),
             'legend_units': self.metadata().key('legend_units'),
             'legend_title': self.metadata().key('legend_title'),
-            'evacuated': self.total_evacuated,
-            'total_needs': self.total_needs
+            'total_needs': total_needs
         }
 
         impact_layer_keywords = self.generate_impact_keywords(extra_keywords)
 
         # Create raster object and return
         impact_layer = Raster(
-            impact,
+            data=impacted_exposure,
             projection=self.hazard.layer.get_projection(),
             geotransform=self.hazard.layer.get_geotransform(),
             name=self.metadata().key('layer_name'),
