876e5f5f48f9ec2c872bb32698806bdd5d0d324e
Ismail Sunni <imajimatika@gmail.com>
2016-09-22 06:44:14 +0700

    Fix merging problem.

--- safe/new_impact_function/test/test_impact_function.py
+++ safe/new_impact_function/test/test_impact_function.py
@@ -5,7 +5,8 @@ import os
 from safe.test.utilities import get_qgis_app, standard_data_path
 QGIS_APP, CANVAS, IFACE, PARENT = get_qgis_app()
 
-from safe.definitions import post_processor_gender, post_processor_value
+from safe.definitionsv4.definitions_v3 import (
+    post_processor_gender, post_processor_value)
 from safe.test.utilities import clone_shp_layer
 from safe.utilities.keyword_io import KeywordIO
 from safe.new_impact_function.impact_function import ImpactFunction
