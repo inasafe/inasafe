cff493d746343b99a503cff317a19faa08ce654c
Ismail Sunni <imajimatika@gmail.com>
2016-09-28 10:35:52 +0700

    Add description in the script.

--- safe/definitionsv4/test/scripts.py
+++ safe/definitionsv4/test/scripts.py
@@ -5,10 +5,11 @@ def generate_field_table():
     """Generating all field table in markdown."""
     for key, fields in all_fields.items():
         print '### %s' % key
-        print '| Key | Name | Field Name |'
-        print '| --- | ---- | ---------- |'
+        print '| Key | Name | Field Name | Description |'
+        print '| --- | ---- | ---------- | ----------- |'
         for i in fields:
-            print '| %s | %s | %s |' % (i['key'], i['name'], i['field_name'])
+            print ('| %s | %s | %s | %s |' %
+                   (i['key'], i['name'], i['field_name'], i['description']))
 
 if __name__ == '__main__':
     generate_field_table()
