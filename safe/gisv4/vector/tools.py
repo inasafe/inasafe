53d8271daeb296510c8ed13da5d2973e2a78bf46
Etienne Trimaille <gustrimaille@yahoo.fr>
2016-10-05 10:56:38 +0700

    refactor vector layer functions into tools

--- safe/gisv4/vector/tools.py
+++ safe/gisv4/vector/tools.py
@@ -11,7 +11,8 @@ Contact : ole.moller.nielsen@gmail.com
 """
 
 from uuid import uuid4
-from qgis.core import QgsVectorLayer, QgsCoordinateReferenceSystem, QGis
+from qgis.core import (
+    QgsVectorLayer, QgsCoordinateReferenceSystem, QGis, QgsFeature, QgsField)
 
 from safe.common.exceptions import MemoryLayerCreationError
 
@@ -57,3 +58,77 @@ def create_memory_layer(
         memory_layer.updateFields()
 
     return memory_layer
+
+
+def copy_layer(source, target):
+    """Copy a vector layer to another one.
+
+    :param source: The vector layer to copy.
+    :type source: QgsVectorLayer
+
+    :param target: The destination.
+    :type source: QgsVectorLayer
+    """
+    out_feature = QgsFeature()
+    data_provider = target.dataProvider()
+
+    for i, feature in enumerate(source.getFeatures()):
+        geom = feature.geometry()
+        out_feature.setGeometry(geom)
+        out_feature.setAttributes(feature.attributes())
+        data_provider.addFeatures([out_feature])
+
+
+def copy_fields(layer, fields_to_copy):
+    """Copy fields inside an attribute table.
+
+    :param layer: The vector layer.
+    :type layer: QgsVectorLayer
+
+    :param fields_to_copy: Dictionary of fields to copy.
+    :type fields_to_copy: dict
+    """
+    for field in fields_to_copy:
+
+        index = layer.fieldNameIndex(field)
+        if index != -1:
+
+            layer.startEditing()
+
+            source_field = layer.fields().at(index)
+            new_field = QgsField(source_field)
+            new_field.setName(fields_to_copy[field])
+
+            layer.addAttribute(new_field)
+
+            new_index = layer.fieldNameIndex(fields_to_copy[field])
+
+            for feature in layer.getFeatures():
+                attributes = feature.attributes()
+                source_value = attributes[index]
+                layer.changeAttributeValue(
+                    feature.id(), new_index, source_value)
+
+            layer.commitChanges()
+            layer.updateFields()
+
+
+def remove_fields(layer, fields_to_remove):
+    """Remove fields from a vector layer.
+
+    :param layer: The vector layer.
+    :type layer: QgsVectorLayer
+
+    :param fields_to_remove: List of fields to copy.
+    :type fields_to_remove: list
+    """
+    index_to_remove = []
+    data_provider = layer.dataProvider()
+
+    for field in fields_to_remove:
+        index = layer.fieldNameIndex(field)
+        if index != -1:
+            index_to_remove.append(index)
+
+    data_provider.deleteAttributes(index_to_remove)
+    layer.updateFields()
