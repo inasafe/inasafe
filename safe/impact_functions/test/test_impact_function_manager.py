0d1249af89138243ab93336158c17f0fbde184af
Ismail Sunni <imajimatika@gmail.com>
2016-10-20 16:07:17 +0700

    Remove unused field and test.

--- safe/impact_functions/test/test_impact_function_manager.py
+++ safe/impact_functions/test/test_impact_function_manager.py
@@ -25,31 +25,10 @@ QGIS_APP, CANVAS, IFACE, PARENT = get_qgis_app()
 
 from safe.impact_functions.loader import register_impact_functions
 from safe.impact_functions.impact_function_manager import ImpactFunctionManager
-from safe.impact_functions.earthquake.itb_earthquake_fatality_model\
-    .impact_function import ITBFatalityFunction
-from safe.impact_functions.earthquake.pager_earthquake_fatality_model \
-    .impact_function import PAGFatalityFunction
-from safe.impact_functions.earthquake.itb_bayesian_earthquake_fatality_model \
-    .impact_function import ITBBayesianFatalityFunction
 
-from safe.definitionsv4.layer_purposes import (
-    layer_purpose_exposure, layer_purpose_hazard)
-from safe.definitionsv4.layer_modes import layer_mode_continuous, \
-    layer_mode_classified
 from safe.definitionsv4.fields import structure_class_field
-from safe.definitionsv4.exposure import exposure_population, exposure_road, \
-    exposure_structure, exposure_place, exposure_land_cover
-from safe.definitionsv4.hazard import (
-    hazard_generic,
-    hazard_earthquake,
-    hazard_flood,
-    hazard_volcanic_ash,
-    hazard_tsunami,
-    hazard_volcano)
-from safe.definitionsv4.hazard_category import hazard_category_single_event, \
-    hazard_category_multiple_event
-from safe.definitionsv4.layer_geometry import layer_geometry_polygon, \
-    layer_geometry_raster
+
+from safe.definitionsv4.hazard import hazard_earthquake
 
 
 class TestImpactFunctionManager(unittest.TestCase):
@@ -79,37 +58,6 @@ class TestImpactFunctionManager(unittest.TestCase):
             'enabled impact functions' % (expected_result, result))
         self.assertEqual(result, expected_result, message)
 
-    @unittest.skip('Skip unit test from InaSAFE v3.')
-    def test_get_function_title(self):
-        """TestImpactFunctionManager: Test getting function title."""
-        impact_function_title = ImpactFunctionManager().get_function_title(
-            ITBFatalityFunction)
-        expected_title = 'Be flooded'
-        message = 'Expecting %s but got %s' % (
-            impact_function_title, expected_title)
-        self.assertEqual(
-            impact_function_title, expected_title, message)
-
-    @unittest.skip('This test is not a test.')
-    def test_get_all_layer_requirements(self):
-        """Test to generate all layer requirements from all IFs."""
-        impact_function_manager = ImpactFunctionManager()
-        for impact_function in impact_function_manager.impact_functions:
-            # from pprint import pprint
-            print '##', impact_function.metadata().get_name()
-            layer_req = impact_function.metadata().get_layer_requirements()
-            for key, value in layer_req.iteritems():
-                print '###', key
-                for k, v in value.iteritems():
-                    print '1. ', k
-                    if isinstance(v, dict):
-                        print '\t-', v['key']
-                    else:
-                        for the_v in v:
-                            print '\t-', the_v['key']
-                print ''
-            print ''
-
     def test_available_hazards(self):
         """Test available_hazards API."""
         impact_function_manager = ImpactFunctionManager()
@@ -119,45 +67,6 @@ class TestImpactFunctionManager(unittest.TestCase):
         expected_result = [hazard_earthquake]
         message = ('I expect %s but I got %s.' % (expected_result, result))
         self.assertItemsEqual(result, expected_result, message)
-
-    @unittest.skip('Skip unit test from InaSAFE v3.')
-    def test_available_exposures(self):
-        """Test available_exposures API."""
-        impact_function_manager = ImpactFunctionManager()
-        result = impact_function_manager.available_exposures()
-        expected_result = [
-            exposure_structure,
-            exposure_road,
-            exposure_population,
-            exposure_place,
-            exposure_land_cover]
-        self.assertItemsEqual(result, expected_result)
-
-    @unittest.skip('Skip unit test from InaSAFE v3.')
-    def test_functions_for_constraint(self):
-        """Test functions_for_constraint."""
-        ifm = ImpactFunctionManager()
-        impact_functions = ifm.functions_for_constraint(
-            'earthquake',
-            'population',
-            'raster',
-            'raster',
-            'continuous',
-            'continuous',
-        )
-        expected = [
-            ITBFatalityFunction.metadata().as_dict(),
-            ITBBayesianFatalityFunction.metadata().as_dict(),
-            PAGFatalityFunction.metadata().as_dict()]
-
-        for key in impact_functions[0].keys():
-            if key == 'parameters':
-                # We do not check the parameters since they are mutable.
-                continue
-            result = [x[key] for x in impact_functions]
-            hope = [x[key] for x in expected]
-            message = key
-            self.assertItemsEqual(result, hope, message)
 
     def test_exposure_class_fields(self):
         """Test for exposure_class_fields."""
