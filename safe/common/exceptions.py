ff8301c8571b6826a1c8fbc54e9ecc42f2a0e932
Ismail Sunni <imajimatika@gmail.com>
2016-10-27 16:50:59 +0700

    Delete unused test and PEP8.

--- safe/common/exceptions.py
+++ safe/common/exceptions.py
@@ -436,5 +436,7 @@ class ErrorDataStore(InaSAFEError):
     """When the datastore has an error."""
     pass
 
+
 class InvalidWizardStep(InaSAFEError):
     """When there is an invalid wizard step."""
+    pass
