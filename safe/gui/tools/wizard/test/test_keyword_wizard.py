4047f969c71ad47b71190a98f1b2f7138160b929
Etienne Trimaille <gustrimaille@yahoo.fr>
2016-11-08 16:23:57 +0700

    move census.geojson to test directory

--- safe/gui/tools/wizard/test/test_keyword_wizard.py
+++ safe/gui/tools/wizard/test/test_keyword_wizard.py
@@ -928,7 +928,7 @@ class TestKeywordWizard(unittest.TestCase):
     def test_exposure_population_polygon_keyword(self):
         """Test exposure population polygon keyword"""
         layer = load_test_vector_layer(
-            'gisv4', 'exposure', 'census.geojson', clone_to_memory=True)
+            'exposure', 'census.geojson', clone_to_memory=True)
         layer.keywords = {}
 
         self.assertIsNotNone(layer)
@@ -1060,7 +1060,7 @@ class TestKeywordWizard(unittest.TestCase):
     def test_existing_exposure_population_polygon_keyword(self):
         """Test existing exposure population polygon keyword"""
         layer = load_test_vector_layer(
-            'gisv4', 'exposure', 'census.geojson', clone_to_memory=True)
+            'exposure', 'census.geojson', clone_to_memory=True)
         expected_keyword = {
             'scale': source_scale,
             'license': source_license,
