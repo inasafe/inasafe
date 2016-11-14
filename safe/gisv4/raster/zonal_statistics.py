cdf3617343e81c80849c9b1162a1f15043a4dea8
Etienne Trimaille <gustrimaille@yahoo.fr>
2016-11-14 15:54:31 +0700

    fix zonal stats with the exposure count

--- safe/gisv4/raster/zonal_statistics.py
+++ safe/gisv4/raster/zonal_statistics.py
@@ -10,7 +10,8 @@ from qgis.core import QgsRasterLayer
 from qgis.analysis import QgsZonalStatistics
 
 from safe.gisv4.vector.tools import copy_layer, create_memory_layer
-from safe.definitionsv4.fields import total_field
+from safe.gisv4.vector.prepare_vector_layer import copy_fields, remove_fields
+from safe.definitionsv4.fields import exposure_count_field, total_field
 from safe.definitionsv4.processing_steps import zonal_stats_steps
 from safe.utilities.profiling import profile
 from safe.utilities.i18n import tr
@@ -60,10 +61,23 @@ def zonal_stats(raster, vector, callback=None):
     result = analysis.calculateStatistics(None)
     LOGGER.debug(tr('Zonal stats on %s : %s' % (raster.source(), result)))
 
+    layer.startEditing()
+    exposure = raster.keywords['exposure']
+    output_field = exposure_count_field['field_name'] % exposure
+    fields_to_rename = {
+        'exposure_sum': output_field
+    }
+    copy_fields(layer, fields_to_rename)
+    remove_fields(layer, fields_to_rename.keys())
+    layer.commitChanges()
+
     layer.keywords = raster.keywords.copy()
     layer.keywords['inasafe_fields'] = vector.keywords['inasafe_fields'].copy()
-    key = total_field['key']
-    layer.keywords['inasafe_fields'][key] = 'exposure_sum'
+    key = exposure_count_field['key'] % raster.keywords['exposure']
+
+    # Special case here, one field is the exposure count and the total.
+    layer.keywords['inasafe_fields'][key] = output_field
+    layer.keywords['inasafe_fields'][total_field['key']] = output_field
 
     layer.keywords['exposure_keywords'] = raster.keywords
     layer.keywords['hazard_keywords'] = vector.keywords['hazard_keywords']
