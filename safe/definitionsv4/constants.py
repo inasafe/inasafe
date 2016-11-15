b62871116ef55b08aeda412b7bfd96a1f8bdca22
Ismail Sunni <imajimatika@gmail.com>
2016-11-15 21:10:13 +0700

    Use list of whole number.

--- safe/definitionsv4/constants.py
+++ safe/definitionsv4/constants.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 """This module contains constants that are used in definitions.
 """
+from PyQt4.QtCore import QVariant
 from safe.utilities.i18n import tr
 
 __copyright__ = "Copyright 2016, The InaSAFE Project"
@@ -13,3 +14,20 @@ inasafe_keyword_version_key = 'keyword_version'
 multipart_polygon_key = 'multipart_polygon'
 no_field = tr('No Field')
 no_data_value = 200
+
+# Number in QVariant
+qvariant_numbers = [
+    QVariant.Int,
+    QVariant.Double,
+    QVariant.UInt,
+    QVariant.LongLong,
+    QVariant.ULongLong
+]
+
+# Whole Number in QVariant / Integer as in math
+qvariant_whole_numbers = [
+    QVariant.Int,
+    QVariant.UInt,
+    QVariant.LongLong,
+    QVariant.ULongLong
+]
