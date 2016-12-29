a38fb8139dcd3ce3b6479f2ed45f534ea6085a2c
Ismail Sunni <imajimatika@gmail.com>
2016-12-29 07:15:04 +0700

    Fix more landscape suggestion.

--- safe/messaging/__init__.py
+++ safe/messaging/__init__.py
@@ -1,20 +1,7 @@
-"""
-InaSAFE Disaster risk assessment tool developed by AusAid - **Paragraph.**
+# coding=utf-8
+"""Messaging init file."""
 
-Contact : ole.moller.nielsen@gmail.com
-
-.. note:: This program is free software; you can redistribute it and/or modify
-     it under the terms of the GNU General Public License as published by
-     the Free Software Foundation; either version 2 of the License, or
-     (at your option) any later version.
-"""
-
-__author__ = 'marco@opengis.ch'
-__revision__ = '$Format:%H$'
-__date__ = '27/05/2013'
-__copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
-                 'Disaster Reduction')
-
+# pylint: disable=unused-import
 from .item.brand import Brand
 from .item.text import Text
 from .item.important_text import ImportantText
@@ -34,3 +21,9 @@ from .item.table import Table
 from message import Message
 from error_message import ErrorMessage
 from styles import PROGRESS_UPDATE_STYLE
+# pylint: enable=unused-import
+
+__copyright__ = "Copyright 2016, The InaSAFE Project"
+__license__ = "GPL version 3"
+__email__ = "info@inasafe.org"
+__revision__ = '$Format:%H$'
