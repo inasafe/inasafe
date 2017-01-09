f1d5f0b1fc259cb3f1d5e865a553df2b129043b8
Etienne Trimaille <gustrimaille@yahoo.fr>
2017-01-09 16:47:40 +0700

    remove impact_functions/bases

--- safe/impact_functions/bases/layer_types/continuous_raster_exposure.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-__author__ = 'Rizky Maulana Nugraha "lucernae" <lana.pcfre@gmail.com>'
-__date__ = '28/05/15'
-
-
-class ContinuousRasterExposureMixin(object):
-
-    def __init__(self):
-        pass
