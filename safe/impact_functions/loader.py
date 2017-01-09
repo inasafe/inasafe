163d026f11818894d4a1799c3e4c880b2b5ac052
Etienne Trimaille <gustrimaille@yahoo.fr>
2017-01-09 16:10:24 +0700

    remove earthquake IF

--- safe/impact_functions/loader.py
+++ safe/impact_functions/loader.py
@@ -1,15 +1,5 @@
 # coding=utf-8
 
-# Earthquake
-from safe.impact_functions.earthquake.earthquake_building\
-    .impact_function import EarthquakeBuildingFunction
-from safe.impact_functions.earthquake.itb_earthquake_fatality_model\
-    .impact_function import ITBFatalityFunction
-from safe.impact_functions.earthquake.pager_earthquake_fatality_model\
-    .impact_function import PAGFatalityFunction
-from safe.impact_functions.earthquake.itb_bayesian_earthquake_fatality_model\
-    .impact_function import ITBBayesianFatalityFunction
-
 from safe.impact_functions.impact_function_manager import ImpactFunctionManager
 
 
@@ -20,10 +10,3 @@ __date__ = '5/25/16'
 def register_impact_functions():
     """Register all the impact functions available."""
     impact_function_registry = ImpactFunctionManager().registry
-
-    # Earthquake
-    impact_function_registry.register(EarthquakeBuildingFunction)
-    impact_function_registry.register(ITBFatalityFunction)
-    impact_function_registry.register(PAGFatalityFunction)
-    # Added in 3.3
-    impact_function_registry.register(ITBBayesianFatalityFunction)
