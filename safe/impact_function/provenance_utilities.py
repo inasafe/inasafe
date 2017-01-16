a248401887a46d3921ad45656009774255dc9ae4
Ismail Sunni <imajimatika@gmail.com>
2017-01-16 14:33:03 +0700

    Add get map legend title.

--- safe/impact_function/provenance_utilities.py
+++ safe/impact_function/provenance_utilities.py
@@ -5,6 +5,13 @@ from safe.utilities.i18n import tr
 
 from safe.definitions.hazard import hazard_generic
 from safe.definitions.hazard_category import hazard_category_single_event
+from safe.definitions.exposure import (
+    exposure_population,
+    exposure_land_cover,
+    exposure_road,
+    exposure_place,
+    exposure_structure
+)
 
 __copyright__ = "Copyright 2016, The InaSAFE Project"
 __license__ = "GPL version 3"
@@ -42,3 +49,27 @@ def get_map_title(hazard, exposure, hazard_category):
                     exposure_name=exposure['name'],
                     hazard_name=hazard['name']))
     return map_title
+
+
+def get_map_legend_title(exposure):
+    """helper to get map legend title.
+
+    :param exposure: An exposure definition.
+    :type exposure: dict
+
+    :returns: Map legend title based on the input.
+    :rtype: str
+    """
+    if exposure in [exposure_population, exposure_structure, exposure_place]:
+        return tr('Number of {exposure_name}').format(
+            exposure_name=exposure['name'])
+    if exposure == exposure_road:
+        return tr('Length of {exposure_name}').format(
+            exposure_name=exposure['name'])
+    if exposure == exposure_land_cover:
+        return tr('Area of {exposure_name}').format(
+            exposure_name=exposure['name'])
+
+    return tr('Number of {exposure_name}').format(
+        exposure_name=exposure['name'])
+
