c9e8974441c63d9715435164c203faca7a650865
Ismail Sunni <imajimatika@gmail.com>
2017-01-17 13:41:19 +0700

    Update description for threshold wizard step.

--- safe/gui/tools/wizard/wizard_strings.py
+++ safe/gui/tools/wizard/wizard_strings.py
@@ -167,10 +167,10 @@ continuous_vector_question = tr(
 )  # (subcategory, category, field, classification, min value, max value)
 continuous_raster_question = tr(
     'You have selected <b>%s %s</b> as a <b>continuous</b> layer with '
-    '<b>%s</b>. '
-    'Please input the minimum and maximum value for each class below. '
-    'The minimum value from the layer is <b>%s</b> and the maximum value is '
-    '<b>%s</b>.'
+    '<b>%s</b>. Minimum value will be included in the range, while maximum '
+    'value will be excluded. Please input the minimum and maximum value for '
+    'each class below. The minimum value from the layer is <b>%s</b> and the '
+    'maximum value is <b>%s</b>.'
 )  # (subcategory, category, classification, min value, max value)
 select_function_constraints2_question = tr(
     'You selected <b>%s</b> hazard and <b>%s</b> exposure. Now, select the '
