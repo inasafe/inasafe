189c19960dbc914dd53ab8b635c28fd71a19cf6a
Etienne Trimaille <gustrimaille@yahoo.fr>
2017-01-24 15:48:13 +0100

    remove useless code

--- safe/utilities/utilities.py
+++ safe/utilities/utilities.py
@@ -146,70 +146,6 @@ def humanise_seconds(seconds):
             days, hours, minutes))
 
 
-def reorder_dictionary(unordered_dictionary, expected_key_order):
-    """Reorder a dictionary according to a list of keys.
-
-    .. versionadded: 3.4
-
-    :param unordered_dictionary: The dictionary to reorder.
-    :type unordered_dictionary: dict
-
-    :param expected_key_order: The list of keys.
-    :type expected_key_order: list
-
-    :return: The new ordered dictionary.
-    :type: OrderedDict
-    """
-
-    ordered_dictionary = OrderedDict()
-
-    for item in expected_key_order:
-        if item in unordered_dictionary:
-            ordered_dictionary[item] = unordered_dictionary[item]
-
-    # Check if something is missing see #2969
-    if len(unordered_dictionary) != len(ordered_dictionary):
-        for key, value in unordered_dictionary.items():
-            if key not in ordered_dictionary.keys():
-                ordered_dictionary[key] = value
-
-    return ordered_dictionary
-
-
-def main_type(feature_type, value_mapping):
-    """Return the the main class from a feature by reading the mapping.
-
-    This function is used by buildings/roads IF.
-
-    .. versionadded: 3.4
-
-    :param feature_type: The type of the feature to test.
-    :type feature_type: str
-
-    :param value_mapping: The value mapping.
-    :type value_mapping: dict
-
-    :return: The main class name, if not found, it will return 'other'.
-    :rtype: str
-    """
-    other = 'other'
-
-    if feature_type in [None, 'NULL', 'null', 'Null', 0]:
-        return other
-
-    if feature_type.__class__.__name__ == 'QPyNullVariant':
-        return other
-
-    for key, values in value_mapping.iteritems():
-        if feature_type in values:
-            feature_class = key
-            break
-    else:
-        feature_class = other
-
-    return feature_class
-
-
 def impact_attribution(keywords, inasafe_flag=False):
     """Make a little table for attribution of data sources used in impact.
 
@@ -344,63 +280,6 @@ def html_to_file(html, file_path=None, open_browser=False):
 
     if open_browser:
         open_in_browser(file_path)
-
-
-def ranges_according_thresholds_list(list_of_thresholds):
-    """Return an ordered dictionary with the ranges according to thresholds.
-
-    This used to classify a raster according to arbitrary number of thresholds
-
-    Given input: [A, B, C, D]
-    it will produce ranges:
-
-    {
-        0: [A, B],
-        1: [B, C],
-        2: [C, D]
-    }
-
-    If you want to list infinite interval, you can set A or D as None. To
-    indicate the interval is open till infinity.
-
-    :param list_of_thresholds:
-    :type list_of_thresholds: list(float)
-    :return:
-    """
-    ranges = OrderedDict()
-    for i, threshold in enumerate(list_of_thresholds):
-        if i >= len(list_of_thresholds) - 1:
-            break
-        threshold_min = list_of_thresholds[i]
-        try:
-            threshold_max = list_of_thresholds[i + 1]
-        except IndexError:
-            threshold_max = None
-        ranges.update({
-            i: [threshold_min, threshold_max]
-        })
-    return ranges
-
-
-def ranges_according_thresholds(low_max, medium_max, high_max):
-    """Return an ordered dictionary with the ranges according to thresholds.
-
-    This used to classify a raster according three thresholds.
-
-    :param low_max: The low threshold.
-    :type low_max: float
-
-    :param medium_max: The medium threshold.
-    :type medium_max: float
-
-    :param high_max: The high threshold.
-    :type high_max: float
-
-    :return The ranges.
-    :rtype OrderedDict
-    """
-    return ranges_according_thresholds_list(
-        [None, 0.0, low_max, medium_max, high_max, None])
 
 
 def replace_accentuated_characters(message):
