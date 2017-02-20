2947a16ac23550412a2922fd6e3f9bf2ab0eef03
Ismail Sunni <imajimatika@gmail.com>
2017-02-20 09:13:10 +0700

    Remove small number hacky fix.

--- safe/definitions/constants.py
+++ safe/definitions/constants.py
@@ -9,7 +9,6 @@ __license__ = "GPL version 3"
 __email__ = "info@inasafe.org"
 __revision__ = '$Format:%H$'
 
-small_number = 2 ** -53  # I think this is small enough
 inasafe_keyword_version_key = 'keyword_version'
 multipart_polygon_key = 'multipart_polygon'
 no_field = tr('No Field')
