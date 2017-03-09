d562375abff5d14167eef92923024a7d2c5da364
Ismail Sunni <imajimatika@gmail.com>
2017-03-09 21:16:21 +0700

    Address Etienne's comments.

--- safe/impact_function/test/test_impact_function.py
+++ safe/impact_function/test/test_impact_function.py
@@ -616,7 +616,7 @@ class TestImpactFunction(unittest.TestCase):
             'earthquake_raster_on_raster_population': False,
             'polygon_classified_on_line': False,
             'polygon_classified_on_point': False,
-            'polygon_classified_on_vector_population': True,
+            'polygon_classified_on_vector_population': False,
             'polygon_continuous_on_line': False,
             'raster_classified_on_classified_raster': False,
             'raster_classified_on_indivisible_polygons_with_grid': False,
@@ -766,7 +766,7 @@ class TestImpactFunction(unittest.TestCase):
             'impact_function_title': impact_function.title,
             'notes': impact_function.notes(),
             'requested_extent': impact_function.requested_extent,
-            'data_store_uri': impact_function.datastore.uri.absolutePath(),
+            'data_store_uri': impact_function.datastore.uri_path,
             'datetime': impact_function.datetime,
         })
 
@@ -825,7 +825,7 @@ class TestImpactFunction(unittest.TestCase):
             'impact_function_title': impact_function.title,
             'notes': impact_function.notes(),
             'requested_extent': impact_function.requested_extent,
-            'data_store_uri': impact_function.datastore.uri.absolutePath(),
+            'data_store_uri': impact_function.datastore.uri_path,
             'datetime': impact_function.datetime,
         })
 
