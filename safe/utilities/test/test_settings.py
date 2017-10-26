02f3538b2502e3af25ae1d2f8530f5d3d294fd44
Ismail Sunni <imajimatika@gmail.com>
2017-10-26 14:25:34 +0700

    Refactor settings.py

--- safe/utilities/test/test_settings.py
+++ safe/utilities/test/test_settings.py
@@ -5,13 +5,14 @@ import unittest
 from safe.test.utilities import get_qgis_app
 QGIS_APP, CANVAS, IFACE, PARENT = get_qgis_app()
 from PyQt4.QtCore import QSettings
-from safe.definitions.constants import zero_default_value, RECENT
-from safe.utilities.default_values import (
-    set_inasafe_default_value_qsetting, get_inasafe_default_value_qsetting)
 from safe.definitions.default_settings import inasafe_default_settings
 from safe.utilities.settings import (
+    general_setting,
+    set_general_setting,
+    delete_general_setting,
     setting,
     set_setting,
+    delete_setting
 )
 
 __copyright__ = "Copyright 2016, The InaSAFE Project"
@@ -25,70 +26,36 @@ class TestSettings(unittest.TestCase):
 
     def setUp(self):
         """Fixture run before all tests"""
-        self.qsetting = QSettings('InaSAFETest')
-        self.qsetting.clear()
+        self.qsettings = QSettings('InaSAFETest')
+        self.qsettings.clear()
 
     def tearDown(self):
         """Fixture run after each test"""
         # Make sure it's empty
-        self.qsetting.clear()
+        self.qsettings.clear()
 
-    def test_get_value(self):
-        """Test we can get a value from a QSettings."""
-        # The expected type does not match the default value.
-        try:
-            self.assertEqual(
-                'default_value',
-                setting('test', 'default_value', bool, self.qsetting))
-        except Exception:
-            self.assertTrue(True)
-        else:
-            self.assertTrue(False)
-
+    def test_read_write_setting(self):
+        """Test read and write setting."""
+        # General Setting
+        set_general_setting('key', 'value', self.qsettings)
         self.assertEqual(
-            'default_value',
-            setting('test', 'default_value', str, self.qsetting))
+            'value', general_setting('key', qsettings=self.qsettings))
+        delete_general_setting('key', qsettings=self.qsettings)
+        self.assertEqual('default', general_setting(
+            'key', default='default', qsettings=self.qsettings))
 
-        self.assertIsNone(
-            setting('test', None, str, self.qsetting))
+        # Under InaSAFE scope
+        set_setting('key', 'value', self.qsettings)
+        self.assertEqual('value', setting('key', qsettings=self.qsettings))
+        delete_setting('key', qsettings=self.qsettings)
+        self.assertEqual('default', setting(
+            'key',default='default', qsettings=self.qsettings))
 
-        # Without default value.
-        self.assertIsNone(setting('test', qsettings=self.qsetting))
+        # Using InaSAFE setting default
+        key = 'developer_mode'
+        actual_value = inasafe_default_settings.get(key)
+        self.assertEqual(actual_value, setting(key, qsettings=self.qsettings))
 
-    def test_inasafe_default_value_qsetting(self):
-        """Test for set and get inasafe_default_value_qsetting."""
-        # Make sure it's empty
-        self.qsetting.clear()
-
-        female_ratio_key = 'female_ratio'
-        real_value = get_inasafe_default_value_qsetting(
-            self.qsetting, RECENT, female_ratio_key)
-        self.assertEqual(zero_default_value, real_value)
-
-        female_ratio_value = 0.8
-        set_inasafe_default_value_qsetting(
-            self.qsetting, RECENT, female_ratio_key, female_ratio_value)
-        real_value = get_inasafe_default_value_qsetting(
-            self.qsetting, RECENT, female_ratio_key)
-        self.assertEqual(female_ratio_value, real_value)
-
-    def test_read_boolean_setting(self):
-        """Test read developer mode setting."""
-        set_setting(
-            'developer_mode',
-            inasafe_default_settings['developer_mode'])
-
-        actual_setting = setting(
-            'developer_mode', inasafe_default_settings['developer_mode'])
-        self.assertFalse(actual_setting)
-
-        set_setting(
-            'developer_mode',
-            not(inasafe_default_settings['developer_mode']))
-
-        actual_setting = setting(
-            'developer_mode', inasafe_default_settings['developer_mode'])
-        self.assertTrue(actual_setting)
 
 if __name__ == '__main__':
     unittest.main()
