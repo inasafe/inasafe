e1b563863a52164012fe853894be4b863be146b0
Etienne Trimaille <gustrimaille@yahoo.fr>
2017-10-30 15:38:17 +0100

    remove pycodestyle pep8, jenkins and make PEP tests on travis

--- safe/test/test_python_pep.py
+++ safe/test/test_python_pep.py
@@ -15,7 +15,7 @@ __revision__ = '$Format:%H$'
 class TestPythonPep(unittest.TestCase):
 
     @unittest.skipIf(
-        os.environ.get('WITH_PYTHON_PEP', False),
+        not os.environ.get('WITH_PYTHON_PEP', True),
         'We can use make flake8 separately')
     def test_flake8(self):
         """Test if the code is Flake8 compliant."""
@@ -42,53 +42,7 @@ class TestPythonPep(unittest.TestCase):
         self.assertEquals(lines, 0, message)
 
     @unittest.skipIf(
-        os.environ.get('WITH_PYTHON_PEP', False),
-        'We can use make pep8 separately')
-    def test_pep8(self):
-        """Test if the code is PEP8 compliant."""
-        if os.environ.get('ON_TRAVIS', False):
-            root = './'
-            command = ['make', 'pep8']
-            output = Popen(command, stdout=PIPE, cwd=root).communicate()[0]
-            default_number_lines = 5
-        elif sys.platform.startswith('win'):
-            root = '../../'
-            command = [
-                'pep8.exe',
-                '--repeat',
-                '--ignore=E203,E121,E122,E123,E124,E125,E126,E127,E128,E402',
-                '--exclude=venv,pydev,safe_extras,keywords_dialog_base.py,'
-                'wizard_dialog_base.py,dock_base.py,options_dialog_base.py,'
-                'minimum_needs_configuration.py,resources_rc.py,help_base.py,'
-                'xml_tools.py,system_tools.py,data_audit.py,'
-                'data_audit_wrapper.py',
-                'safe']
-            # Shamelessly hardcoded path for now..TS
-            path = (
-                "C:\\PROGRA~1\\QGIS2~1.14\\apps\\Python27\\scripts")
-            path = os.path.normpath(path)
-            output = Popen(
-                command,
-                stdout=PIPE,
-                cwd=root,
-                executable=os.path.join(path, 'pep8.exe')).communicate()[0]
-            default_number_lines = 0
-
-        else:
-            # OSX and linux just delegate to make
-            root = '../../'
-            command = ['make', 'pep8']
-            output = Popen(command, stdout=PIPE, cwd=root).communicate()[0]
-            default_number_lines = 5
-
-        # make pep8 produces some extra lines by default.
-        lines = len(output.splitlines()) - default_number_lines
-        print output
-        message = 'Hey mate, go back to your keyboard :)'
-        self.assertEquals(lines, 0, message)
-
-    @unittest.skipIf(
-        os.environ.get('WITH_PYTHON_PEP', False),
+        not os.environ.get('WITH_PYTHON_PEP', True),
         'We can use make pep257 separately')
     def test_pep257(self):
         """Test if docstrings are PEP257 compliant."""
