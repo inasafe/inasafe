62ffe67c369757360d224a41901d84a0e9f08e0b
myarjunar <myarjunar@gmail.com>
2017-11-16 17:28:54 +0700

    fix default map report not generated

--- safe/report/impact_report.py
+++ safe/report/impact_report.py
@@ -788,8 +788,9 @@ class ImpactReport(object):
                             target_dir = os.path.join(
                                 dirname, 'resources', target_resource)
                             # copy here
-                            if not os.path.exists(target_dir):
-                                shutil.copytree(resource, target_dir)
+                            if os.path.exists(target_dir):
+                                shutil.rmtree(target_dir)
+                            shutil.copytree(resource, target_dir)
                     component.output = output
                 except Exception as e:  # pylint: disable=broad-except
                     generation_error_code = self.REPORT_GENERATION_FAILED
