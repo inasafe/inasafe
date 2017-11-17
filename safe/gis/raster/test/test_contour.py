d0f46d88a84c39d8120867c850d29ad2475a1f12
Ismail Sunni <imajimatika@gmail.com>
2017-11-17 10:48:04 +0700

    Add contour for shakemap raster layer.

---
+++ safe/gis/raster/test/test_contour.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+"""Test Contour."""
+
+import unittest
+
+__copyright__ = "Copyright 2017, The InaSAFE Project"
+__license__ = "GPL version 3"
+__email__ = "info@inasafe.org"
+__revision__ = '$Format:%H$'
+
+
+class TestContour(unittest.TestCase):
+    def test_contour(self):
+        """Test create contour"""
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
