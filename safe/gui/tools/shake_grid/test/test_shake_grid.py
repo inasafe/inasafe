d830843fffde58242341c0aa3e3e78f67240a6be
Ismail Sunni <imajimatika@gmail.com>
2017-11-21 10:37:16 +0700

    More unit test for shake map converter.

--- safe/gui/tools/shake_grid/test/test_shake_grid.py
+++ safe/gui/tools/shake_grid/test/test_shake_grid.py
@@ -31,9 +31,20 @@ SOURCE_PATH = standard_data_path(
 GRID_PATH = os.path.join(temp_dir(__name__), 'grid.xml')
 shutil.copyfile(SOURCE_PATH, GRID_PATH)
 
+NORMAL_SHAKE_GRID = ShakeGrid(
+    'Normal Shake Map',
+    'Normal',
+    GRID_PATH,
+    output_dir=temp_dir(sub_dir='normal')
+)
 
-SHAKE_GRID = ShakeGrid(
-    'Test Title', 'Test Source', GRID_PATH, smoothing_method=NUMPY_SMOOTHING)
+SMOOTHED_SHAKE_GRID = ShakeGrid(
+    'Smoothed Shake Map',
+    'Smoothed',
+    GRID_PATH,
+    output_dir=temp_dir(sub_dir='smoothed'),
+    smoothing_method=NUMPY_SMOOTHING
+)
 
 
 class TestShakeGrid(unittest.TestCase):
@@ -65,62 +76,75 @@ class TestShakeGrid(unittest.TestCase):
             message = 'Expected %s features, got %s' % (count, feature_count)
         return flag, message
 
+    def test_conversion(self):
+        """Test conversion for getting the raster."""
+        normal_shakemap_tif = NORMAL_SHAKE_GRID.mmi_to_raster()
+        normal_contour_shp = NORMAL_SHAKE_GRID.mmi_to_contours()
+
+        smoothed_shakemap_tif = SMOOTHED_SHAKE_GRID.mmi_to_raster()
+        smoothed_contour_shp = SMOOTHED_SHAKE_GRID.mmi_to_contours()
+
+        print normal_shakemap_tif
+        print normal_contour_shp
+        print smoothed_shakemap_tif
+        print smoothed_contour_shp
+
     def test_extract_date_time(self):
         """Test extract_date_time giving the correct output."""
         # Test on SHAKE_GRID
         expected_year = 2013
         message = 'Expected year should be %s, I got %s' % (
-            expected_year, SHAKE_GRID.year)
-        self.assertEqual(SHAKE_GRID.year, expected_year, message)
+            expected_year, SMOOTHED_SHAKE_GRID.year)
+        self.assertEqual(SMOOTHED_SHAKE_GRID.year, expected_year, message)
 
         expected_month = 11
         message = 'Expected month should be %s, I got %s' % (
-            expected_month, SHAKE_GRID.month)
-        self.assertEqual(SHAKE_GRID.month, expected_month, message)
+            expected_month, SMOOTHED_SHAKE_GRID.month)
+        self.assertEqual(SMOOTHED_SHAKE_GRID.month, expected_month, message)
 
         expected_day = 5
         message = 'Expected day should be %s, I got %s' % (
-            expected_day, SHAKE_GRID.day)
-        self.assertEqual(SHAKE_GRID.day, expected_day, message)
+            expected_day, SMOOTHED_SHAKE_GRID.day)
+        self.assertEqual(SMOOTHED_SHAKE_GRID.day, expected_day, message)
 
         expected_hour = 6
         message = 'Expected hour should be %s, I got %s' % (
-            expected_hour, SHAKE_GRID.hour)
-        self.assertEqual(SHAKE_GRID.hour, expected_hour, message)
+            expected_hour, SMOOTHED_SHAKE_GRID.hour)
+        self.assertEqual(SMOOTHED_SHAKE_GRID.hour, expected_hour, message)
 
         expected_minute = 8
         message = 'Expected minute should be %s, I got %s' % (
-            expected_minute, SHAKE_GRID.minute)
-        self.assertEqual(SHAKE_GRID.minute, expected_minute, message)
+            expected_minute, SMOOTHED_SHAKE_GRID.minute)
+        self.assertEqual(SMOOTHED_SHAKE_GRID.minute, expected_minute, message)
 
         expected_second = 9
         message = 'Expected second should be %s, I got %s' % (
-            expected_second, SHAKE_GRID.second)
-        self.assertEqual(SHAKE_GRID.second, expected_second, message)
+            expected_second, SMOOTHED_SHAKE_GRID.second)
+        self.assertEqual(SMOOTHED_SHAKE_GRID.second, expected_second, message)
 
     def test_parse_grid_xml(self):
         """Test parse_grid_xml works."""
-        self.assertEquals(5, SHAKE_GRID.day)
-        self.assertEquals(11, SHAKE_GRID.month)
-        self.assertEquals(2013, SHAKE_GRID.year)
-        self.assertEquals(6, SHAKE_GRID.hour)
-        self.assertEquals(8, SHAKE_GRID.minute)
-        self.assertEquals(9, SHAKE_GRID.second)
-        self.assertEquals('Asia/Jakarta', SHAKE_GRID.time_zone)
-        self.assertEquals(140.62, SHAKE_GRID.longitude)
-        self.assertEquals(-2.43, SHAKE_GRID.latitude)
-        self.assertEquals(10.0, SHAKE_GRID.depth)
-        self.assertEquals('Papua', SHAKE_GRID.location)
-        self.assertEquals(139.37, SHAKE_GRID.x_minimum)
-        self.assertEquals(141.87, SHAKE_GRID.x_maximum)
-        self.assertEquals(-3.67875, SHAKE_GRID.y_minimum)
-        self.assertEquals(-1.18125, SHAKE_GRID.y_maximum)
+        self.assertEquals(5, SMOOTHED_SHAKE_GRID.day)
+        self.assertEquals(11, SMOOTHED_SHAKE_GRID.month)
+        self.assertEquals(2013, SMOOTHED_SHAKE_GRID.year)
+        self.assertEquals(6, SMOOTHED_SHAKE_GRID.hour)
+        self.assertEquals(8, SMOOTHED_SHAKE_GRID.minute)
+        self.assertEquals(9, SMOOTHED_SHAKE_GRID.second)
+        self.assertEquals('Asia/Jakarta', SMOOTHED_SHAKE_GRID.time_zone)
+        self.assertEquals(140.62, SMOOTHED_SHAKE_GRID.longitude)
+        self.assertEquals(-2.43, SMOOTHED_SHAKE_GRID.latitude)
+        self.assertEquals(10.0, SMOOTHED_SHAKE_GRID.depth)
+        self.assertEquals('Papua', SMOOTHED_SHAKE_GRID.location)
+        self.assertEquals(139.37, SMOOTHED_SHAKE_GRID.x_minimum)
+        self.assertEquals(141.87, SMOOTHED_SHAKE_GRID.x_maximum)
+        self.assertEquals(-3.67875, SMOOTHED_SHAKE_GRID.y_minimum)
+        self.assertEquals(-1.18125, SMOOTHED_SHAKE_GRID.y_maximum)
 
-        grid_xml_data = SHAKE_GRID.mmi_data
+        grid_xml_data = SMOOTHED_SHAKE_GRID.mmi_data
         self.assertEquals(10201, len(grid_xml_data))
 
         # Check SHAKE_GRID.grid_bounding_box
-        bounds = SHAKE_GRID.grid_bounding_box.toString()
+        bounds = SMOOTHED_SHAKE_GRID.grid_bounding_box.toString()
         expected_result = (
             '139.3700000000000045,-3.6787500000000000 : '
             '141.8700000000000045,-1.1812499999999999')
@@ -129,7 +153,7 @@ class TestShakeGrid(unittest.TestCase):
 
     def test_grid_file_path(self):
         """Test grid_file_path works properly."""
-        grid_path = SHAKE_GRID.grid_file_path()
+        grid_path = SMOOTHED_SHAKE_GRID.grid_file_path()
         expected_path = GRID_PATH
         message = 'Grid path should be %s, but I got %s' % (
             expected_path, grid_path)
@@ -137,8 +161,9 @@ class TestShakeGrid(unittest.TestCase):
 
     def test_mmi_to_delimited_text(self):
         """Test mmi_to_delimited_text works."""
-        delimited_string = SHAKE_GRID.mmi_to_delimited_text()
-        number_of_line = len(SHAKE_GRID.mmi_data) + 1  # Add 1 for header
+        delimited_string = SMOOTHED_SHAKE_GRID.mmi_to_delimited_text()
+        # Add 1 for header
+        number_of_line = len(SMOOTHED_SHAKE_GRID.mmi_data) + 1
         substring = (
             'lon,lat,mmi\n'
             '139.37,-1.1813,1.0\n'
@@ -153,7 +178,7 @@ class TestShakeGrid(unittest.TestCase):
     def test_mmi_to_delimited_file(self):
         """Test mmi_to_delimited_file works."""
         # Check CSV File
-        file_path = SHAKE_GRID.mmi_to_delimited_file(
+        file_path = SMOOTHED_SHAKE_GRID.mmi_to_delimited_file(
             force_flag=True)
         delimited_file = file(file_path)
         delimited_string = delimited_file.readlines()
@@ -170,7 +195,7 @@ class TestShakeGrid(unittest.TestCase):
     def test_mmi_to_raster(self):
         """Check we can convert the shake event to a raster."""
         # Check the tif file
-        raster_path = SHAKE_GRID.mmi_to_raster(force_flag=True)
+        raster_path = SMOOTHED_SHAKE_GRID.mmi_to_raster(force_flag=True)
         self.assertTrue(os.path.exists(raster_path))
         # Check the qml file
         expected_qml = raster_path.replace('tif', 'qml')
@@ -185,7 +210,7 @@ class TestShakeGrid(unittest.TestCase):
     def test_mmi_to_shapefile(self):
         """Check we can convert the shake event to a shapefile."""
         # Check the shp file
-        file_path = SHAKE_GRID.mmi_to_shapefile(force_flag=True)
+        file_path = SMOOTHED_SHAKE_GRID.mmi_to_shapefile(force_flag=True)
         self.assertTrue(os.path.exists(file_path))
         # Check the qml file
         expected_qml = file_path.replace('shp', 'qml')
@@ -197,7 +222,7 @@ class TestShakeGrid(unittest.TestCase):
         os.environ.get('ON_TRAVIS', False), 'This test is failing in docker.')
     def test_event_to_contours(self):
         """Check we can extract contours from the event."""
-        file_path = SHAKE_GRID.mmi_to_contours(
+        file_path = SMOOTHED_SHAKE_GRID.mmi_to_contours(
             force_flag=True, algorithm='invdist')
         self.assertTrue(self.check_feature_count(file_path, 16))
         self.assertTrue(os.path.exists(file_path))
@@ -205,10 +230,10 @@ class TestShakeGrid(unittest.TestCase):
         message = '%s not found' % expected_qml
         self.assertTrue(os.path.exists(expected_qml), message)
 
-        file_path = SHAKE_GRID.mmi_to_contours(
+        file_path = SMOOTHED_SHAKE_GRID.mmi_to_contours(
             force_flag=True, algorithm='nearest')
         self.assertTrue(self.check_feature_count(file_path, 132))
-        file_path = SHAKE_GRID.mmi_to_contours(
+        file_path = SMOOTHED_SHAKE_GRID.mmi_to_contours(
             force_flag=True, algorithm='average')
         self.assertTrue(self.check_feature_count(file_path, 132))
 
