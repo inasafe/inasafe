d93acf650bf5134d5ff15001460282b56fca2a57
Ismail Sunni <imajimatika@gmail.com>
2017-12-20 17:38:22 +0700

    Fix unit test and update test data.

--- safe/gis/vector/test/test_summary.py
+++ safe/gis/vector/test/test_summary.py
@@ -13,7 +13,10 @@ from safe.definitions.fields import (
     exposure_count_field,
     productivity_field,
     production_cost_field,
-    production_value_field
+    production_value_field,
+    affected_productivity_field,
+    affected_production_cost_field,
+    affected_production_value_field,
 )
 from safe.gis.vector.tools import read_dynamic_inasafe_field
 from safe.gis.vector.summary_1_aggregate_hazard import (
@@ -246,14 +249,15 @@ class TestSummary(unittest.TestCase):
 
         # We should have
         # one column per hazard
-        # one for the exposure
-        # one for total affected
-        # one for total not affected
-        # one for total not exposed
-        # one for total
-        # one for affected productivity
-        # one for affected production cost
-        # one for affected production value
+
+        # 1. one for the exposure
+        # 2. one for total affected
+        # 3. one for total not affected
+        # 4. one for total not exposed
+        # 5. one for total
+        # 6. one for affected productivity
+        # 7. one for affected production cost
+        # 8. one for affected production value
         self.assertEqual(layer.fields().count(), len(unique_hazard) + 8)
 
     def test_summarize_result(self):
@@ -267,11 +271,12 @@ class TestSummary(unittest.TestCase):
 
         summarizer_dicts = summarize_result(exposure_summary)
 
-        productivity_summary = summarizer_dicts.get(productivity_field['key'])
+        productivity_summary = summarizer_dicts.get(
+            affected_productivity_field['key'])
         production_cost_summary = summarizer_dicts.get(
-            production_cost_field['key'])
+            affected_production_cost_field['key'])
         production_value_summary = summarizer_dicts.get(
-            production_value_field['key'])
+            affected_production_value_field['key'])
 
         self.assertIsNotNone(productivity_summary)
         self.assertIsNotNone(production_cost_summary)
