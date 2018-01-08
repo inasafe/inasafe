3ad608913c4f228f00444a14684ff2188ea50c8b
Etienne Trimaille <gustrimaille@yahoo.fr>
2018-01-08 16:29:47 +0300

    make Extent a singleton and fix multiexposure dialog to use it

--- safe/utilities/extent.py
+++ safe/utilities/extent.py
@@ -25,6 +25,22 @@ __email__ = "info@inasafe.org"
 __revision__ = '$Format:%H$'
 
 
+def singleton(class_):
+    """Singleton definition.
+
+    Method 1 from
+    https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+    """
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return get_instance
+
+
+@singleton
 class Extent(object):
     """Extent class to handle analysis extent.
 
