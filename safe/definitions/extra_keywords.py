2126656df142406a03ff9906f8ddcc028852e6fa
Ismail Sunni <imajimatika@gmail.com>
2018-01-10 09:58:09 +0700

    Fix unit test and make sure no duplicated definitions.

--- safe/definitions/extra_keywords.py
+++ safe/definitions/extra_keywords.py
@@ -14,73 +14,73 @@ __revision__ = '$Format:%H$'
 extra_keyword_analysis_type = {
     'key': 'analysis_type',
     'name': tr('Analysis type'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_latitude = {
     'key': 'latitude',
     'name': tr('Latitude'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_longitude = {
     'key': 'longitude',
     'name': tr('Longitude'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_magnitude = {
     'key': 'magnitude',
     'name': tr('Magnitude'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_depth = {
     'key': 'depth',
     'name': tr('Depth'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_description = {
     'key': 'description',
     'name': tr('Description'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_location = {
     'key': 'location',
     'name': tr('Location'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_time_zone = {
     'key': 'time_zone',
     'name': tr('Time zone'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_x_minimum = {
     'key': 'x_minimum',
     'name': tr('X minimum'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_x_maximum = {
     'key': 'x_maximum',
     'name': tr('X maximum'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_y_minimum = {
     'key': 'y_minimum',
     'name': tr('Y minimum'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_y_maximum = {
     'key': 'y_maximum',
     'name': tr('Y maximum'),
-    'description': None
+    'description': tr('')
 }
 
 extra_keyword_volcano_name = {
@@ -203,7 +203,7 @@ all_extra_keywords = [
     extra_keyword_x_minimum,
     extra_keyword_y_maximum,
     extra_keyword_y_minimum,
-]
+] + ash_extra_keywords
 
 # map all extra keywords to a pair of key and name
 all_extra_keywords_name = {}
@@ -219,3 +219,4 @@ for extra_keyword in all_extra_keywords:
     else:
         all_extra_keywords_description[extra_keyword['key']] = (
             extra_keyword['name'])
+    del extra_keyword  # Make sure there is no duplicate dictionary
