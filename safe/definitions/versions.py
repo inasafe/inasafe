15bee61b1b0e11de0d0689bbba275b2d151b26f5
Etienne Trimaille <gustrimaille@yahoo.fr>
2018-03-12 11:17:21 +0100

    bump to version 4.4.0

--- safe/definitions/versions.py
+++ safe/definitions/versions.py
@@ -8,12 +8,12 @@ __email__ = "info@inasafe.org"
 __revision__ = '$Format:%H$'
 
 # InaSAFE version (please synchronize with metadata.txt or travis will fail !)
-inasafe_version = '4.3.2'
+inasafe_version = '4.4.0'
 # alpha, beta, rc or final (Travis will fail)
-inasafe_release_status = 'final'
+inasafe_release_status = 'beta'
 
 # InaSAFE Keyword Version compatibility.
-inasafe_keyword_version = '4.3'
+inasafe_keyword_version = '4.4'
 keyword_version_compatibilities = {
     # 'InaSAFE keyword version': 'List of supported InaSAFE keyword version'
     '3.3': ['3.2'],
@@ -21,5 +21,6 @@ keyword_version_compatibilities = {
     '3.5': ['3.4', '3.3'],
     '4.1': ['4.0'],
     '4.2': ['4.1', '4.0'],
-    '4.3': ['4.2', '4.1', '4.0']
+    '4.3': ['4.2', '4.1', '4.0'],
+    '4.4': ['4.3', '4.2', '4.1', '4.0'],
 }
