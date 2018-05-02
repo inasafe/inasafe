eb643049c010bfdd972dfcbbead7f10da9afaa7f
Ismail Sunni <imajimatika@gmail.com>
2018-05-02 08:32:23 +0700

    Add more handling for exception in uploading layer to geonode.

--- safe/utilities/geonode/test/test_geonode_uploaded.py
+++ safe/utilities/geonode/test/test_geonode_uploaded.py
@@ -13,14 +13,14 @@ import unittest
 from safe.definitions.constants import INASAFE_TEST
 from safe.test.utilities import load_test_vector_layer, load_test_raster_layer
 from safe.utilities.geonode.upload_layer_requests import upload, login_user
+from safe.common.exceptions import GeoNodeLoginError, GeoNodeInstanceError
 
 from safe.test.utilities import get_qgis_app
 QGIS_APP, CANVAS, IFACE, PARENT = get_qgis_app(qsetting=INASAFE_TEST)
 
-LOGIN = ''
-PASSWORD = ''
-GEONODE_URL = ''
-
+LOGIN = 'test_geonode_user'
+PASSWORD = 'test_geonode_password'
+GEONODE_URL = 'http://138.201.158.66'
 
 class GeonodeUploadTest(unittest.TestCase):
 
@@ -46,14 +46,41 @@ class GeonodeUploadTest(unittest.TestCase):
             self.assertEqual(GEONODE_URL, '', 'URL must be empty.')
 
     @unittest.skipUnless(LOGIN, 'You need to fill LOGIN and PASSWORD above.')
-    def test_login_and_upload_layers(self):
-        """Test login page."""
+    def test_login(self):
+        """Test login to geonode."""
+        dummy_user = 'NotValidUser'
+        dummy_password = 'NotValidPassword'
+        not_geonode_instance = 'http://google.com'
+        # Failed login (invalid geonode)
+        self.assertRaises(
+            GeoNodeInstanceError,
+            login_user,
+            not_geonode_instance,
+            LOGIN,
+            PASSWORD)
+        # Failed login (wrong credential)
+        self.assertRaises(
+            GeoNodeLoginError,
+            login_user,
+            GEONODE_URL,
+            dummy_user,
+            dummy_password)
+
+    @unittest.skipUnless(LOGIN, 'You need to fill LOGIN and PASSWORD above.')
+    def test_upload_raster_layer(self):
+        """Test upload raster layer."""
         # Connection
         session = login_user(GEONODE_URL, LOGIN, PASSWORD)
 
         # Upload a single raster layer
         result = upload(GEONODE_URL, session, self.raster.source())
         self.assertTrue(result['success'])
+
+    @unittest.skipUnless(LOGIN, 'You need to fill LOGIN and PASSWORD above.')
+    def test_upload_shapefile_layers(self):
+        """Test upload shapefile layer."""
+        # Connection
+        session = login_user(GEONODE_URL, LOGIN, PASSWORD)
 
         # Upload a vector layer (with many files)
         result = upload(GEONODE_URL, session, self.vector.source())
