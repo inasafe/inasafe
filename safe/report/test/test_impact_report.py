77503987b084b91aa88540251017317b3c20223f
Alessandro Pasotti <elpaso@itopen.it>
2018-05-03 14:58:20 +0200

    Use deepdiff on dict comparison

--- safe/report/test/test_impact_report.py
+++ safe/report/test/test_impact_report.py
@@ -1180,7 +1180,7 @@ class TestImpactReport(unittest.TestCase):
         }
         actual_context = minimum_needs.context
 
-        self.assertEqual(DeepDiff(expected_context, actual_context), {})
+        self.assertEqual(DeepDiff(expected_context, actual_context, ignore_order=True), {})
 
         self.assertTrue(
             minimum_needs.output, empty_component_output_message)
@@ -1317,7 +1317,7 @@ class TestImpactReport(unittest.TestCase):
         }
         actual_context = minimum_needs.context
 
-        self.assertDictEqual(expected_context, actual_context)
+        self.assertEqual(DeepDiff(expected_context, actual_context, ignore_order=True), {})
         self.assertTrue(
             minimum_needs.output, empty_component_output_message)
 
