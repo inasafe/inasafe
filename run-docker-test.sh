fef74c4fd5e4130862e912d787a3a6047c655035
Alessandro Pasotti <elpaso@itopen.it>
2018-05-08 14:10:03 +0200

    Update run docker to QGIS3

--- run-docker-test.sh
+++ run-docker-test.sh
@@ -1,8 +1,38 @@
 #!/usr/bin/env bash
+IMAGE=elpaso/qgis-testing-environment
+#IMAGE=kartoza/qgis-testing
+QGIS_VERSION_TAG=master
 
-docker run -d --name qgis-testing-environment -v ${PWD}:/tests_directory -e WITH_PYTHON_PEP=false -e ON_TRAVIS=false -e MUTE_LOGS=true -e DISPLAY=:99 kartoza/qgis-testing:boundlessgeo-2.14.7
+DISPLAY=${DISPLAY:-:99}
+
+if [ "${DISPLAY}" != ":99" ]; then
+    xhost +
+fi
+
+docker run -d --name qgis-testing-environment \
+    -v ${PWD}:/tests_directory \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e WITH_PYTHON_PEP=false \
+    -e ON_TRAVIS=false \
+    -e INASAFE_DISABLE_WELCOME_MESSAGE=1 \
+    -e INASAFE_LOGGING_LEVEL=50 \
+    -e MUTE_LOGS=true \
+    -e DISPLAY=${DISPLAY} \
+    ${IMAGE}:${QGIS_VERSION_TAG}
+
+
 sleep 10
+docker exec -it qgis-testing-environment sh -c "pip3 install -r /tests_directory/REQUIREMENTS.txt"
+docker exec -it qgis-testing-environment sh -c "pip3 install -r /tests_directory/REQUIREMENTS_TESTING.txt"
+
 docker exec -it qgis-testing-environment sh -c "qgis_setup.sh inasafe"
+
+# FIX default installation because the sources must be in "inasafe" parent folder
+docker exec -it qgis-testing-environment sh -c "rm -f  /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/inasafe"
+docker exec -it qgis-testing-environment sh -c "ln -s /tests_directory/ /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/inasafe"
+
+# Run the real test
 time docker exec -it qgis-testing-environment sh -c "qgis_testrunner.sh test_suite.test_package"
+
 docker kill qgis-testing-environment
 docker rm qgis-testing-environment
