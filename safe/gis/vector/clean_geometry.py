3e2f28f5a8e2af9fe15e3b6337dfb3918620e208
Nyall Dawson <nyall.dawson@gmail.com>
2018-06-11 10:12:59 +1000

    Ensure that clean_layer updates layer with repaired geometries

--- safe/gis/vector/clean_geometry.py
+++ safe/gis/vector/clean_geometry.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 """Try to make a layer valid."""
+from qgis.core import QgsFeatureRequest
 
 from safe.common.custom_logging import LOGGER
 from safe.definitions.processing_steps import clean_geometry_steps
@@ -31,11 +32,11 @@ def clean_layer(layer):
     count = 0
 
     # iterate through all features
-    for feature in layer.getFeatures():
+    for feature in layer.getFeatures(QgsFeatureRequest().setSubsetOfAttributes([])):
         geom = feature.geometry()
         geometry_cleaned = geometry_checker(geom)
-        if geometry_cleaned:
-            feature.setGeometry(geometry_cleaned)
+        if geometry_cleaned and not geom.equals(geometry_cleaned):
+            layer.changeGeometry(feature.id(), geometry_cleaned, skipDefaultValue=True)
         else:
             count += 1
             layer.deleteFeature(feature.id())
