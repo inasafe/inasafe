711860fd5bdc6a5a7f728ecc61f95ceec35568c7
Nyall Dawson <nyall.dawson@gmail.com>
2018-06-28 11:13:28 +1000

    Replace custom clip alg with built-in processing algorithm
    Because:
    - it's native c++ code (faster)
    - lots of unit tests, user tests (so should be more stable)
    - burden of maintainence is on QGIS project

--- safe/gis/vector/clip.py
+++ safe/gis/vector/clip.py
@@ -2,19 +2,18 @@
 
 """Clip and mask a hazard layer."""
 
-import logging
+import processing
 
 from qgis.core import (
-    QgsGeometry,
+    QgsApplication,
     QgsFeatureRequest,
-    QgsWkbTypes,
-    QgsFeature,
-)
+    QgsProcessingContext,
+    QgsProject)
+from qgis.analysis import QgsNativeAlgorithms
 
+from safe.common.exceptions import ProcessingInstallationError
 from safe.definitions.processing_steps import clip_steps
 from safe.gis.sanity_check import check_layer
-from safe.gis.vector.tools import create_memory_layer
-from safe.utilities.i18n import tr
 from safe.utilities.profiling import profile
 
 __copyright__ = "Copyright 2016, The InaSAFE Project"
@@ -23,7 +22,17 @@ __email__ = "info@inasafe.org"
 __revision__ = '$Format:%H$'
 
 
-LOGGER = logging.getLogger('InaSAFE')
+def create_processing_context():
+    """
+    Creates a default processing context
+    """
+    context = QgsProcessingContext()
+    context.setProject(QgsProject.instance())
+
+    # skip Processing geometry checks - Inasafe has its own geometry validation
+    # routines which have already been used
+    context.setInvalidGeometryCheck(QgsFeatureRequest.GeometryNoCheck)
+    return context
 
 
 @profile
@@ -31,10 +40,6 @@ def clip(layer_to_clip, mask_layer):
     """Clip a vector layer with another.
 
     Issue https://github.com/inasafe/inasafe/issues/3186
-
-    Note : This algorithm is copied from :
-    https://github.com/qgis/QGIS/blob/master/python/plugins/processing/algs/
-    qgis/Clip.py
 
     :param layer_to_clip: The vector layer to clip.
     :type layer_to_clip: QgsVectorLayer
@@ -51,120 +56,24 @@ def clip(layer_to_clip, mask_layer):
     output_layer_name = output_layer_name % (
         layer_to_clip.keywords['layer_purpose'])
 
-    writer = create_memory_layer(
-        output_layer_name,
-        layer_to_clip.geometryType(),
-        layer_to_clip.crs(),
-        layer_to_clip.fields()
-    )
-    writer.startEditing()
+    parameters = {'INPUT': layer_to_clip,
+                  'OVERLAY': mask_layer,
+                  'OUTPUT': 'memory:'}
 
-    # Begin copy/paste from Processing plugin.
-    # Please follow their code as their code is optimized.
-    # The code below is not following our coding standards because we want to
-    # be able to track any diffs from QGIS easily.
+    # TODO implement callback through QgsProcessingFeedback object
 
-    # first build up a list of clip geometries
-    clip_geometries = []
-    request = QgsFeatureRequest().setSubsetOfAttributes([])
-    for mask_feature in mask_layer.getFeatures(request):
-        clip_geometries.append(QgsGeometry(mask_feature.geometry()))
+    # Required if running from command line
+    if not QgsApplication.processingRegistry().algorithms():
+        QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
 
-    # are we clipping against a single feature? if so,
-    # we can show finer progress reports
-    if len(clip_geometries) > 1:
-        # noinspection PyTypeChecker,PyCallByClass,PyArgumentList
-        combined_clip_geom = QgsGeometry.unaryUnion(clip_geometries)
-        single_clip_feature = False
-    else:
-        combined_clip_geom = clip_geometries[0]
-        single_clip_feature = True
+    context = create_processing_context()
+    result = processing.run('native:clip', parameters, context=context)
+    if result is None:
+        raise ProcessingInstallationError
 
-    # use prepared geometries for faster intersection tests
-    # noinspection PyArgumentList
-    engine = QgsGeometry.createGeometryEngine(combined_clip_geom.constGet())
-    engine.prepareGeometry()
+    clipped = result['OUTPUT']
 
-    tested_feature_ids = set()
-
-    for i, clip_geom in enumerate(clip_geometries):
-        request = QgsFeatureRequest().setFilterRect(clip_geom.boundingBox())
-        input_features = [f for f in layer_to_clip.getFeatures(request)]
-
-        if not input_features:
-            continue
-
-        # if single_clip_feature:
-        #     total = 100.0 / len(input_features)
-        # else:
-        #     total = 0
-
-        for current, in_feat in enumerate(input_features):
-            if not in_feat.geometry():
-                continue
-
-            if in_feat.id() in tested_feature_ids:
-                # don't retest a feature we have already checked
-                continue
-
-            tested_feature_ids.add(in_feat.id())
-
-            if not engine.intersects(in_feat.geometry().constGet()):
-                continue
-
-            if not engine.contains(in_feat.geometry().constGet()):
-                cur_geom = in_feat.geometry()
-                new_geom = combined_clip_geom.intersection(cur_geom)
-                if new_geom.wkbType() == QgsWkbTypes.Unknown \
-                        or QgsWkbTypes.flatType(
-                        new_geom.constGet().wkbType()) == \
-                        QgsWkbTypes.GeometryCollection:
-                    int_com = in_feat.geometry().combine(new_geom)
-                    int_sym = in_feat.geometry().symDifference(new_geom)
-                    if not int_com or not int_sym:
-                        # LOGGER.debug(
-                        #     tr('GEOS geoprocessing error: One or more input '
-                        #        'features have invalid geometry.'))
-                        pass
-                    else:
-                        new_geom = int_com.difference(int_sym)
-                        if new_geom.isEmpty()\
-                                or not new_geom.isGeosValid():
-                            # LOGGER.debug(
-                            #     tr('GEOS geoprocessing error: One or more '
-                            #        'input features have invalid geometry.'))
-                            pass
-            else:
-                # clip geometry totally contains feature geometry,
-                # so no need to perform intersection
-                new_geom = in_feat.geometry()
-
-            try:
-                out_feat = QgsFeature()
-                out_feat.setGeometry(new_geom)
-                out_feat.setAttributes(in_feat.attributes())
-                if new_geom.type() == layer_to_clip.geometryType():
-                    writer.addFeature(out_feat)
-            except BaseException:
-                LOGGER.debug(
-                    tr('Feature geometry error: One or more output features '
-                       'ignored due to invalid geometry.'))
-                continue
-
-            # TODO implement callback
-            if single_clip_feature:
-                # progress.setPercentage(int(current * total))
-                pass
-
-        if not single_clip_feature:
-            # coarse progress report for multiple clip geometries
-            # progress.setPercentage(100.0 * i / len(clip_geoms))
-            pass
-
-    # End copy/paste from Processing plugin.
-    writer.commitChanges()
-
-    writer.keywords = layer_to_clip.keywords.copy()
-    writer.keywords['title'] = output_layer_name
-    check_layer(writer)
-    return writer
+    clipped.keywords = layer_to_clip.keywords.copy()
+    clipped.keywords['title'] = output_layer_name
+    check_layer(clipped)
+    return clipped
