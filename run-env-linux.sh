cb8b2495243915181dc1200daed9a4f6052bd33a
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2019-10-31 21:31:15 +0700

    Maintenance commit for repo
    - Assign default python-version for pyenv
    - Pin package version in REQUIREMENTS_TESTING.txt
    - Update run-env-linux.sh
    - Fix new flake issues

--- run-env-linux.sh
+++ run-env-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-QGIS_PREFIX_PATH=/usr/local/qgis-2.18
+QGIS_PREFIX_PATH=/usr
 if [ -n "$1" ]; then
     QGIS_PREFIX_PATH=$1
 fi
@@ -11,14 +11,12 @@ echo $QGIS_PREFIX_PATH
 export QGIS_PREFIX_PATH=$QGIS_PREFIX_PATH
 export QGIS_PATH=$QGIS_PREFIX_PATH
 export LD_LIBRARY_PATH=${QGIS_PREFIX_PATH}/lib
-export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python:${QGIS_PREFIX_PATH}/share/qgis/python/plugins:${PYTHONPATH}
+export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python:${QGIS_PREFIX_PATH}/share/qgis/python/plugins:~/.local/share/QGIS/QGIS3/profiles/default/python/plugins:${PYTHONPATH}
 
 echo "QGIS PATH: $QGIS_PREFIX_PATH"
 export QGIS_DEBUG=0
 export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
 export QGIS_DEBUG_FILE=/tmp/inasafe/realtime/logs/qgis-debug.log
-
-export PATH=${QGIS_PREFIX_PATH}/bin:$PATH
 
 export INASAFE_WORK_DIR=/tmp/inasafe
 export INASAFE_POPULATION_PATH=`pwd`/realtime/fixtures/exposure/population.tif
@@ -34,7 +32,7 @@ export INASAFE_REALTIME_REST_LOGIN_URL=http://localhost:8000/realtime/api-auth/l
 export INASAFE_SENTRY=1
 
 echo "This script is intended to be sourced to set up your shell to"
-echo "use a QGIS 2.0 built in $QGIS_PREFIX_PATH"
+echo "use a QGIS 3 built in $QGIS_PREFIX_PATH"
 echo
 echo "To use it do:"
 echo "source $BASH_SOURCE /your/optional/install/path"
