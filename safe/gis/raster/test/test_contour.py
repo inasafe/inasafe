c059a19f3f3daf744acf749b2388c06686918b2c
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2019-11-11 23:36:04 +0700

    Skip test_contour unit test in QGIS 3.4
    Skip safe.gis.raster.test.test_contour unit test
    in QGIS 3.4. This is due the qgis_testrunner.sh thread
    was stalling. The stalling code is inside qgis binary,
    so I decided that we won't further debug the problem.
    
    The unit test can be run directly with unittest
    without problem.

--- safe/gis/raster/test/test_contour.py
+++ safe/gis/raster/test/test_contour.py
@@ -23,9 +23,12 @@ __revision__ = '$Format:%H$'
 
 
 class TestContour(unittest.TestCase):
-
     """Test Contour"""
 
+    @unittest.skipIf(
+        os.environ.get('QGIS_VERSION_TAG') == 'release-3_4',
+        'Skip this in QGIS 3.4 due to stall issue from core qgis thread.'
+    )
     def test_contour(self):
         """Test create contour"""
         output_file_path = unique_filename(suffix='-contour.gpkg')
@@ -46,6 +49,10 @@ class TestContour(unittest.TestCase):
         self.assertTrue(os.path.exists(metadata_path))
         self.assertTrue(metadata_path.endswith('.xml'))
 
+    @unittest.skipIf(
+        os.environ.get('QGIS_VERSION_TAG') == 'release-3_4',
+        'Skip this in QGIS 3.4 due to stall issue from core qgis thread.'
+    )
     def test_smoothing(self):
         """Test smoothing method."""
         # Load shake map layer
@@ -58,6 +65,10 @@ class TestContour(unittest.TestCase):
         smoothed_shakemap_path = smooth_shakemap(shakemap_layer.source())
         self.assertTrue(os.path.exists(smoothed_shakemap_path))
 
+    @unittest.skipIf(
+        os.environ.get('QGIS_VERSION_TAG') == 'release-3_4',
+        'Skip this in QGIS 3.4 due to stall issue from core qgis thread.'
+    )
     def test_contour_shakemap(self):
         """Test for contour creation (with smoothing)."""
         # Original
